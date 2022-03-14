93d36a0eafce0118a535fe04050354dd1231d361
Ganesan Ramalingam <grama@microsoft.com>
2022-03-14 14:29:38 -0700

    Initial version of model-proto creation with model-local functions
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>

--- onnxscript/__main__.py
+++ onnxscript/__main__.py
@@ -1,10 +1,49 @@
 # SPDX-License-Identifier: Apache-2.0
 
-import sys
+from typing import Optional
+import os
 from .converter import convert
+import onnx
 
 # command-line utility to invoke converter on a python file
 
+def to_single_model_proto(input_py_file: str, output_onnx_file: Optional[str] = None):
+    if (not output_onnx_file):
+        prefix, ext = os.path.splitext(input_py_file)
+        output_onnx_file = prefix + ".onnx"
+
+    fnlist = convert(input_py_file)
+
+    # for now, treat the last function in input-file as the "main graph"
+    # TODO: let user specify main function via an option
+    if (not fnlist):
+        print ("No functions in input.")
+        return
+    main = fnlist.pop(-1)
+
+    # For now, hard-code opset imports.
+    # TODO: extract opset imports from translated IR
+    graph = main.to_graph_proto()
+    model = onnx.helper.make_model(
+        graph,
+        functions = [f.to_function_proto() for f in fnlist],
+        producer_name='p2o',
+        opset_imports=[onnx.helper.make_opsetid("", 15)])
+
+    # TODO: add options for user to specify whether to check generated model
+    # model = onnx.shape_inference.infer_shapes(model)
+    # onnx.checker.check_model(model)
+
+    onnx.save(model, output_onnx_file)
+
 if __name__ == '__main__':
-    for i in range(1, len(sys.argv)):
-        convert(sys.argv[i])
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', help='Translate input to a single ModelProto', action='store_true')
+    parser.add_argument('rest', nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    for input_file in args.rest:
+        if args.model:
+            to_single_model_proto(input_file)
+        else:
+            convert(input_file)
