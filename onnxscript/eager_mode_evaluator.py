bdc98fab2a7858eac28bdadf870450fc3c962d59
Liqun Fu <liqfu@microsoft.com>
2022-03-22 17:03:17 -0700

    update with reviewers' comments
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnxscript/eager_mode_evaluator.py
+++ onnxscript/eager_mode_evaluator.py
@@ -1,36 +1,43 @@
+import numbers
+import functools
 import numpy as np
+import typing
+
 import onnx
 from onnxruntime import InferenceSession
-from onnx import ValueInfoProto, TensorProto
+from onnx import ValueInfoProto
 from onnx import numpy_helper
 from onnx import AttributeProto
 import onnx.shape_inference
 from onnxscript.utils import convert_data_to_value_infos
-import numbers
-import functools
-import typing
 
 version = 15
-# domain = "onnx.ai"
 domain = ""
 
 
-def convert_to_tensor(v):
+def convert_to_tensor(v, k):
     if isinstance(v, np.ndarray):
         return numpy_helper.from_array(v)
     elif isinstance(v, list):
         return numpy_helper.from_array(np.array(v))
     elif isinstance(v, numbers.Number):
         return numpy_helper.from_array(np.array([v]))
-    elif not isinstance(v, onnx.TensorProto):
-        raise ValueError("attribute {attribute_name} must be convertable to TensorProto, got {type}", k, type(v))
+    elif isinstance(v, onnx.TensorProto):
+        return v
+    else:
+        raise ValueError("attribute {attribute_name} must be convertable to TensorProto, got {type}", k, type(v)) # noqa E501
 
 
-def convert_attributes_to_tensors_with_schema(attribute_dict, schema_attribute_dict):
+def convert_attributes_to_tensors_with_schema(
+    attribute_dict, schema_attribute_dict):
+    # Constant and ConstantLike are the 2 ops in onnx
+    # that take a tensor as attribute value.
+    # onnx-script tends to use a literal number for attribute.
+    # This methods is to make this scenario work.
     for k, v in attribute_dict.items():
         attribute_type = schema_attribute_dict[k].type
         if attribute_type == AttributeProto.TENSOR:
-            attribute_dict[k] = convert_to_tensor(v)
+            attribute_dict[k] = convert_to_tensor(v, k)
 
 
 def call(opname, domain, version, *args, **vargs):
@@ -50,9 +57,11 @@ def call(opname, domain, version, *args, **vargs):
         return vi
 
     output_value_infos = [make_value_info(name) for name in outputs]
-    graph_temp = onnx.helper.make_graph([node], "node_graph", input_value_infos, output_value_infos)
+    graph_temp = onnx.helper.make_graph(
+        [node], "node_graph", input_value_infos, output_value_infos)
     model_temp = onnx.helper.make_model(graph_temp)
-    model = onnx.shape_inference.infer_shapes(model_temp, check_type=True, strict_mode=True)
+    model = onnx.shape_inference.infer_shapes(
+        model_temp, check_type=True, strict_mode=True)
     sess = InferenceSession(model.SerializeToString())
 
     session_run_input = {}
@@ -61,7 +70,7 @@ def call(opname, domain, version, *args, **vargs):
             session_run_input[input] = arg
         else:
             session_run_input[input] = np.array([arg], dtype=np.float32)
-    
+
     got = sess.run(None, session_run_input)
     return got[0] if len(got) == 1 else got
 
