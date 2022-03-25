558beba712c08d8c075679e5e3e8fbfad212048c
liqun Fu <liqfu@microsoft.com>
2022-03-25 14:45:56 -0700

    add debug eager mode (#15)

--- onnxscript/utils.py
+++ onnxscript/utils.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
 from typing import Any
+import onnx
+from onnx import TensorProto
 
 # TODO: enable invocation of ORT kernels
 
@@ -30,3 +33,22 @@ def call_ort(opname, *args, **kwds):
     model = Model("todo")
     ort_args = [make_ort_value(x) for x in args]
     return model(ort_args)
+
+
+def convert_arrays_to_value_infos(names, arr_list):
+    value_infos = []
+    for name, arr in zip(names, arr_list):
+        elem_type: TensorProto.DataType
+        shape: tuple
+        if isinstance(arr, np.ndarray):
+            elem_type = onnx.mapping.NP_TYPE_TO_TENSOR_TYPE[arr.dtype]
+            shape = arr.shape
+        else:
+            raise ValueError(f"cannot covert a {type(arr)} to value_info")
+
+        value_info = onnx.helper.make_tensor_value_info(
+            name=name,
+            elem_type=elem_type,
+            shape=shape)
+        value_infos.append(value_info)
+    return value_infos
