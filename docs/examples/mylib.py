15c61a552e655d08e74b39edf5eb1b66982ffb83
Ganesan Ramalingam <grama@microsoft.com>
2022-03-31 21:38:00 -0700

    Fix mylib example

--- docs/examples/mylib.py
+++ docs/examples/mylib.py
@@ -11,15 +11,14 @@ import onnxscript
 from onnxscript.onnx import opset15 as op
 
 # The domain/version of the library functions defined below
-__opset_domain__ = "com.mydomain"
-__opset_version__ = 1
+opset = CustomOpset('com.mydomain', 1)
 
 
-@onnxscript.func
+@script(opset)
 def l2norm(X):
     return op.ReduceSum(X * X, keepdims=1)
 
 
-@onnxscript.func
+@script(opset)
 def square_loss(X, Y):
     return l2norm(X - Y)
