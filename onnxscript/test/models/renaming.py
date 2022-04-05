f4490eb319219d2dcf2e4306a102d847ff705b03
Ganesan Ramalingam <grama@microsoft.com>
2022-04-05 12:40:01 -0700

    Ensure opset is specified

--- onnxscript/test/models/renaming.py
+++ onnxscript/test/models/renaming.py
@@ -1,20 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
+from onnxscript.onnx import opset15 as op
 from onnxscript.onnx_types import FLOAT
 
 # same variable assigned multiple times
 
 
 def renaming(A: FLOAT["N"]) -> FLOAT["N"]:
-    T = oxs.Abs(A)
-    T = oxs.Neg(A)
+    T = op.Abs(A)
+    T = op.Neg(A)
     return T
 
 # clash between generated-name and pre-existing name
 
 
 def renaming2(A: FLOAT["N"]) -> FLOAT["N"]:
-    T_0 = oxs.Relu(A)
-    T = oxs.Abs(A)
-    T = oxs.Neg(A)
+    T_0 = op.Relu(A)
+    T = op.Abs(A)
+    T = op.Neg(A)
     return T
--- onnxscript/test/models/subfunction.py
+++ onnxscript/test/models/subfunction.py
@@ -9,23 +9,23 @@ opset = CustomOpset('this', 1)
 
 @script(opset)
 def MySelu(X: FLOAT[None], alpha: FLOAT[1], gamma: FLOAT[1]) -> FLOAT[None]:
-    neg = gamma * (alpha * oxs.Exp(X) - alpha)
+    neg = gamma * (alpha * op.Exp(X) - alpha)
     pos = gamma * X
-    return oxs.Where(X <= 0, neg, pos)
+    return op.Where(X <= 0, neg, pos)
 
 @script(opset)
 def MyElu(X: FLOAT[None], beta: FLOAT[1]) -> FLOAT[None]:
-    alpha = oxs.Constant(value_float=1.)
+    alpha = op.Constant(value_float=1.)
     return MySelu(X, alpha, beta)
 
 @script(opset)
 def MyEluB(X: FLOAT[None], beta: FLOAT[1]) -> FLOAT[None]:
-    alpha = oxs.Constant(value_float=1.)
+    alpha = op.Constant(value_float=1.)
     res = MySelu(X, alpha, beta)
     return res
 
 @script(opset)
 def MyEluC(X: FLOAT[None], beta: FLOAT[1]) -> FLOAT[None]:
-    alpha = oxs.Constant(value_float=1.)
-    res = oxs.Identity(MySelu(X, alpha, beta))
+    alpha = op.Constant(value_float=1.)
+    res = op.Identity(MySelu(X, alpha, beta))
     return res
