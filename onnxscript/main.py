27065f8df45c9cb96f32b519180acaa2d89a3147
Ganesan Ramalingam <grama@microsoft.com>
2022-04-08 16:54:07 -0700

    flake8 warning

--- onnxscript/main.py
+++ onnxscript/main.py
@@ -45,7 +45,9 @@ class OnnxFunction:
 
 def script(opset=None):
     if (opset is not None) and (not isinstance(opset, values.Opset)):
-        raise TypeError("Script parameter must be an opset. Did you use @script instead of @script()?")
+        raise TypeError(
+            "Script parameter must be an opset. Did you use @script instead of @script()?")
+
     def transform(f):
         return OnnxFunction(f, opset)
     return transform
