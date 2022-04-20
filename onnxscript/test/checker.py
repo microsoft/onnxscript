7eb0f867c2e7b7b07b6dbfbbd203939d968cbffd
Ganesan Ramalingam <grama@microsoft.com>
2022-04-19 18:08:55 -0700

    Details for subgraph matching

--- onnxscript/test/checker.py
+++ onnxscript/test/checker.py
@@ -1,3 +1,4 @@
+from platform import node
 import onnx
 
 
@@ -46,6 +47,9 @@ def same_attrs(attrs1, attrs2, graph_equality):
             return False
     return True
 
+# Return the name of an input/output of a function or graph
+def ioname(x):
+    return x.name if isinstance(x, onnx.ValueInfoProto) else x
 
 def isomorphic(fn1: onnx.FunctionProto, fn2: onnx.FunctionProto):
     '''
@@ -57,10 +61,8 @@ def isomorphic(fn1: onnx.FunctionProto, fn2: onnx.FunctionProto):
     # Ok for function names/domain to be different.
 
     # Attribute parameters and inputs must be same for both:
-    if (fn1.input != fn2.input):
-        return False
-    if (fn1.attribute != fn2.attribute):
-        return False
+    if (fn1.input != fn2.input): return False
+    if (fn1.attribute != fn2.attribute): return False
 
     # Opset imports must be same (but possibly in different order):
 
@@ -69,77 +71,99 @@ def isomorphic(fn1: onnx.FunctionProto, fn2: onnx.FunctionProto):
         # TODO: assuming each domain has only one entry in a valid FunctionProto
         return {entry.domain: entry.version for entry in f.opset_import}
 
-    if (imports(fn1) != imports(fn2)):
-        return False
+    if (imports(fn1) != imports(fn2)): return False
 
     # Now do a specific form of isomorphism check: Both must compute the same
     # set of operations, possibly in different order as long as they respect
     # the topological-sort order requirement. The two may use different names
     # for intermediate-values, as long as the computation is the same.
 
-    if len(fn1.node) != len(fn2.node):
-        return False
+    if len(fn1.node) != len(fn2.node): return False
 
+    # Compute a map from variables v to their definition-sites.
+    # A definition-site (n, i) indicates the i-th output of n-th node
+    # The special value (-1, i) is used to indicate the i-th input of a function/graph
     def defmap(f):
-        return {x: (ni, xi) for ni, n in enumerate(f.node)
-                for xi, x in enumerate(n.output)}
+        result = {}
+        for (i, x) in enumerate(f.input):
+            result[ioname(x)] = (-1, i)
+        for ni, n in enumerate(f.node):
+            for xi, x in enumerate(n.output):
+                result[x] = (ni, xi) 
+        return result
 
+    outer_scopes = []
     fn1map = defmap(fn1)
     fn2map = defmap(fn2)
+    node_mapping = {}
+    delayed_checks = []
 
     # Check that fn1 computes the same value for var1 as fn2 computes for var2:
     def same_value(var1, var2):
-        # Inputs must match each other:
-        if var1 in fn1.input or var2 in fn2.input:
-            return var2 == var2
-        if var2 in fn2.input:
-            return False
-
-        # Computed values must be computed by isomorphic nodes:
         if (var1 not in fn1map or var2 not in fn2map):
-            return False  # Undefined variables
-
+            # Both variables must be in same scope:
+            if (var1 in fn1map) or (var2 in fn2map) or (not outer_scopes): return False
+            # For variables in outer-scopes, delay check until later
+            delayed_checks.append(var1, var2)
+            return True
         (node1, index1) = fn1map[var1]
         (node2, index2) = fn2map[var2]
-
         return (index1 == index2) and same_node(node1, node2)
 
-    node_mapping = {}
-
     def same_node(n1, n2):
+        if (n1 == -1) and (n2 == -1): return True # Both are inputs
+        if (n1 == -1) or (n2 == -1): return False # Only one is input
         nonlocal node_mapping
         if (n1 in node_mapping):
             return node_mapping[n1] == n2
         node1 = fn1.node[n1]
         node2 = fn2.node[n2]
-        if node1.op_type != node2.op_type:
-            return False
-        if node1.domain != node2.domain:
-            return False
+        if node1.op_type != node2.op_type: return False
+        if node1.domain != node2.domain: return False
         # check attrs
-        if not same_attrs(node1.attribute, node2.attribute, None):
-            return False
+        if not same_attrs(node1.attribute, node2.attribute, same_sub_graph): return False
 
         # Nodes represent same computation. Cache the comparison result.
         node_mapping[n1] = n2
         return True
 
     def same_sub_graph(g1, g2):
-        raise NotImplemented("Sub graph comparison")
+        if len(g1.input) != len(g2.input): return False
+        # TODO: check types
+        if g1.HasField("initializer") or g2.HasField("initializer"): return False # TODO
+        if g1.HasField("sparse_initializer") or g2.HasField("sparse_initializer"): return False # TODO
+        # Save information about outer scope TODO: delayed_checks?
+        nonlocal fn1map, fn2map, node_mapping
+        outer_scopes.insert(0, (fn1map, fn2map, node_mapping))
+        fn1map = defmap(g1)
+        fn2map = defmap(g2)
+        node_mapping = {}
 
-    # Check that both functions compute the same value for all outputs:
-    if len(fn1.output) != len(fn2.output):
-        return False
-    # For now, we allow the function outputs to have different names.
-    for x, y in zip(fn1.output, fn2.output):
-        if not same_value(x, y):
-            return False
+        if not same_outputs(g1.output, g2.output): return False
+        # TODO completeness tests!
+     
+        # Restore information about outer scope
+        fn1map, fn2map, node_mapping = outer_scopes.pop(0)
+        temp = delayed_checks
+        delayed_checks = []
+        for (v1, v2) in temp:
+            if not same_value(v1, v2): return False
+        return True
+    
+    def same_outputs(list1, list2):
+        # Check that both functions compute the same value for all outputs:
+        if len(fn1.output) != len(fn2.output): return False
+
+        # For now, we allow the function outputs to have different names.
+        for x, y in zip(fn1.output, fn2.output):
+            if not same_value(ioname(x), ioname(y)):
+                return False
+
+    if not same_outputs(fn1.output, fn2.output): return False
 
     # We do not allow for unused values in the function, which are
     # hard to handle in an isomorphism check.
-    if len(node_mapping) != len(fn1.node):
-        return False
-    if len(set(node_mapping.values())) != len(fn2.node):
-        return False
+    if len(node_mapping) != len(fn1.node): return False
+    if len(set(node_mapping.values())) != len(fn2.node): return False
 
     return True
