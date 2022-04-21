23b164943262fc0dbdd9416e51665cc24108e5aa
Ganesan Ramalingam <grama@microsoft.com>
2022-04-21 11:11:48 -0700

    Refactor into a class

--- onnxscript/test/checker.py
+++ onnxscript/test/checker.py
@@ -56,6 +56,89 @@ def same_attrs(attrs1, attrs2, graph_equality):
 def ioname(x):
     return x.name if isinstance(x, onnx.ValueInfoProto) else x
 
+class Matcher:
+    '''
+    An isomorphism matcher for two functions or two graphs.
+    '''
+
+    def __init__(self, fg1, fg2, outer_scope) -> None:
+
+        def defmap(f):
+            '''
+            Compute a map from variables v to their definition-sites.
+            A definition-site (n, i) indicates the i-th output of n-th node
+            The special value (-1, i) is used to indicate the i-th input of a function/graph.
+            '''
+            result = {}
+            for (i, x) in enumerate(f.input):
+                result[ioname(x)] = (-1, i)
+            for ni, n in enumerate(f.node):
+                for xi, x in enumerate(n.output):
+                    result[x] = (ni, xi) 
+            return result
+
+        self.defmap1 = defmap(fg1)
+        self.defmap2 = defmap(fg2)
+        self.nodelist1 = fg1.node
+        self.nodelist2 = fg2.node
+        self.node_mapping = {}
+        self.outer_scope_checks = []
+        self.outer_scope = outer_scope
+
+    def same_value(self, var1, var2):
+        '''Match two variables (strings).'''
+        if (var1 not in self.defmap1 or var2 not in self.defmap2):
+            # If one of the variables is in current scope, or if there is no outer scope, fail
+            if (var1 in self.defmap1) or (var2 in self.defmap2) or (self.outer_scope is None): return False
+            # Both variables are in outer-scopes. Delay check until later
+            self.outer_scope_checks.append((var1, var2))
+            return True
+        (node1, index1) = self.defmap1[var1]
+        (node2, index2) = self.defmap2[var2]
+        return (index1 == index2) and self.same_node(node1, node2)
+
+    def same_node(self, n1, n2):
+        '''Match two node-indices. The special node-index -1 represents inputs.'''
+        if (n1 == -1) and (n2 == -1): return True # Both are inputs
+        if (n1 == -1) or (n2 == -1): return False # Only one is input
+        if (n1 in self.node_mapping):
+            return self.node_mapping[n1] == n2
+        node1 = self.nodelist1[n1]
+        node2 = self.nodelist2[n2]
+        if node1.op_type != node2.op_type: return False
+        if node1.domain != node2.domain: return False
+        # check attrs
+        if not same_attrs(node1.attribute, node2.attribute, self.same_sub_graph): return False
+        if not self.same_value_list(node1.input, node2.input): return False
+
+        # Nodes represent same computation. Cache the comparison result.
+        self.node_mapping[n1] = n2
+        return True
+
+    def same_sub_graph(self, g1, g2):
+        if len(g1.input) != len(g2.input): return False
+        # TODO: check types
+        if g1.initializer or g2.initializer: return False # TODO
+        if g1.sparse_initializer or g2.sparse_initializer: return False # TODO
+
+        sub_graph_matcher = Matcher(g1, g2, self)
+
+        if not sub_graph_matcher.same_value_list(g1.output, g2.output): return False
+        # TODO completeness tests!
+     
+        for (v1, v2) in sub_graph_matcher.outer_scope_checks:
+            if not self.same_value(v1, v2): return False
+        return True
+    
+    def same_value_list(self, list1, list2):
+        '''Match two lists of variables (either a string or ValueInfoProto)'''
+        if len(list1) != len(list2): return False
+        for x, y in zip(list1, list2):
+            if not self.same_value(ioname(x), ioname(y)):
+                return False
+        
+        return True
+        
 def isomorphic(fn1: onnx.FunctionProto, fn2: onnx.FunctionProto):
     '''
     Checks that two function bodies are isomorphic.
@@ -85,96 +168,12 @@ def isomorphic(fn1: onnx.FunctionProto, fn2: onnx.FunctionProto):
 
     if len(fn1.node) != len(fn2.node): return False
 
-    # Compute a map from variables v to their definition-sites.
-    # A definition-site (n, i) indicates the i-th output of n-th node
-    # The special value (-1, i) is used to indicate the i-th input of a function/graph
-    def defmap(f):
-        result = {}
-        for (i, x) in enumerate(f.input):
-            result[ioname(x)] = (-1, i)
-        for ni, n in enumerate(f.node):
-            for xi, x in enumerate(n.output):
-                result[x] = (ni, xi) 
-        return result
-
-    class Scope:
-        def __init__(self, fg1, fg2, outer_scope) -> None:
-            self.fn1map = defmap(fg1)
-            self.fn2map = defmap(fg2)
-            self.nodelist1 = fg1.node
-            self.nodelist2 = fg2.node
-            self.node_mapping = {}
-            self.delayed_checks = []
-            self.outer_scope = outer_scope
-
-    scope = Scope(fn1, fn2, None)
-
-    # Check that fn1 computes the same value for var1 as fn2 computes for var2:
-    def same_value(var1, var2):
-        if (var1 not in scope.fn1map or var2 not in scope.fn2map):
-            # If one of the variables is in current scope, or if there is no outer scope, fail
-            if (var1 in scope.fn1map) or (var2 in scope.fn2map) or (scope.outer_scope is None): return False
-            # For variables in outer-scopes, delay check until later
-            scope.delayed_checks.append((var1, var2))
-            return True
-        (node1, index1) = scope.fn1map[var1]
-        (node2, index2) = scope.fn2map[var2]
-        return (index1 == index2) and same_node(node1, node2)
-
-    def same_node(n1, n2):
-        if (n1 == -1) and (n2 == -1): return True # Both are inputs
-        if (n1 == -1) or (n2 == -1): return False # Only one is input
-        if (n1 in scope.node_mapping):
-            return scope.node_mapping[n1] == n2
-        node1 = scope.nodelist1[n1]
-        node2 = scope.nodelist2[n2]
-        if node1.op_type != node2.op_type: return False
-        if node1.domain != node2.domain: return False
-        # check attrs
-        if not same_attrs(node1.attribute, node2.attribute, same_sub_graph): return False
-        if not same_outputs(node1.input, node2.input): return False # TODO: fix name 'same_outputs'
-
-        # Nodes represent same computation. Cache the comparison result.
-        scope.node_mapping[n1] = n2
-        return True
-
-    def same_sub_graph(g1, g2):
-        if len(g1.input) != len(g2.input): return False
-        # TODO: check types
-        if g1.initializer or g2.initializer: return False # TODO
-        if g1.sparse_initializer or g2.sparse_initializer: return False # TODO
-
-        # Save information about outer scope TODO: delayed_checks?
-        nonlocal scope
-        scope = Scope(g1, g2, scope)
-
-        if not same_outputs(g1.output, g2.output): return False
-        # TODO completeness tests!
-     
-        # Restore information about outer scope
-        outer_scope_checks = scope.delayed_checks
-        scope = scope.outer_scope
-
-        for (v1, v2) in outer_scope_checks:
-            if not same_value(v1, v2): return False
-        return True
-    
-    def same_outputs(list1, list2):
-        # Check that both functions compute the same value for all outputs:
-        if len(list1) != len(list2): return False
-
-        # For now, we allow the function outputs to have different names.
-        for x, y in zip(list1, list2):
-            if not same_value(ioname(x), ioname(y)):
-                return False
-        
-        return True
-
-    if not same_outputs(fn1.output, fn2.output): return False
+    matcher = Matcher(fn1, fn2, None)
+    if not matcher.same_value_list(fn1.output, fn2.output): return False
 
     # We do not allow for unused values in the function, which are
     # hard to handle in an isomorphism check.
-    if len(scope.node_mapping) != len(fn1.node): return False
-    if len(set(scope.node_mapping.values())) != len(fn2.node): return False
+    if len(matcher.node_mapping) != len(fn1.node): return False
+    if len(set(matcher.node_mapping.values())) != len(fn2.node): return False
 
     return True
--- onnxscript/test/testutils.py
+++ onnxscript/test/testutils.py
@@ -6,22 +6,14 @@ from onnxscript import OnnxFunction
 from .checker import isomorphic
 import onnx.helper
 
-def print_ir_function(f):
-    print(str(f))
-    for s in f.stmts:
-        for attr in s.attrs:
-            if attr.attr_proto.HasField("g"):
-                print(onnx.helper.printable_graph(attr.attr_proto.g))
-
 def function_proto(f):
     if isinstance(f, FunctionProto):
         return f
     if isinstance(f, OnnxFunction):
-        print_ir_function(f.function_ir)
         return f.to_function_proto()
     raise TypeError(f"Cannot convert {type(f)} to FunctionProto")
 
 
 class TestBase(unittest.TestCase):
     def assertSame(self, fn1, fn2):
-        self.assertTrue(isomorphic(function_proto(fn1), function_proto(fn1)))
+        self.assertTrue(isomorphic(function_proto(fn1), function_proto(fn2)))
