0b9a694d24fd3b17c2eb6cc9dc433212ee4bcf23
Ganesan Ramalingam <grama@microsoft.com>
2022-05-10 12:47:06 -0700

    Complete second use-case

--- onnxscript/test/models/opt_output.py
+++ onnxscript/test/models/opt_output.py
@@ -1,3 +1,4 @@
+from tkinter import Y
 from onnxscript import script
 from onnxscript.onnx import opset15 as op
 # from onnxscript.onnx_types import FLOAT
@@ -12,8 +13,10 @@ from onnxscript.onnx import opset15 as op
 # is required in OnnxScript.
 # Example: LayerNormalization
 
-# Toy version of LayerNormalization-like example:
-# Second output is optional. But in the function-definition we always return it.
+# Here is a toy version of a LayerNormalization-like example:
+# Here, the second output is optional. But this impacts only the caller's
+# code, and not the function-definition. In the function definition, we always
+# return it.
 @script()
 def MeanDiff (x) :
     mean = op.ReduceMean(x)
@@ -22,8 +25,38 @@ def MeanDiff (x) :
     # It is the inliner's responsibility to handle this correctly.
     return (diff, mean)
 
+# A call to a function with an optional output:
+@script()
+def MeanDiffCaller(x):
+    diff, _ = MeanDiff(x)
+    return diff * diff
+
 # Use Case 2: In this scenario, the inputs/attributes determine which outputs
 # are computed. In particular, the op may return different numbers of outputs
 # in different situations. An example is the BatchNormalization op. This
 # introduces some challenges (in situations where use-case 1's approach is
 # not feasible).
+
+# BatchNorm can still be defined as a function, as in use-case 1, by computing
+# the extra outputs even in the case where it is unused. This can lead to inefficiency
+# due to redundant computation, but that can be eliminated by optimization subsequent
+# to inlining.
+
+# However, the question here is whether it is useful to support examples such as
+# the one below:
+
+@script()
+def ConditionalOptOutput (x, y, flag: bool):
+    if flag:
+        z1 = x + y
+        # Challenge: Should we support this kind of usage?
+        # If so, how do we handle this?
+        # Specifically, the ONNX representation does not enable us to capture
+        # conditionals that return different number of outputs in different branches.
+        # We would need some way to represent a dummy "None" value.
+        z2 = None
+    else:
+        z1 = x + y
+        z2 = x / y
+    return z1, z2
+
