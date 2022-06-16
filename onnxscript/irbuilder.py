fd52cd3f5312c2ab0ad6097b50c4e7946e324060
xadupre <xadupre@microsoft.com>
2022-06-16 16:15:14 +0200

    lint
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnxscript/irbuilder.py
+++ onnxscript/irbuilder.py
@@ -273,12 +273,12 @@ class Function:
         opset_imports = [onnx.helper.make_opsetid(domain, version)
                          for domain, version in opsets.items()]
         if len(self.attr_protos) == 0:
-            inputs=[x.name for x in self.inputs]
+            inputs = [x.name for x in self.inputs]
         else:
             if hasattr(onnx.FunctionProto, 'attribute_proto'):
-                inputs=[x.name for x in self.inputs]
+                inputs = [x.name for x in self.inputs]
             else:
-                inputs=[x.name for x in self.inputs] + [a.name for a in self.attrs]
+                inputs = [x.name for x in self.inputs] + [a.name for a in self.attrs]
         f = helper.make_function(
             self.domain,
             self.name,
