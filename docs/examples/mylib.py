37e00730bb2321ae9cfb932a1fcef30fccb7d7f9
xadupre <xadupre@microsoft.com>
2022-06-27 12:37:38 +0200

    complete renaming
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- docs/examples/mylib.py
+++ docs/examples/mylib.py
@@ -7,7 +7,7 @@ of multiple functions.
 """
 
 from onnxscript import script, export_onnx_lib
-from onnxscript.onnx import opset15 as op
+from onnxscript.onnx_opset import opset15 as op
 from onnxscript.values import Opset
 
 # The domain/version of the library functions defined below
