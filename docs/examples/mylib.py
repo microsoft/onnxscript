5e19155b24b455de8daf40aad9e13f7c0a1f4a5a
Ganesan Ramalingam <grama@microsoft.com>
2022-06-29 16:54:00 -0700

    Update examples

--- docs/examples/mylib.py
+++ docs/examples/mylib.py
@@ -1,19 +1,23 @@
 """
-Define an onnx-script library of multiple functions
+Exporting a library of multiple functions
 ===================================================
 
 The examples below show how we can define a library consisting
 of multiple functions.
+
+**This is preliminary. Proto extensions required to fully support LibProto.**
 """
 
 from onnxscript import script, export_onnx_lib
 from onnxscript.onnx import opset15 as op
 from onnxscript.values import Opset
 
+#%%
 # The domain/version of the library functions defined below
 opset = Opset('com.mydomain', 1)
 
-
+#%%
+# The definitions of the functions:
 @script(opset)
 def l2norm(X):
     return op.ReduceSum(X * X, keepdims=1)
@@ -23,5 +27,6 @@ def l2norm(X):
 def square_loss(X, Y):
     return l2norm(X - Y)
 
+#%%
 # Export the functions as an ONNX library.
 export_onnx_lib([l2norm, square_loss], "mylib.onnxlib")
