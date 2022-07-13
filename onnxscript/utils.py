1c872904f596ddc8c0f33a6936b03fa1c314d0cf
Ganesan Ramalingam <grama@microsoft.com>
2022-07-13 11:14:07 -0700

    Fix white space

--- onnxscript/utils.py
+++ onnxscript/utils.py
@@ -13,7 +13,9 @@ from onnx import TensorProto, ValueInfoProto, ModelProto, FunctionProto
 try:
     from onnx.printer import to_text as proto_to_text
 except ImportError:
-    def proto_to_text (x): return "<print utility unavailable>"
+    def proto_to_text(
+        x): return "<print utility unavailable>"
+
 
 def map_pytype_to_schema_allowed_dtype(onnx_schema_types, dtype):
     # ONNX TensorProto data type is a supper set of python dtype.
@@ -37,35 +39,41 @@ def convert_arrays_to_value_infos(names, arr_list, op_schema_formal_parameter=No
         if isinstance(arr, list):
             # sequence, assuming it is a float sequence
             # list should be replace by another container retaining the type information
-            nparray = np.asarray(arr)
+            nparray = np.asarray(
+                arr)
             if len(arr) == 0:
-                nparray = nparray.astype(np.float32)
+                nparray = nparray.astype(
+                    np.float32)
             if op_schema_formal_parameter and len(op_schema_formal_parameter) > i:
                 elem_type = onnx.mapping.NP_TYPE_TO_TENSOR_TYPE[
-                        map_pytype_to_schema_allowed_dtype(
-                            op_schema_formal_parameter[i].types, nparray.dtype)]
+                    map_pytype_to_schema_allowed_dtype(
+                        op_schema_formal_parameter[i].types, nparray.dtype)]
             else:
                 elem_type = onnx.mapping.NP_TYPE_TO_TENSOR_TYPE[nparray.dtype]
             info = onnx.helper.make_tensor_sequence_value_info(
                 name=name, elem_type=elem_type, shape=None)
-            value_infos.append(info)
+            value_infos.append(
+                info)
             continue
 
         if isinstance(arr, np.ndarray):
             elem_type = onnx.mapping.NP_TYPE_TO_TENSOR_TYPE[arr.dtype]
             shape = arr.shape
         elif isinstance(arr, numbers.Number):
-            nparray = np.array(arr)
+            nparray = np.array(
+                arr)
             elem_type = onnx.mapping.NP_TYPE_TO_TENSOR_TYPE[nparray.dtype]
             shape = nparray.shape
         else:
-            raise ValueError(f"cannot convert a {type(arr)} to value_info")
+            raise ValueError(
+                f"cannot convert a {type(arr)} to value_info")
 
         value_info = onnx.helper.make_tensor_value_info(
             name=name,
             elem_type=elem_type,
             shape=shape)
-        value_infos.append(value_info)
+        value_infos.append(
+            value_info)
     return value_infos
 
 
@@ -91,8 +99,10 @@ def make_model_from_function_proto(
         ModelProto
     """
 
-    input_names = [vi.name for vi in input_value_infos]
-    output_names = [vi.name for vi in output_value_infos]
+    input_names = [
+        vi.name for vi in input_value_infos]
+    output_names = [
+        vi.name for vi in output_value_infos]
     node = onnx.helper.make_node(
         function_proto.name, input_names, output_names,
         domain=function_proto.domain,
@@ -107,7 +117,8 @@ def make_model_from_function_proto(
             onnx.helper.make_opsetid(function_proto.domain, function_opset_version)]
     model = onnx.helper.make_model(
         graph,
-        functions=[function_proto],
+        functions=[
+            function_proto],
         producer_name='onnx-script',
         opset_imports=model_proto_opset)
     return model
