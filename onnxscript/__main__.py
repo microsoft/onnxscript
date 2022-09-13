725cb2c00ea37737d00d4e0c428bc22588a44f82
sdpython <xavier.dupre@gmail.com>
2022-09-13 14:45:07 +0200

    use click instead of fire
    Signed-off-by: sdpython <xavier.dupre@gmail.com>

--- onnxscript/__main__.py
+++ onnxscript/__main__.py
@@ -3,22 +3,104 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
+import os
 import sys
+from typing import Optional
+import onnx
+import onnx.helper as helper
+import click
+from onnxscript.converter import Converter
 from onnxscript.backend.onnx_export import export2python
 
+@click.group()
+def cli():
+    pass
 
-def onnx2script(filename, use_operators=False, rename=False):
+
+def convert_file(script):
+    converter = Converter()
+    return converter.convert_file(script)
+
+
+def to_single_model_proto(model, input_py_file: str, output_onnx_file: Optional[str] = None):
+    if (not output_onnx_file):
+        prefix, ext = os.path.splitext(input_py_file)
+        output_onnx_file = prefix + ".onnx"
+
+    fnlist = convert_file(input_py_file)
+
+    if (not fnlist):
+        print("No functions in input.")
+        return
+
+    if model:
+        # treat last function as main graph for model
+        main = fnlist.pop(-1)
+        graph = main.to_graph_proto()
+    else:
+        # For now, we use a ModelProto with an empty graph to represent a library
+        graph = onnx.GraphProto()
+
+    # For now, hard-code opset imports.
+    # TODO: extract opset imports from translated IR
+
+    model = onnx.helper.make_model(
+        graph,
+        functions=[f.to_function_proto() for f in fnlist],
+        producer_name='p2o',
+        opset_imports=[onnx.helper.make_opsetid("", 15)])
+
+    # TODO: add options for user to specify whether to check generated model
+    # model = onnx.shape_inference.infer_shapes(model)
+    # onnx.checker.check_model(model)
+
+    onnx.save(model, output_onnx_file)
+
+
+def to_text(input_py_file: str):
+    def print_ir_function(f):
+        print(str(f))
+        for s in f.stmts:
+            for attr in s.attrs:
+                if attr.attr_proto.HasField("g"):
+                    print(helper.printable_graph(attr.attr_proto.g))
+
+    fnlist = convert_file(input_py_file)
+    for f in fnlist:
+        print_ir_function(f)
+
+
+@cli.command()
+@click.option('--fmt', type=click.Choice(['text', 'model', 'lib'], case_sensitive=False),
+              help="Translate input to a single ModelProto ('model'), "
+                   "into a LibProto ('lib'), "
+                   "or into text 'text').")
+@click.option('name', '--name', envvar='PATHS', multiple=True, type=click.Path(), 
+              help="File or files to convert.")
+def translate(fmt="text", name=None):
+    """Translate a file or many files into a ModelProto, a LibProto or text."""
+    if fmt == "text":
+        for name in name:
+            to_text(name)
+    else:
+        for name in name:
+            to_single_model_proto(fmt == "model", name)
+
+
+@cli.command()
+@click.option('name', '--name', envvar='PATHS', multiple=False, type=click.Path(), 
+              help="filename to convert")
+@click.option("--op", is_flag=True, default=False,
+              help="converts a numerical operator into op.Add (False) or keep it (True)")
+@click.option("--rename", is_flag=True, default=False,
+              help="to use shorter variable name")
+def onnx2script(name, op=False, rename=False):
     """Exports an onnx graph to a script in following onnx-script syntax.
     The result is printed on the standard output.
-
-    :param filename: onnx file to convert
-    :param use_operators: converts a numerical operator into op.Add (False) or keep it (True)
-    :param rename: to use shorter name
     """
-    code = export2python(filename, use_operators=use_operators, rename=rename)
+    code = export2python(name, use_operators=op, rename=rename)
     print(code)
 
 
 if __name__ == "__main__":
-    import fire
-    fire.Fire(dict(onnx2script=onnx2script), sys.argv[1:])
+    cli()
