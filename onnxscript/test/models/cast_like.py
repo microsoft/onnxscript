d99e4851cfcd6f43e1d92215dab1e40d72d48106
Ganesan Ramalingam <grama@microsoft.com>
2022-09-14 11:34:04 -0700

    Update CastLike tests

--- onnxscript/test/models/cast_like.py
+++ onnxscript/test/models/cast_like.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------
 
 from onnxscript import script
-from onnxscript.onnx_opset import opset15
+from onnxscript.onnx_opset import opset15 as op
 from onnxscript.onnx_types import FLOAT, BOOL
 
 
@@ -12,36 +12,57 @@ from onnxscript.onnx_types import FLOAT, BOOL
 def inc_right(A: FLOAT[...]) -> FLOAT[...]:
     return A + 1
 
+@script()
+def inc_right_expanded(A: FLOAT[...]) -> FLOAT[...]:
+    return A + op.CastLike(1, A)
 
 @script()
 def inc_left(A: FLOAT[...]) -> FLOAT[...]:
     return 1 + A
 
+@script()
+def inc_left_expanded(A: FLOAT[...]) -> FLOAT[...]:
+    return op.CastLike(1, A) + A
 
 @script()
 def cmp_zero_right(A: FLOAT[...]) -> BOOL[...]:
     return A == 0
 
+@script()
+def cmp_zero_right_expanded(A: FLOAT[...]) -> BOOL[...]:
+    return A == op.CastLike(0, A)
 
 @script()
 def cmp_zero_mright(A: FLOAT[...]) -> BOOL[...]:
     return A == -11
 
+@script()
+def cmp_zero_mright_expanded(A: FLOAT[...]) -> BOOL[...]:
+    return A == op.CastLike(-11, A)
 
 @script()
 def cmp_zero_left(A: FLOAT[...]) -> BOOL[...]:
     return 0 == A
 
+@script()
+def cmp_zero_left_expanded(A: FLOAT[...]) -> BOOL[...]:
+    return op.CastLike(0, A) == A
 
 @script()
 def div_right(A: FLOAT[...]) -> FLOAT[...]:
     return A / 2
 
+@script()
+def div_right_expanded(A: FLOAT[...]) -> FLOAT[...]:
+    return A / op.CastLike(2, A)
 
 @script()
 def div_minus_right(A: FLOAT[...]) -> FLOAT[...]:
     return A / (-2)
 
+@script()
+def div_minus_right_expanded(A: FLOAT[...]) -> FLOAT[...]:
+    return A / op.CastLike(-2, A)
 
 # @script()
 # def div_minus_minus_right(A: FLOAT[...]) -> FLOAT[...]:
--- onnxscript/test/testutils.py
+++ onnxscript/test/testutils.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------
 
 import unittest
-from onnx import FunctionProto, GraphProto, parser
+from onnx import FunctionProto, GraphProto, ModelProto, parser
 from onnxscript import OnnxFunction
 from onnxscript.test.checker import isomorphic
 
@@ -26,6 +26,16 @@ def graph_proto(g):
         return parser.parse_graph(g)
     raise TypeError(f"Cannot convert {type(g)} to ModelProto")
 
+def to_function_or_graph(testcase):
+    if isinstance(testcase, FunctionProto):
+        return testcase
+    if isinstance(testcase, GraphProto):
+        return testcase
+    if isinstance(testcase, ModelProto):
+        return testcase.graph
+    if isinstance(testcase, OnnxFunction):
+        return testcase.to_function_proto()
+    raise TypeError(f"Cannot convert {type(testcase)} to FunctionProto or GraphProto")
 
 class TestBase(unittest.TestCase):
     def validate(self, fn):
@@ -33,7 +43,7 @@ class TestBase(unittest.TestCase):
         return fn.to_function_proto()
 
     def assertSame(self, fn1, fn2):
-        self.assertTrue(isomorphic(function_proto(fn1), function_proto(fn2)))
+        self.assertTrue(isomorphic(to_function_or_graph(fn1), to_function_or_graph(fn2)))
 
     def assertSameGraph(self, graph1, graph2):
         self.assertTrue(isomorphic(graph_proto(graph1), graph_proto(graph2)))
