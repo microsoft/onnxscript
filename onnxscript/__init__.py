d721a19f254c25aa4517945da5086208ae501f66
AllenTiTaiWang <titaiwang@microsoft.com>
2022-09-19 23:05:19 +0000

    input package name

--- onnxscript/__init__.py
+++ onnxscript/__init__.py
@@ -14,7 +14,8 @@ else:
     import importlib_metadata
 
 try:
-    __version__ = importlib_metadata.version(__package__)
+    # TODO: should we algin the folder name with pacakge name? It's onnxscript and onnx-script now. That way, we can use __package__ here.
+    __version__ = importlib_metadata.version("onnx-script")
 except Exception:  # nosec: allow bare except
     # If the package is not installed or the version lookup fails
     __version__ = None
