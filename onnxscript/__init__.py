1ded05899099519cd2e0b4f8bf700fbbdd1ac986
AllenTiTaiWang <titaiwang@microsoft.com>
2022-09-21 13:59:48 +0000

    test on changes

--- onnxscript/__init__.py
+++ onnxscript/__init__.py
@@ -10,12 +10,13 @@ from .backend.onnx_export import export2python as proto2python
 if sys.version_info[0:2] >= (3, 8):
     import importlib.metadata as importlib_metadata
 else:
-    # In Python 3.7, importlib.metadata is not available
+    # TODO: Remove this when Python 3.7 is deprecated
     import importlib_metadata
 
 try:
-    # TODO: should we algin the folder name with pacakge name? It's onnxscript and onnx-script now. That way, we can use __package__ here.
-    __version__ = importlib_metadata.version("onnx-script")
+    # TODO: should we algin the folder name with pacakge name? 
+    # It's onnxscript and onnx-script now. That way, we can use __package__ here.
+    __version__ = importlib_metadata.version(__package__)
 except Exception:  # nosec: allow bare except
     # If the package is not installed or the version lookup fails
     __version__ = None
