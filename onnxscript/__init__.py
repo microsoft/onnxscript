a48de872e8baf734153b6cd9e0dacb033d36fec8
Ti-Tai Wang <titaiwang@microsoft.com>
2022-09-23 09:52:29 -0700

    [Lint] Add Lint and move metadata from setup.py to pyproject.toml (#120)
    This PR basically migrates the lint and CI from ONNX to here.
    
    1. Add lint: black, isort, mypy, and flake8
        - flake8 remains basic setting as main branch.
        - auto-formatter will be introduced in the next PR (#126 )
    
    3. Add pyproject.toml (includes what was in requirements.txt, setup.cfg,
    and metadata in setup.py)
    - Avoiding parsing documents in setup.py can reduce bugs (version_str
    was -0.1-)
        - Put metadata all in one file (pyproject.toml )
    - benefit: toml becomes more important in python, check
    [here](https://peps.python.org/pep-0680/)
      
    4. Add lint CI to align coding style 
    - `Lint / Enforce style (pull_request)` CI utilizes style.sh to do the
    test
    - We can only make `Lint / Enforce style (pull_request)` as required
    after a reasonable transition time period
     
    6. Continue work (actually apply lint on files) on #126  
    
    fixes #105
    
    Co-authored-by: Ubuntu <titaiwang@titaiwanglinuxcpudev.y3zdd0j2xrqelnmezcgpqgmnte.jx.internal.cloudapp.net>

--- onnxscript/__init__.py
+++ onnxscript/__init__.py
@@ -3,9 +3,22 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
-__version__ = '0.1'
-
+import sys
 from .main import script, export_onnx_lib, OnnxFunction
 from .backend.onnx_export import export2python as proto2python
 
-__all__ = [script, export_onnx_lib, OnnxFunction, proto2python]
+if sys.version_info[0:2] >= (3, 8):
+    import importlib.metadata as importlib_metadata
+else:
+    # TODO: Remove this when Python 3.7 is deprecated
+    import importlib_metadata
+
+try:
+    # TODO: should we algin the folder name with package name?
+    # It's onnxscript and onnx-script now. That way, we can use __package__ here.
+    __version__ = importlib_metadata.version("onnx-script")
+except importlib_metadata.PackageNotFoundError:
+    __version__ = None
+
+
+__all__ = ["script", "export_onnx_lib", "OnnxFunction", "proto2python"]
---
+++ tools/style.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+set +o errexit
+set -o nounset
+
+
+cd "$(git rev-parse --show-toplevel)"
+
+err=0
+trap 'err=1' ERR
+
+echo -e "\n::group:: ===> check flake8..."
+flake8 onnxscript --config .flake8
+echo -e "::endgroup::"
+
+echo -e "\n::group:: ===> check isort..."
+isort onnxscript --color --diff --check
+echo -e "::endgroup::"
+
+echo -e "\n::group:: ===> check black format..."
+black onnxscript --color --diff --check
+echo -e "::endgroup::"
+
+git diff --exit-code
+
+test $err = 0 # Return non-zero if any command failed
