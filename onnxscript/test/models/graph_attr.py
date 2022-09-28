0ad0ad8c238f77a235e42639de13cf24097fef97
Ganesan Ramalingam <grama@microsoft.com>
2022-09-28 11:00:17 -0700

    Test case

--- onnxscript/test/models/graph_attr.py
+++ onnxscript/test/models/graph_attr.py
@@ -10,24 +10,26 @@ from onnxscript.onnx_types import FLOAT, BOOL, INT64
 
 import numpy as np
 
-# @script()
-# def CumulativeSum(X):
-#     @graphattr(parent=CumulativeSum)
-#     def Sum(sum_in, next):
-#         sum_out = sum_in + next
-#         scan_out = op.Identity(sum_out)
-#         return sum_out, scan_out
-#     all_sum, cumulative_sum = op.Scan (0, X, body=Sum, num_scan_inputs=1)
-#     return cumulative_sum
+@script()
+def CumulativeSum(X):
+    '''Test use of a nested-function as a graph-attribute, using the Scan operator.'''
+    @graphattr(parent=CumulativeSum)
+    def Sum(sum_in, next):
+        sum_out = sum_in + next
+        scan_out = op.Identity(sum_out)
+        return sum_out, scan_out
+    all_sum, cumulative_sum = op.Scan (0, X, body=Sum, num_scan_inputs=1)
+    return cumulative_sum
 
+model = CumulativeSum.to_model_proto()
 
 # X = np.array([1, 2, 3, 4, 5], dtype=np.int32)
-
 # Y = CumulativeSum(X)
 # print(Y)
 
 @script()
 def SumTo(N):
+    '''Test use of a nested-function as a graph-attribute, using the Loop operator.'''
     @graphattr(parent=SumTo)
     def LoopBody(i: INT64, cond: BOOL, sum_in: INT64):
         cond_out = op.Identity(cond)
