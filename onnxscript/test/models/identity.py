1b1cecbee0f8989c8945bce8e4bd5de01bd93d3f
G. Ramalingam <grama@microsoft.com>
2022-11-14 12:56:08 -0800

    Add support for returning input as output (#184)
    Add support so that graph-inputs can be returned as graph-outputs (which
    requires introducing an Identity op in ONNX).
    
    Co-authored-by: Justin Chu <justinchuby@users.noreply.github.com>

---
+++ onnxscript/test/models/identity.py
@@ -0,0 +1,45 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+# Test cases for automatic introduction of Identity (copy)
+
+from onnxscript import script
+from onnxscript.onnx_opset import opset15 as op
+from onnxscript.onnx_types import BOOL, FLOAT
+
+
+@script()
+def id1(A: FLOAT[...]) -> FLOAT[...]:
+    return A  # treat as op.Identity(A)
+
+@script()
+def id1_expanded(A: FLOAT[...]) -> FLOAT[...]:
+    return op.Identity(A)
+
+@script()
+def id2(A: FLOAT[...]) -> FLOAT[...]:
+    B = A
+    return B  # treat as op.Identity(B) == op.Identity(A)
+
+@script()
+def id2_expanded(A: FLOAT[...]) -> FLOAT[...]:
+    B = A
+    return op.Identity(B)
+
+@script()
+def control_flow_id1(A: FLOAT[...], flag: BOOL) -> FLOAT[...]:
+    if flag:
+        y = A  # treat as op.Identity(A)
+    else:
+        y = op.Abs(A)
+    return y
+
+@script()
+def control_flow_id1_expanded(A: FLOAT[...], flag: BOOL) -> FLOAT[...]:
+    if flag:
+        y = op.Identity(A)
+    else:
+        y = op.Abs(A)
+    return y
