6ce723e6f8ffa28b8ef328c783d88ac110b0169e
Justin Chu <justinchu@microsoft.com>
2022-11-21 05:29:11 +0000

    Simplify setuptools

--- setup.py
+++ setup.py
@@ -3,32 +3,7 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
-# -*- coding: utf-8 -*-
-
-import os
-
 import setuptools
 
-this = os.path.dirname(__file__)
 
-packages = setuptools.find_packages()
-assert packages
-
-README = os.path.join(os.getcwd(), "README.md")
-with open(README, encoding="utf-8") as f:
-    long_description = f.read()
-    start_pos = long_description.find("## Contributing")
-    if start_pos >= 0:
-        long_description = long_description[:start_pos]
-
-setuptools.setup(
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/onnx/onnx-script",
-    packages=packages,
-    include_package_data=True,
-    package_data={
-        "onnx-script": ["py.typed"],
-        "onnx": ["py.typed"],
-    },
-)
+setuptools.setup()
