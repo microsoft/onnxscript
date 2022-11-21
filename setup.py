c3c546923fee2f5ca65750c9a7b6ecb21c4964bb
Justin Chu <justinchu@microsoft.com>
2022-11-21 05:41:21 +0000

    Add note

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+"""Setup script for the onnxscript package.
 
+NOTE: Put all metadata in pyproject.toml.
+"""
 import setuptools
 
 setuptools.setup()
