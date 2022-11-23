fa8ce18734b6ec28395c79b889b6520a061f8288
Justin Chu <justinchu@microsoft.com>
2022-11-23 02:30:52 +0000

    feat(atenlib): Create sample functions and tests
    [ghstack-poisoned]

--- onnxscript/fuction_libs/torch_aten/ops/core.py
+++ onnxscript/fuction_libs/torch_aten/ops/core.py
@@ -1,16 +1,19 @@
 """All operators in torch.ops.aten.
 
 - No inplace operators.
+- All functions should not have the script() decorator. This is because
+    we want to delay the compilation of the function.
 """
-import onnxscript
-from onnxscript.onnx_opset import default_opset as op
-
-from beartype import Is
+from beartype.vale import Is
 from typing_extensions import Annotated
 
+from onnxscript.onnx_opset import default_opset as op
 
-def atenop(name, spec=None):
+
+def atenop(name):
     """A no-op decorator for torch.aten operators."""
+
+    del name
 
     def decorator(func):
         return func
@@ -20,9 +23,9 @@ def atenop(name, spec=None):
 
 # TODO: put this in nn
 @atenop("aten::relu6")
-def Relu6(X):
-    zero = op.CastLike(0, X)
-    return op.Max(X, zero)
+def Relu6(self):
+    zero = op.CastLike(0, self)
+    return op.Max(self, zero)
 
 
 @atenop("aten::selu")
@@ -30,12 +33,13 @@ def Selu(self):
     return op.Selu(self)
 
 
-@onnxscript.script()
 @atenop("aten::elu")
 def Elu(
     self,
-    alpha: Annotated[float, Is[lambda x: x == 1.0]] = 1.0,
+    alpha: float = 1.0,
     scale: Annotated[float, Is[lambda x: x == 1.0]] = 1.0,
-    input_scale: float = 1.0,
+    input_scale: Annotated[float, Is[lambda x: x == 1.0]] = 1.0,
 ):
+    # del scale
+    # del input_scale
     return op.Elu(self, alpha=alpha)
