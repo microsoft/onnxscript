0a250fd2e41d10df042cd6afbedd23ab8fcb57ce
xiaowuhu <xiaowuhu@microsoft.com>
2023-01-01 19:31:18 +0800

    Update ops_correctness_test.py

--- onnxscript/test/function_libs/torch_aten/ops_correctness_test.py
+++ onnxscript/test/function_libs/torch_aten/ops_correctness_test.py
@@ -355,8 +355,9 @@ EXPECTED_SKIPS_OR_FAILS = (
     xfail("sub", dtypes=BOOL_TYPES, reason="Sub is not defined on bool tensors"),
     xfail(
         "sum",
-        dtypes=except(torch.bfloat16, torch.int32, torch.int64, torch.double, torch.float32, torch.float16),
-        reason="Sum is not defined on bool tensors"),
+        dtypes=dtypes_except(torch.bfloat16, torch.int32, torch.int64, torch.double, torch.float32, torch.float16),
+        reason="Sum is not defined on bool tensors",
+    )
     xfail(
         "tan",
         dtypes=BOOL_TYPES + INT_TYPES,
