078471e68eede008919fec16c11806e763dc9f4d
Justin Chu <justinchuby@users.noreply.github.com>
2023-01-03 07:56:20 -0800

    feat(atenlib): use typevars for functions (#261)
    Stack from [ghstack](https://github.com/ezyang/ghstack) (oldest at
    bottom):
    * __->__ #261
    
    This change implements
    
    ```python
    TTensor = TypeVar("TTensor", bound=_TensorType)
    TFloat = TypeVar("TFloat", bound=_FloatType)
    TInt = TypeVar("TInt", bound=_IntType)
    TReal = TypeVar("TReal", bound=_RealType)
    ```
    
    and uses them to annotate the functions. This information will be used
    to generate symbolic functions which adds glue nodes and dispatches to
    the ONNX functions.
    
    In order for type checking to work, the generated ops need to use
    `TypeVar`s as well.
    
    - Also fix mypy ignore directives

--- onnxscript/function_libs/torch_aten/typing.py
+++ onnxscript/function_libs/torch_aten/typing.py
@@ -9,10 +9,59 @@ from __future__ import annotations
 
 from typing import TypeVar, Union
 
-from onnxscript import DOUBLE, FLOAT, FLOAT16, INT8, INT16, INT32, INT64
+from onnxscript import (
+    BFLOAT16,
+    BOOL,
+    COMPLEX64,
+    COMPLEX128,
+    DOUBLE,
+    FLOAT,
+    FLOAT16,
+    INT8,
+    INT16,
+    INT32,
+    INT64,
+    UINT8,
+    UINT16,
+    UINT32,
+    UINT64,
+)
 
-FloatType = Union[FLOAT16, FLOAT, DOUBLE]
-IntType = Union[INT8, INT16, INT32, INT64]
+_TensorType = Union[
+    BFLOAT16,
+    BOOL,
+    COMPLEX64,
+    COMPLEX128,
+    DOUBLE,
+    FLOAT,
+    FLOAT16,
+    INT8,
+    INT16,
+    INT32,
+    INT64,
+    UINT8,
+    UINT16,
+    UINT32,
+    UINT64,
+]
+_FloatType = Union[FLOAT16, FLOAT, DOUBLE]
+_IntType = Union[INT8, INT16, INT32, INT64]
+_RealType = Union[
+    BFLOAT16,
+    FLOAT16,
+    FLOAT,
+    DOUBLE,
+    INT8,
+    INT16,
+    INT32,
+    INT64,
+    UINT8,
+    UINT16,
+    UINT32,
+    UINT64,
+]
 
-TFloat = TypeVar("TFloat", bound=FloatType)
-TInt = TypeVar("TInt", bound=IntType)
+TTensor = TypeVar("TTensor", bound=_TensorType)
+TFloat = TypeVar("TFloat", bound=_FloatType)
+TInt = TypeVar("TInt", bound=_IntType)
+TReal = TypeVar("TReal", bound=_RealType)
