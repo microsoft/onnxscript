479a3d1b94f3e00245421e8a8c8d7ae62b8d017d
Jay Zhang <jiz@microsoft.com>
2023-01-11 09:13:34 +0800

    Add a new op embedding.

--- onnxscript/function_libs/torch_aten/ops/core.py
+++ onnxscript/function_libs/torch_aten/ops/core.py
@@ -1506,16 +1506,15 @@ def aten_einsum(
     raise NotImplementedError()
 
 
+@torch_op("aten::embedding")
 def aten_embedding(
-    weight: TensorType,
-    indices: TensorType,
-    padding_idx: int = -1,
-    scale_grad_by_freq: bool = False,
-    sparse: bool = False,
-) -> TensorType:
+    weight: TTensor,
+    indices: TTensor,
+    **kwargs,
+) -> TTensor:
     # embedding(Tensor weight, Tensor indices, int padding_idx=-1, bool scale_grad_by_freq=False, bool sparse=False) -> Tensor
 
-    raise NotImplementedError()
+    return op.Gather(weight, indices)
 
 
 def aten_embedding_backward(
