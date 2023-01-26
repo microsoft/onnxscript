dfedee4fedbfa3c75c44c4c38891f8fb52248ca0
Justin Chu <justinchuby@users.noreply.github.com>
2023-01-25 18:29:09 -0800

    Update onnxscript/function_libs/torch_aten/ops/core.py

--- onnxscript/function_libs/torch_aten/ops/core.py
+++ onnxscript/function_libs/torch_aten/ops/core.py
@@ -51,7 +51,7 @@ def aten_acosh(self: TFloat) -> TFloat:
 
 
 @torch_op("aten::add")
-def aten_add(self: TReal, other: TReal, alpha: float) -> TReal:
+def aten_add(self: TReal, other: TReal, alpha: float = 1.0) -> TReal:
     # add.Tensor(Tensor self, Tensor other, *, Scalar alpha=1) -> Tensor
     one = op.Constant(value_float=1.0)
     alpha = op.CastLike(alpha, one)
