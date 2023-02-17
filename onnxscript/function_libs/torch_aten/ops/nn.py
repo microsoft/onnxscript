6a4228f72bae81d5b3240d812ad0ee760b12a547
xiaowuhu <xiaowuhu@microsoft.com>
2023-02-17 17:02:47 +0800

    Update nn.py

--- onnxscript/function_libs/torch_aten/ops/nn.py
+++ onnxscript/function_libs/torch_aten/ops/nn.py
@@ -250,7 +250,7 @@ def aten_cross_entropy(
 ) -> TFloatOrBFloat16:
     # cross_entropy_loss(Tensor self, Tensor target, Tensor? weight=None, int reduction=Mean, SymInt ignore_index=-100, float label_smoothing=0.0) -> Tensor
 
-    if weight is None:
+    if op.OptionalHasElement(weight):
         result, _ = op.SoftmaxCrossEntropyLoss(
             self, target, reduction=reduction, ignore_index=ignore_index
         )
