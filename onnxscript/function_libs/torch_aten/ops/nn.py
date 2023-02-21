5eedb407eb56237cdaf53918c744ad9ee1231fa9
xiaowuhu <xiaowuhu@microsoft.com>
2023-02-21 14:40:38 +0800

    Update nn.py

--- onnxscript/function_libs/torch_aten/ops/nn.py
+++ onnxscript/function_libs/torch_aten/ops/nn.py
@@ -747,12 +747,28 @@ def aten_nll_loss(
     target: Sequence[INT64],
     weight: Optional[TFloat] = None,
     reduction: int = 1,
-    ignore_index: INT64 = -100,
+    ignore_index: int = -100,
 ) -> TFloat:
     # nll_loss(Tensor self, Tensor target, Tensor? weight=None, int reduction=Mean, SymInt ignore_index=-100) -> Tensor
-
     reduction_vals = ["none", "mean", "sum"]
-    reduction = reduction_vals[reduction]
+    reduction_str = reduction_vals[reduction]
+
+    if weight is None:
+        result = _aten_nll_loss_none(self, target, reduction=reduction_str, ignore_index=ignore_index)
+    else:
+        result = _aten_nll_loss_weight(self, target, weight, reduction=reduction_str, ignore_index=ignore_index)
+    return result
+
+
+@torch_op("aten::nll_loss", overload=True)
+def _aten_nll_loss_weight(
+    self: TFloat,
+    target: Sequence[INT64],
+    weight: TFloat,
+    reduction: str,
+    ignore_index: int,
+) -> TFloat:
+    # nll_loss(Tensor self, Tensor target, Tensor? weight=None, int reduction=Mean, SymInt ignore_index=-100) -> Tensor
 
     rank_self = op.Size(op.Shape(self))
     if rank_self == 1:
@@ -761,15 +777,35 @@ def aten_nll_loss(
         if rank_self - rank_target == 1:  # rank(target) == rank(self) - 1
             target = op.Unsqueeze(target, op.Constant(value_ints=[0]))
 
-    # if op.OptionalHasElement(weight):
-    if weight is not None:
-        result = op.NegativeLogLikelihoodLoss(
-            self, target, weight=weight, ignore_index=ignore_index, reduction=reduction
-        )
-    else:
-        result = op.NegativeLogLikelihoodLoss(
-            self, target, ignore_index=ignore_index, reduction=reduction
-        )
+    result = op.NegativeLogLikelihoodLoss(
+        self, target, weight, ignore_index=ignore_index, reduction=reduction
+    )
+
+    if rank_self == 1:
+        result = op.Squeeze(result)
+
+    return result
+
+
+@torch_op("aten::nll_loss", overload=True)
+def _aten_nll_loss_none(
+    self: TFloat,
+    target: Sequence[INT64],
+    reduction: str,
+    ignore_index: int,
+) -> TFloat:
+    # nll_loss(Tensor self, Tensor target, Tensor? weight=None, int reduction=Mean, SymInt ignore_index=-100) -> Tensor
+
+    rank_self = op.Size(op.Shape(self))
+    if rank_self == 1:
+        self = op.Unsqueeze(self, op.Constant(value_ints=[0]))
+        rank_target = op.Size(op.Shape(target))
+        if rank_self - rank_target == 1:  # rank(target) == rank(self) - 1
+            target = op.Unsqueeze(target, op.Constant(value_ints=[0]))
+
+    result = op.NegativeLogLikelihoodLoss(
+        self, target, ignore_index=ignore_index, reduction=reduction
+    )
 
     if rank_self == 1:
         result = op.Squeeze(result)
