267f90c6c88950b0e4a2aca684c81c9af3b7deae
xiaowuhu <xiaowuhu@microsoft.com>
2023-02-22 15:47:39 +0800

    Update ops_correctness_test.py

--- onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
+++ onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
@@ -208,6 +208,7 @@ def _nll_loss_input_wrangler(
     args: list[Any], kwargs: dict[str, Any]
 ) -> tuple[list[Any], dict[str, Any]]:
     if "reduction" in kwargs:
+        # aten_nll_loss can only accept integer argument instead of string
         reduction_vals = ["none", "mean", "sum"]
         value = kwargs["reduction"]
         kwargs["reduction"] = reduction_vals.index(value)
@@ -389,8 +390,8 @@ OPINFO_FUNCTION_MAPPING_TRACE_ONLY: dict[
     "nn.functional.conv2d": core_ops.aten_conv2d,
     "nn.functional.conv3d": core_ops.aten_conv3d,
     "nn.functional.nll_loss": (nn_ops.aten_nll_loss, _nll_loss_input_wrangler),
-    # "nn.functional.nll_loss2d": (nn_ops.aten_nll_loss2d, _nll_loss_input_wrangler),  # not in OPS_DB
-    # "nn.functional.nll_loss_nd": (nn_ops.aten_nll_loss_nd, _nll_loss_input_wrangler),  # not in OPS_DB
+    "nn.functional.nll_loss": (nn_ops.aten_nll_loss2d, _nll_loss_input_wrangler),
+    "nn.functional.nll_loss": (nn_ops.aten_nll_loss_nd, _nll_loss_input_wrangler),
     "nn.functional.gelu": nn_ops.aten_gelu,
     "nn.functional.linear": nn_ops.aten_linear,
     "ones_like": core_ops.aten_ones_like,
