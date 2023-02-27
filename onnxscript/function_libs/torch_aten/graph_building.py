ffed433de96205e8e5dadfa3d600627a11090642
BowenBao <bowbao@microsoft.com>
2023-02-27 15:33:27 -0800

    Merge 'initializers' into 'TorchScriptGraph'

--- onnxscript/function_libs/torch_aten/graph_building.py
+++ onnxscript/function_libs/torch_aten/graph_building.py
@@ -296,10 +296,15 @@ class TorchScriptGraph:
         # All the functions used, deduplicated by name
         # key: (name, domain)
         self._function_store: Dict[Tuple[str, str], onnxscript.OnnxFunction] = {}
+        self._initializers: Dict[str, torch.Tensor] = {}
 
     @property
     def torch_graph(self):
         return self._torch_graph
+
+    @property
+    def initializers(self) -> Mapping[str, torch.Tensor]:
+        return self._initializers
 
     @beartype
     def add_input(
@@ -326,6 +331,12 @@ class TorchScriptGraph:
         )
         tensor_value = _wrap_torch_value_to_tensor(torch_value)
         return tensor_value  # type: ignore[return-value]
+
+    @beartype
+    def add_initializer(
+        self, input_name: str, input_value: torch.Tensor
+    ) -> None:
+        self._initializers[input_name] = input_value
 
     @beartype
     def register_outputs(
@@ -447,7 +458,7 @@ class TorchScriptGraph:
 
     @beartype
     def to_model_proto(
-        self, initializers: Mapping[str, torch.Tensor], opset_version: Optional[int]
+        self, opset_version: int
     ) -> onnx.ModelProto:
         (
             proto,
@@ -455,7 +466,7 @@ class TorchScriptGraph:
             _,
             _,
         ) = self._torch_graph._export_onnx(  # type: ignore[attr-defined] # pylint: disable=protected-access
-            initializers=initializers,
+            initializers=self.initializers,
             onnx_opset_version=opset_version,
             # TODO(justinchuby): Figure out how to get the dynamic axes from the inputs
             dynamic_axes={},
