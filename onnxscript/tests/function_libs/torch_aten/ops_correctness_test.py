f3baa71f98e98d75a352833b97aa924125fce0db
xiaowuhu <xiaowuhu@microsoft.com>
2023-02-28 14:30:58 +0800

    Update ops_correctness_test.py

--- onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
+++ onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
@@ -764,10 +764,6 @@ class TestOutputConsistency(unittest.TestCase):
                 inputs=repr(inputs),
                 kwargs=repr(cpu_sample.kwargs),
             ):
-
-                if i == 19:
-                    print("19")
-
                 skip_reason = _should_skip_test_sample(op.name, cpu_sample)
                 if skip_reason is not None:
                     # Cannot use self.skip because pytest would skip the entire test
