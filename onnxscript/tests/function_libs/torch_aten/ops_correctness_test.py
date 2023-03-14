5ab0628b1d30ef9bedb4c150118154fa60c5ab2b
Justin Chu <justinchu@microsoft.com>
2023-03-14 17:53:43 +0000

    op db info

--- onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
+++ onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
@@ -2,9 +2,16 @@
 
 ## How to add a new operator test
 
-1. To enable test cases for an operator, add an entry to the
-`OPINFO_FUNCTION_MAPPING_SCRIPTED` map, run the test and follow instruction in
-error messages if there are any.
+This test use PyTorch's OpInfo mechanism to generate test cases for each operator.
+You may find all OpInfos in https://github.com/pytorch/pytorch/blob/7ec0d6f006fdd2c9b978dc6aa4923144684a3f51/torch/testing/_internal/common_methods_invocations.py#L8804
+
+1. To enable test cases for an operator
+    1a. If the op is not `trace_only`, add an entry to the
+    `OPINFO_FUNCTION_MAPPING_SCRIPTED` map.
+    1b. If the op is `trace_only`, add an entry to the
+    `OPINFO_FUNCTION_MAPPING_TRACE_ONLY` map.
+
+    The entries are <op_info_name: function> pairs.
 2. Edit `EXPECTED_SKIPS_OR_FAILS` and/or `SKIP_SUBTESTS` to skip or xfail tests.
 Prefer xfail over skip when possible.
     2a. If a test is now failing because of xpass, because some previous errors
@@ -1056,7 +1063,7 @@ class TestOutputConsistency(unittest.TestCase):
     This is a parameterized test suite.
     """
 
-    # The function evaluator to use. This is a function that takes a function and its arguments
+    # The function executor to use. This is a function that takes a function and its arguments
     # and returns the output of the function.
     function_executor: Callable
 
