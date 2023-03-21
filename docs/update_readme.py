639593fa024ef1b067e354f9c595c00a480a7581
shubhambhokare1 <shubhambhokare@gmail.com>
2023-03-21 10:07:34 +0000

    add encoding

--- docs/update_readme.py
+++ docs/update_readme.py
@@ -1,12 +1,12 @@
 final_readme = []
-with open('README.md') as f:
+with open('README.md', encoding="utf-8") as f:
     readme = f.readlines()
     for line in readme:
         final_readme.append(line)
         if line == "```python update-readme\n":
-            with open('docs/tutorial/examples/hardmax_end_to_end.py') as example_f:
+            with open('docs/tutorial/examples/hardmax_end_to_end.py', encoding="utf-8") as example_f:
                 example_code = example_f.readlines()
                 final_readme += example_code
 
-with open('README.md', 'w') as f:
+with open('README.md', 'w', encoding="utf-8") as f:
     f.writelines(final_readme)
