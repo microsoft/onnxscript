7dd64965de1707972eeb6766fbe2809df08c81f7
Justin Chu <justinchu@microsoft.com>
2023-03-23 01:43:58 +0000

    Snap

--- onnxscript/function_libs/torch_aten/ops/nn.py
+++ onnxscript/function_libs/torch_aten/ops/nn.py
@@ -1080,8 +1080,8 @@ def aten_scaled_dot_product_attention(
     # Use trace_only to obtain the permutation for Transpose
     perm = list(range(len(key.shape)))
     perm[-1], perm[-2] = perm[-2], perm[-1]
-    scale = op.Optional() if scale is None else scale
-    attn_mask = op.Optional() if attn_mask is None else attn_mask
+    scale = 1.0 if scale is None else scale
+    attn_mask = False if attn_mask is None else attn_mask
     return _aten_scaled_dot_product_attention_onnx(
         query, key, value, attn_mask, scale, dropout_p, is_causal, perm
     )
@@ -1101,8 +1101,8 @@ def _aten_scaled_dot_product_attention_onnx(
     # Reference: https://pytorch.org/docs/stable/generated/torch.nn.functional.scaled_dot_product_attention.html
     is_causal = op.Cast(is_causal, to=BOOL.dtype)
     if is_causal:
-        dim_l = op.Shape(query)[-2]
-        dim_s = op.Shape(key)[-2]
+        dim_l = op.Shape(query)[-2:-1]
+        dim_s = op.Shape(key)[-2:-1]
         # attn_mask = torch.ones(L, S, dtype=torch.bool) := {
         one = op.Cast(op.Constant(value_int=1), to=BOOL.dtype)
         size = op.Concat(dim_l, dim_s, axis=0)
@@ -1112,11 +1112,11 @@ def _aten_scaled_dot_product_attention_onnx(
 
     # Cast the boolean mask to match the query
     # mask_fill(attn_mask, not attn_mask, -float('inf'))
-    mask_fill_value_cast = op.CastLike(op.Constant(value_float=-float("inf")), attn_mask)
-    attn_mask = op.Where(op.Not(attn_mask), mask_fill_value_cast, attn_mask)
+    mask_fill_value_cast = op.CastLike(op.Constant(value_float=-float("inf")), query)
+    attn_mask = op.Where(op.Not(attn_mask), mask_fill_value_cast, 0.0)
 
     if op.Not(op.OptionalHasElement(scale)):
-        scale = 1 / op.Sqrt(op.CaskLike(op.Shape(query)[-1], query))
+        scale = 1 / op.Sqrt(op.CastLike(op.Shape(query)[-1], query))
 
     attn_weight = op.Softmax(
         (op.MatMul(query, op.Transpose(key, perm=perm)) * scale) + attn_mask,
@@ -1140,8 +1140,8 @@ def aten_scaled_dot_product_attention_float_mask(
     # Use trace_only to obtain the permutation for Transpose
     perm = list(range(len(key.shape)))
     perm[-1], perm[-2] = perm[-2], perm[-1]
-    scale = op.Optional() if scale is None else scale
-    attn_mask = op.Optional() if attn_mask is None else attn_mask
+    scale = 1.0 if scale is None else scale
+    attn_mask = 0.0 if attn_mask is None else attn_mask
     return _aten_scaled_dot_product_attention_float_onnx(
         query, key, value, attn_mask, scale, dropout_p, is_causal, perm
     )
@@ -1161,8 +1161,8 @@ def _aten_scaled_dot_product_attention_float_mask_onnx(
     # Reference: https://pytorch.org/docs/stable/generated/torch.nn.functional.scaled_dot_product_attention.html
     is_causal = op.Cast(is_causal, to=BOOL.dtype)
     if is_causal:
-        dim_l = op.Shape(query)[-2]
-        dim_s = op.Shape(key)[-2]
+        dim_l = op.Shape(query)[-2:-1]
+        dim_s = op.Shape(key)[-2:-1]
         # attn_mask = torch.ones(L, S) := {
         one = op.CastLike(op.Constant(value_int=1), attn_mask)
         size = op.Concat(dim_l, dim_s, axis=0)
@@ -1171,7 +1171,7 @@ def _aten_scaled_dot_product_attention_float_mask_onnx(
         attn_mask = op.Trilu(attn_mask, upper=0)
 
     if op.Not(op.OptionalHasElement(scale)):
-        scale = 1 / op.Sqrt(op.CaskLike(op.Shape(query)[-1], query))
+        scale = 1 / op.Sqrt(op.CastLike(op.Shape(query)[-1], query))
 
     attn_weight = op.Softmax(
         (op.MatMul(query, op.Transpose(key, perm=perm)) * scale) + attn_mask,
