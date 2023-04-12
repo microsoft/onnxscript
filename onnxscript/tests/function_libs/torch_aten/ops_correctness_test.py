c6505cf19829d264c37873a123404cc22bac401b
Justin Chu <justinchu@microsoft.com>
2023-04-12 17:30:24 +0000

    Move version_utils to _internal | chore
    Move version_utils to `_internal` so that it can be used my onnxscript
    
    [ghstack-poisoned]

--- onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
+++ onnxscript/tests/function_libs/torch_aten/ops_correctness_test.py
@@ -58,11 +58,11 @@ from torch.utils import _pytree as pytree
 
 import onnxscript
 import onnxscript.evaluator
+from onnxscript._internal import version_utils
 from onnxscript.function_libs.torch_aten import graph_building
 from onnxscript.function_libs.torch_aten.ops import core as core_ops
 from onnxscript.function_libs.torch_aten.ops import nn as nn_ops
 from onnxscript.function_libs.torch_aten.ops import special as special_ops
-from onnxscript.tests.common import version_utils
 from onnxscript.tests.function_libs.torch_aten import extra_opinfo
 
 T = TypeVar("T")
