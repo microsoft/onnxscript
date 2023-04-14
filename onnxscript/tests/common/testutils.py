ccf71c91594e530e5ab3ae9f4ad248448003947c
Justin Chu <justinchu@microsoft.com>
2023-04-14 02:16:11 +0000

    docs

--- onnxscript/tests/common/testutils.py
+++ onnxscript/tests/common/testutils.py
@@ -7,6 +7,7 @@ import unittest
 
 
 class TestBase(unittest.TestCase):
+    """The base class for testing ONNX Script functions for internal use."""
     def validate(self, fn):
         """Validate script function translation."""
         return fn.to_function_proto()
