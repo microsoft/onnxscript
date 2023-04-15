1c59318253079f017fe4ad113505453c9dd3c0bc
xiaowuhu <xiaowuhu@microsoft.com>
2023-04-15 10:54:10 +0800

    Revert "Add ops(hardtanh, slice_scatter) | feat(atenlib) (#558)"
    This reverts commit 7abb9d3ad966d62c9f7847f84dc2b68976483643.

--- onnxscript/function_libs/torch_aten/ops/nn.py
+++ onnxscript/function_libs/torch_aten/ops/nn.py
@@ -529,11 +529,10 @@ def aten_hardswish_backward(grad_output: TensorType, self: TensorType) -> Tensor
     raise NotImplementedError()
 
 
-@torch_op("aten::hardtanh")
-def aten_hardtanh(self: TReal, min_val: float = -1.0, max_val: float = 1.0) -> TReal:
+def aten_hardtanh(self: TensorType, min_val: float = -1.0, max_val: float = 1.0) -> TensorType:
     """hardtanh(Tensor self, Scalar min_val=-1, Scalar max_val=1) -> Tensor"""
 
-    return op.Clip(self, min_val, max_val)
+    raise NotImplementedError()
 
 
 def aten_hardtanh_backward(
