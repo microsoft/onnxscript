49f17ece4df46cfe11035f3a76c7afaa79bc81f8
Jay Zhang <jiz@microsoft.com>
2023-04-20 14:49:19 +0800

    Add the final gen file. It has no lint issue and its output file has no lint issue either.

--- onnxscript/function_libs/tools/generate_prims_signatures.py
+++ onnxscript/function_libs/tools/generate_prims_signatures.py
@@ -11,51 +11,18 @@ import ast
 import logging
 import os
 import textwrap
-import typing
-from typing import Any, Callable, Dict, List, Mapping, Sequence, Union
+from pathlib import Path
+from typing import Any, Dict, List, Sequence
 
-
+import black
+import isort
 import torch
 import torchgen.gen
 import torchgen.model
-import yaml
+from torch._ops import _OpNamespace
+from torchgen.model import FunctionSchema
 
 import opgen.pygen as cg
-from torch._ops import _OpNamespace
-
-from torchgen.model import (
-    Argument,
-    FunctionSchema,
-    NativeFunction,
-    Type,
-    Variant,
-)
-
-def load_native_function_yaml(yaml_path: str):
-    with open(yaml_path, encoding="utf-8") as f:
-        yaml_str = f.read()
-    with open(yaml_path, encoding="utf-8") as f:
-        all_functions = yaml.safe_load(f)
-    valid_tags = set()
-    # Mark all tags as valid, since we don't want to validate them.
-    for func in all_functions:
-        if "tags" not in func:
-            continue
-        # if isinstance(func["tags"], Sequence):
-        #     continue;
-        valid_tags.add(func["tags"])
-
-    return yaml_str, valid_tags
-
-
-def parse_native_functions_yaml(yaml_path: str) -> tuple[Any, Any]:
-    """Parses the native_functions.yaml file."""
-    yaml_str, valid_tags = load_native_function_yaml(yaml_path)
-    yaml_struct = yaml.load(yaml_str, Loader=torchgen.gen.LineLoader)
-    parsed = torchgen.gen.parse_native_yaml_struct(
-        yaml_struct, valid_tags, path=yaml_path, skip_native_fns_gen=True
-    )
-    return parsed.native_functions, parsed.backend_indices
 
 
 def create_list_type(arg: torchgen.model.Argument) -> cg.TypeRef:
@@ -122,30 +89,18 @@ def get_argument_type(arg: torchgen.model.Argument) -> cg.TypeRef:
     return inner_node
 
 
-def should_generate_signature(func: torchgen.model.NativeFunction) -> bool:
+def should_generate_signature(op_name: str, schema: FunctionSchema) -> bool:
     """Returns whether the signature for the given function should be generated."""
-    if func.func.name.name.base.startswith("_"):
+    if op_name.split("_")[1].startswith("_"):
         return False
-    if func.func.name.name.inplace:
+    if schema.name.name.inplace:
         return False
-    if func.func.name.overload_name and func.func.name.overload_name != "Tensor":
+    if schema.name.overload_name and schema.name.overload_name != "Tensor":
         # Ignore overloads for now.
         # Some ops only have overloaded versions, like aten::add.Tensor. And we
         # want to generate the aten::add op.
         return False
     return True
-
-
-def get_op_name(func: torchgen.model.NativeFunction) -> str:
-    if func.func.name.overload_name and func.func.name.overload_name != "Tensor":
-        # Do not include the overload name if it is "Tensor", since ops like
-        # aten::add.Tensor is what we want for aten::add.
-        name = f"{func.func.name.name.base}_{func.func.name.overload_name}"
-    else:
-        name = f"{func.func.name.name.base}"
-
-    # Prefix with namespace to avoid name conflicts with other operators and arguments.
-    return f"{func.namespace}_{name}"
 
 
 def parse_default_value(arg: torchgen.model.Argument) -> Any:
@@ -207,16 +162,15 @@ def format_arg_name(arg: torchgen.model.Argument) -> str:
     return arg.name
 
 
-def create_signature(func: torchgen.model.NativeFunction) -> cg.FunctionDef:
+def create_signature(op_name: str, schema: FunctionSchema) -> cg.FunctionDef:
     """Creates the signature for the given function."""
-    op_name = get_op_name(func)
     args = [
         arg.argument if isinstance(arg, torchgen.model.SelfArgument) else arg
-        for arg in func.func.arguments.positional
+        for arg in schema.arguments.positional
     ]
     kwargs = [
         arg
-        for arg in func.func.arguments.kwarg_only
+        for arg in schema.arguments.kwarg_only
         if not isinstance(arg, torchgen.model.TensorOptionsArguments)
     ]
 
@@ -247,21 +201,25 @@ def create_signature(func: torchgen.model.NativeFunction) -> cg.FunctionDef:
     return cg.FunctionDef(
         op_name,
         *py_args,
-        return_type=create_return_type(func.func.returns),
+        return_type=create_return_type(schema.returns),
         body=[
-            cg.ThunkStmt(f'"""{func.func}"""'),
+            cg.ThunkStmt(f'"""{schema}"""'),
             cg.Raise(cg.Call(cg.Name("NotImplementedError"))),  # type: ignore[list-item]
         ],
     )
 
 
 def create_onnx_function_module(
-    functions: Sequence[torchgen.model.NativeFunction],
+    functions: Sequence[tuple(str, FunctionSchema)],  # type: ignore[valid-type]
 ) -> cg.Module:
     """Creates the onnx function module."""
     return cg.Module(
         cg.ImportFrom("__future__", cg.Alias("annotations")),
-        *[create_signature(func) for func in functions if should_generate_signature(func)],
+        *[
+            create_signature(name, schema)
+            for name, schema in functions
+            if should_generate_signature(name, schema)
+        ],
     )
 
 
@@ -278,53 +236,55 @@ def copyright_header() -> str:
         """
     )
 
-def _get_func_schema_of_namespace(namespaces: List[_OpNamespace]) -> (Dict[str, FunctionSchema]):
+
+def _get_func_schema_of_namespace(namespaces: List[_OpNamespace]) -> Dict[str, FunctionSchema]:
     table: Dict[str, FunctionSchema] = {}
     not_supported_ops = ["normal"]
     for op_namespace in namespaces:
         for attr_name in dir(op_namespace):
             op_overload_packet = getattr(op_namespace, attr_name)
-            if not isinstance(op_overload_packet, torch._ops.OpOverloadPacket):
+            if not isinstance(
+                op_overload_packet,
+                torch._ops.OpOverloadPacket,  # pylint: disable=protected-access
+            ):
                 continue
 
-            print("=== op: ", attr_name)
-            if attr_name in not_supported_ops or (not isinstance(op_overload_packet.return_type, tuple) and op_overload_packet.return_type.name.lower() != "new"):
-                print("=== not supported op and schema: ", attr_name, getattr(op_overload_packet, 'schema'))
+            if attr_name in not_supported_ops or (
+                not isinstance(op_overload_packet.return_type, tuple)
+                and op_overload_packet.return_type.name.lower() != "new"
+            ):
+                print("=== not supported op and schema: ", op_overload_packet.schema)
                 continue
 
-            func_schema = FunctionSchema.parse(getattr(op_overload_packet, 'schema'))
-            table[attr_name] = func_schema
+            func_schema = FunctionSchema.parse(op_overload_packet.schema)
+            op_name = op_namespace.name + "_" + attr_name
+            table[op_name] = func_schema
 
     return table
 
-my_table = _get_func_schema_of_namespace([torch.ops.prims])
 
 def main(args: argparse.Namespace) -> None:
-    functions: dict[str, dict[str, torchgen.model.NativeFunction]] = {}
     all_ops = _get_func_schema_of_namespace([torch.ops.prims])
     functions: dict[str, dict[str, FunctionSchema]] = {}
-    for func in all_ops:
-        if not should_generate_signature(func):
+    for op_name, schema in all_ops.items():
+        if not should_generate_signature(op_name, schema):
             continue
 
-        module_name = typing.cast(str, func.python_module)
-        if not module_name:
-            module_name = "core"
+        module_name = op_name.split("_")[0]
         if module_name not in functions:
             functions[module_name] = {}
-        op_name = get_op_name(func)
         if op_name in functions[module_name]:
             logging.warning(
-                "Duplicated function: %s, overload: %s", op_name, func.func.name.overload_name
+                "Duplicated function: %s, overload: %s", op_name, schema.name.overload_name
             )
             continue
-        functions[module_name][op_name] = func
+        functions[module_name][op_name] = schema
 
     os.makedirs(args.outdir, exist_ok=True)
 
     for module_name, module_functions in functions.items():
         sorted_functions = sorted(module_functions.items(), key=lambda x: x[0])
-        py_module = create_onnx_function_module([func for _, func in sorted_functions])
+        py_module = create_onnx_function_module(list(sorted_functions))
         py_module.accept(cg.ImportAdjuster())
         py_module.accept(cg.DocCommentBuilder())
         output_path = os.path.join(args.outdir, f"{module_name}.py")
@@ -346,6 +306,16 @@ def main(args: argparse.Namespace) -> None:
                 )
             )
             py_module.accept(cg.PythonWriter(f))
+
+    # Format the generated files so that they pass linting.
+    # line_length=95 is to match the lintrunner rules.
+    isort.file(output_path)
+    black.format_file_in_place(
+        Path(output_path),
+        fast=True,
+        mode=black.Mode(line_length=95),
+        write_back=black.WriteBack.YES,
+    )
     print("Done.")
 
 
