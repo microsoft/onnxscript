63cb2eb28377592b88cb71683902d61ea73bed1b
Ti-Tai Wang <titaiwang@microsoft.com>
2023-05-04 13:17:59 -0700

    Refactor Overloads | feat(torchlib) (#719)
    After this PR, overloads is deleted, and when we register a **aten
    overload**, we use it's specific name: `aten::mean.dim`. If it's an
    **onnx overload** created regarding ONNXFunction limitations, such as
    `aten_bitwise_not_bool`, we can simply register it under the same aten
    name. The dispatcher in converter side is expected to find the proper
    one with op schema.

--- onnxscript/function_libs/torch_lib/registration.py
+++ onnxscript/function_libs/torch_lib/registration.py
@@ -13,14 +13,12 @@ class OverloadedFunction:
 
     Attributes:
         name: Name of the op. E.g. "aten::add".
-        default: Default function.
-        overloads: Overloads.
+        overloads: Overloads function.
         privates: Private functions not exposed to users.
     """
 
     def __init__(self, name: str):
         self.name = name
-        self.default: Optional[Any] = None
         self.overloads: list[Any] = []
         self.privates: list[Any] = []
 
@@ -31,17 +29,13 @@ class Registry:
     def __init__(self):
         self._registry: dict[str, OverloadedFunction] = {}
 
-    def register(
-        self, func: Any, name: str, *, overload: bool = False, private: bool = False
-    ) -> None:
+    def register(self, func: Any, name: str, *, private: bool = False) -> None:
         """Register a function."""
 
-        if overload:
-            self._registry.setdefault(name, OverloadedFunction(name)).overloads.append(func)
-        elif private:
+        if private:
             self._registry.setdefault(name, OverloadedFunction(name)).privates.append(func)
         else:
-            self._registry.setdefault(name, OverloadedFunction(name)).default = func
+            self._registry.setdefault(name, OverloadedFunction(name)).overloads.append(func)
 
     def __getitem__(self, name):
         return self._registry[name]
@@ -63,7 +57,6 @@ default_registry = Registry()
 def torch_op(
     name,
     *,
-    overload: bool = False,
     registry: Optional[Registry] = None,
     trace_only: bool = False,
     private: bool = False,
@@ -72,7 +65,6 @@ def torch_op(
 
     Args:
         name: ATen name of the function. E.g. "aten::add".
-        overload: Whether the function is an overload (not default).
         registry: Registry to register the function to. If None, the default registry is used.
         trace_only: Whether the function should only be traced and not compiled.
         private: Whether the function is private (not directly exposed). It should
@@ -95,7 +87,7 @@ def torch_op(
             processed_func = onnxscript.script(opset=custom_opset)(func)
 
         assert registry is not None
-        registry.register(processed_func, name, overload=overload, private=private)
+        registry.register(processed_func, name, private=private)
         return processed_func
 
     return wrapper
