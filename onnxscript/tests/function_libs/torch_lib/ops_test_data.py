149b16131d2da566ed57581481daac3dcc8beb4b
BowenBao <bowbao@microsoft.com>
2023-06-01 14:56:29 -0700

    Fix 'aten.gather' input order | fix(torchlib)
    [ghstack-poisoned]

--- onnxscript/tests/function_libs/torch_lib/ops_test_data.py
+++ onnxscript/tests/function_libs/torch_lib/ops_test_data.py
@@ -155,14 +155,6 @@ def _flip_input_wrangler(
     return args, kwargs
 
 
-def _gather_input_wrangler(
-    args: list[Any], kwargs: dict[str, Any]
-) -> tuple[list[Any], dict[str, Any]]:
-    # Make the dim argument an attribute
-    kwargs["dim"] = args.pop(1)
-    return args, kwargs
-
-
 def _grid_sample_input_wrangler(
     args: list[Any], kwargs: dict[str, Any]
 ) -> tuple[list[Any], dict[str, Any]]:
@@ -377,7 +369,6 @@ OPINFO_FUNCTION_MAPPING_SCRIPTED: dict[
     "full": core_ops.aten_full,
     "full_like_dtype": core_ops.aten_full_like_dtype,
     "full_like": core_ops.aten_full_like,
-    "gather": (core_ops.aten_gather, _gather_input_wrangler),
     "ge": core_ops.aten_ge,
     # "greater_equal": core_ops.aten_greater_equal,  # no test case in OPS_DB
     # "greater": core_ops.aten_greater,  # no test case in OPS_DB
