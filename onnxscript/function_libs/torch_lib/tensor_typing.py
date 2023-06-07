f5ec7e3bb4d5e5a3b6c4bf0f65778d37ccd72300
Justin Chu <justinchu@microsoft.com>
2023-06-07 18:56:32 +0000

    Change TTensor etc. to be constrained | fix(typing)
    Fixes https://github.com/microsoft/onnxscript/issues/707

--- onnxscript/function_libs/torch_lib/tensor_typing.py
+++ onnxscript/function_libs/torch_lib/tensor_typing.py
@@ -28,7 +28,7 @@ from onnxscript import (
 # NOTE: We do not care about unsigned types beyond UINT8 because PyTorch does not us them.
 # More detail can be found: https://pytorch.org/docs/stable/tensors.html
 
-_TensorType = Union[
+TensorType = Union[
     BFLOAT16,
     BOOL,
     COMPLEX64,
@@ -42,7 +42,7 @@ _TensorType = Union[
     INT64,
     UINT8,
 ]
-_FloatType = Union[FLOAT16, FLOAT, DOUBLE]
+FloatType = Union[FLOAT16, FLOAT, DOUBLE]
 IntType = Union[INT8, INT16, INT32, INT64]
 RealType = Union[
     BFLOAT16,
@@ -55,17 +55,58 @@ RealType = Union[
     INT64,
 ]
 
-TTensor = TypeVar("TTensor", bound=_TensorType)
-TTensorOrString = TypeVar("TTensorOrString", bound=Union[_TensorType, STRING])
-TFloat = TypeVar("TFloat", bound=_FloatType)
-TFloatOrBFloat16 = TypeVar("TFloatOrBFloat16", bound=Union[FLOAT16, FLOAT, DOUBLE, BFLOAT16])
-TFloatOrUInt8 = TypeVar("TFloatOrUInt8", bound=Union[FLOAT, FLOAT16, DOUBLE, INT8, UINT8])
-TInt = TypeVar("TInt", bound=IntType)
-TReal = TypeVar("TReal", bound=RealType)
+TTensor = TypeVar(
+    "TTensor",
+    BFLOAT16,
+    BOOL,
+    COMPLEX64,
+    COMPLEX128,
+    DOUBLE,
+    FLOAT,
+    FLOAT16,
+    INT8,
+    INT16,
+    INT32,
+    INT64,
+    UINT8,
+)
+TTensorOrString = TypeVar(
+    "TTensorOrString",
+    BFLOAT16,
+    BOOL,
+    COMPLEX64,
+    COMPLEX128,
+    DOUBLE,
+    FLOAT,
+    FLOAT16,
+    INT8,
+    INT16,
+    INT32,
+    INT64,
+    UINT8,
+    STRING,
+)
+TFloat = TypeVar("TFloat", FLOAT16, FLOAT, DOUBLE)
+TFloatOrBFloat16 = TypeVar("TFloatOrBFloat16", FLOAT16, FLOAT, DOUBLE, BFLOAT16)
+TFloatOrUInt8 = TypeVar("TFloatOrUInt8", FLOAT, FLOAT16, DOUBLE, INT8, UINT8)
+TInt = TypeVar("TInt", INT8, INT16, INT32, INT64)
+TReal = TypeVar(
+    "TReal",
+    BFLOAT16,
+    FLOAT16,
+    FLOAT,
+    DOUBLE,
+    INT8,
+    INT16,
+    INT32,
+    INT64,
+)
 TRealUnlessInt16OrInt8 = TypeVar(
-    "TRealUnlessInt16OrInt8", bound=Union[FLOAT16, FLOAT, DOUBLE, BFLOAT16, INT32, INT64]
+    "TRealUnlessInt16OrInt8", FLOAT16, FLOAT, DOUBLE, BFLOAT16, INT32, INT64
 )
 TRealUnlessFloat16OrInt8 = TypeVar(
-    "TRealUnlessFloat16OrInt8", bound=Union[DOUBLE, FLOAT, INT16, INT32, INT64]
+    "TRealUnlessFloat16OrInt8", DOUBLE, FLOAT, INT16, INT32, INT64
 )
-TrealOrUInt8 = TypeVar("TrealOrUInt8", bound=Union[RealType, UINT8])
+TrealOrUInt8 = TypeVar(
+    "TrealOrUInt8", BFLOAT16, FLOAT16, FLOAT, DOUBLE, INT8, INT16, INT32, INT64, UINT8
+)
