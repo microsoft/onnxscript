8990bff909d06017d55902781f74151b476fab98
Justin Chu <justinchu@microsoft.com>
2023-06-19 19:18:31 +0000

    Simplify runtime type checking decorator
    - Remove the warning mode to simplify code as it is no longer the default and unused.
    - Renamed the decorator to `runtime_typing.checked` to hide implementation details (beartype)

---
+++ onnxscript/_internal/runtime_typing.py
@@ -0,0 +1,39 @@
+"""An internal wrapper for the beartype library.
+
+Decorate the function with `@runtime_typing.checked` to enable runtime
+type checking. The decorator is a no-op when the `beartype` library is not
+installed.
+"""
+import typing
+import warnings
+
+__all__ = [
+    "checked",
+]
+
+try:
+    from beartype import beartype as checked
+    from beartype import roar as _roar
+
+    # Beartype warns when we import from typing because the types are deprecated
+    # in Python 3.9. But there will be a long time until we can move to using
+    # the native container types for type annotations (when 3.9 is the lowest
+    # supported version). So we silence the warning.
+    warnings.filterwarnings(
+        "ignore",
+        category=_roar.BeartypeDecorHintPep585DeprecationWarning,
+    )
+except ImportError:
+    def checked(func):
+        return func
+except Exception as e:  # pylint: disable=broad-exception-caught
+    # Warn errors that are not import errors (unexpected).
+    warnings.warn(f"{e}", stacklevel=2)
+    def checked(func):
+        return func
+
+
+if typing.TYPE_CHECKING:
+    # This is a hack to make mypy play nicely with the beartype decorator.
+    def checked(func):
+        return func
