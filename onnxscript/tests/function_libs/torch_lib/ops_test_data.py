1637dc1b7afe01923f4f25b147d3ad6f82345ce6
Justin Chu <justinchu@microsoft.com>
2023-07-05 18:03:23 +0000

    Test erfcx | fix(torchlib)

--- onnxscript/tests/function_libs/torch_lib/ops_test_data.py
+++ onnxscript/tests/function_libs/torch_lib/ops_test_data.py
@@ -556,7 +556,7 @@ TESTED_TORCHLIB_OPS: tuple[TorchLibOpInfo, ...] = (
     TorchLibOpInfo(
         "erfc", special_ops.aten_special_erfc, tolerance={torch.float16: (1e-2, 2e-4)}
     ),
-    # TorchLibOpInfo("erfcx", special_ops.aten_special_erfcx),  # not in OPS_DB
+    TorchLibOpInfo("special.erfcx", special_ops.aten_special_erfcx),
     TorchLibOpInfo("fill", core_ops.aten_fill),
     TorchLibOpInfo("flip", core_ops.aten_flip, input_wrangler=_flip_input_wrangler),
     TorchLibOpInfo("floor", core_ops.aten_floor),
