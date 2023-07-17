bbdbf1e124ceac3c43f76df1ef9a9658f58cc715
Justin Chu <justinchuby@users.noreply.github.com>
2023-07-17 11:11:01 -0700

    Simplify model error message in test | test(torchlib) (#882)
    Stack from [ghstack](https://github.com/ezyang/ghstack) (oldest at
    bottom):
    * #883
    * __->__ #882
    * #881
    
    Remove the duplicate check_model message when displaying

--- onnxscript/tests/function_libs/torch_lib/ops_test_common.py
+++ onnxscript/tests/function_libs/torch_lib/ops_test_common.py
@@ -517,9 +517,7 @@ def graph_executor(
             onnx.checker.check_model(onnx_model, full_check=True)
         except onnx.checker.ValidationError as e:
             raise AssertionError(
-                f"ONNX model is invalid: {e}. "
-                f"Model:\n"
-                f"{onnxscript.proto2text(onnx_model)}"
+                f"ONNX model is invalid. Model:\n{onnxscript.proto2text(onnx_model)}"
             ) from e
 
         try:
