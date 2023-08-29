f270dc1eed9fbdbe28204ec956530484c0307d12
Justin Chu <justinchuby@users.noreply.github.com>
2023-08-29 10:42:43 -0700

    Bump ONNX and ORT in CI | chore(ci) (#1037)
    ONNX 1.14.1; ORT 1.15.1

--- noxfile.py
+++ noxfile.py
@@ -26,8 +26,8 @@ COMMON_TEST_DEPENDENCIES = (
     "pytest!=7.1.0",
     "pyyaml",
 )
-ONNX = "onnx==1.14.0"
-ONNX_RUNTIME = "onnxruntime==1.15.0"
+ONNX = "onnx==1.14.1"
+ONNX_RUNTIME = "onnxruntime==1.15.1"
 PYTORCH = "torch==2.0.1"
 ONNX_RUNTIME_NIGHTLY_DEPENDENCIES = (
     "flatbuffers",
