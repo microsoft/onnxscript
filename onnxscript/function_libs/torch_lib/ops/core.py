c68468e6ca492335de773a1718d995292f4ffa5b
Justin Chu <justinchuby@users.noreply.github.com>
2023-10-25 13:54:47 -0700

    Use Gemm to implement addmm | fix(torchlib) (#1113)
    When I looked at the test coverage for `addmm` (below), I realized mat1
    and mat2 are always 2d tensors. So the rank check is redundant. `addmm`
    is now fully mapped to `Gemm`, which should completely resolve
    https://github.com/microsoft/onnxscript/issues/1089
    
    Closes https://github.com/microsoft/onnxscript/issues/1110
    
    
    ![image](https://github.com/microsoft/onnxscript/assets/11205048/073347ca-d677-4c87-94fa-e40a13642569)

--- onnxscript/function_libs/torch_lib/ops/core.py
+++ onnxscript/function_libs/torch_lib/ops/core.py
@@ -222,37 +222,15 @@ def aten_addcmul(
 
 @torch_op("aten::addmm")
 def aten_addmm(
-    self: TInt, mat1: TInt, mat2: TInt, beta: float = 1.0, alpha: float = 1.0
-) -> TInt:
+    self: TReal, mat1: TReal, mat2: TReal, beta: float = 1.0, alpha: float = 1.0
+) -> TReal:
     """addmm(Tensor self, Tensor mat1, Tensor mat2, *, Scalar beta=1, Scalar alpha=1) -> Tensor"""
 
-    mat1_mat2 = op.MatMul(mat1, mat2)
-    scaled_mat1_mat2 = op.Mul(mat1_mat2, alpha)
-    scaled_self = op.Mul(self, beta)
-    return op.Add(scaled_self, scaled_mat1_mat2)
+    # NOTE: ONNX Runtime does not support int inputs to Gemm as of 1.16.
+    # To support int inputs, consider an overriding implementation that casts to float and back.
 
-
-@torch_op("aten::addmm")
-def aten_addmm_gemm(
-    self: TFloat, mat1: TFloat, mat2: TFloat, beta: float = 1.0, alpha: float = 1.0
-) -> TFloat:
-    """addmm(Tensor self, Tensor mat1, Tensor mat2, *, Scalar beta=1, Scalar alpha=1) -> Tensor"""
-
-    # A special case when rank of mat1 and mat2 are 2, we can use Gemm instead of MatMul
-    # We expect the if branches to be folded away by optimization passes
-    # TODO(#1110): Handle Gemm with a graph rewriting pass instead of hard coding the branching logic here
-    use_gemm = op.And(
-        op.Equal(Rank(mat1), op.Constant(value_int=2)),
-        op.Equal(Rank(mat2), op.Constant(value_int=2)),
-    )
-    if use_gemm:
-        result = op.Gemm(mat1, mat2, self, alpha=alpha, beta=beta)
-    else:
-        mat1_mat2 = op.MatMul(mat1, mat2)
-        scaled_mat1_mat2 = op.Mul(mat1_mat2, alpha)
-        scaled_self = op.Mul(self, beta)
-        result = op.Add(scaled_self, scaled_mat1_mat2)
-    return result
+    # addmm only accepts 2d tensors: https://pytorch.org/docs/stable/generated/torch.addmm.html
+    return op.Gemm(mat1, mat2, self, alpha=alpha, beta=beta)
 
 
 @torch_op("aten::addmv")
