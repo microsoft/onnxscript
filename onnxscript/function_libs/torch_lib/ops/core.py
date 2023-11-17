7518840b8ecd7d1ec54316f4aad3bcc3ea4d89fd
Justin Chu <justinchuby@users.noreply.github.com>
2023-11-16 16:41:00 -0800

    Update core.py

--- onnxscript/function_libs/torch_lib/ops/core.py
+++ onnxscript/function_libs/torch_lib/ops/core.py
@@ -7731,6 +7731,13 @@ def aten_sym_size(self: TReal, dim: int = 0) -> TReal:
     # NOTE: onnxscript doesn't support attribute process,
     # so op.Shape(self, start=dim, end=dim + 1) is not supported.
 
+    # TODO(titaiwang): ORT==1.15 fixes SegFault
+    # https://github.com/microsoft/onnxscript/pull/484#discussion_r1136105039
+    # Change the op to:
+    # shape = op.Shape(self)
+    # idx= op.Reshape(dim, [1])
+    # return op.Gather(shape, idx)
+
     shape = op.Shape(self)
     # Reshape helps dim from int to tensor, and
     # input arguments support attribute processing.
