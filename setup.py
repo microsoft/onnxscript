ab9b10cb545e46ddccb8890bcc1d0436882a6333
twata <twata@preferred.jp>
2023-11-20 13:35:16 +0900

    lint

--- setup.py
+++ setup.py
@@ -23,9 +23,11 @@ if os.environ.get("ONNX_SCRIPT_RELEASE") != "1":
     date = datetime.date.today().strftime("%Y%m%d")
     version = f"{version}.dev{date}"
 
-    commit_hash_cmd = subprocess.run(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE)
+    commit_hash_cmd = subprocess.run(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, check=False)
     if commit_hash_cmd.returncode == 0:
-        project_urls["Commit"] = f"https://github.com/microsoft/onnxscript/tree/{commit_hash_cmd.stdout.decode('utf-8').strip()}"
+        project_urls[
+            "Commit"
+        ] = f"https://github.com/microsoft/onnxscript/tree/{commit_hash_cmd.stdout.decode('utf-8').strip()}"
 
 # NOTE: Do not include other metadata in setup.py. Put it in pyproject.toml.
 setuptools.setup(version=version, project_urls=project_urls, url="https://onnxscript.ai/")
