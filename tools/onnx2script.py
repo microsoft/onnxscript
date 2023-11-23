83fac650b24e2e775b3ac11859f7a6fe45e34f26
Ganesan Ramalingam <grama@microsoft.com>
2023-11-23 09:00:49 -0800

    Minor fixes

--- tools/onnx2script.py
+++ tools/onnx2script.py
@@ -7,27 +7,27 @@ import argparse
 import onnx
 import onnxscript
 import os
+from typing import Optional
 
 
-def convert2script() -> None:
+def convert2script(input_file_name: str, output_file_name: Optional[str], verbose: bool) -> None:
+    model = onnx.load(input_file_name, load_external_data=False)
+    python_code = onnxscript.proto2python(model, use_operators = not verbose, inline_constants = not verbose)
+
+    # If output file name is not provided, use the input file name with .py extension
+    if output_file_name is None:
+        base_name = os.path.splitext(input_file_name)[0]  # Remove extension
+        output_file_name = base_name + ".py"
+
+    with open(output_file_name, "w") as f:
+        f.write(python_code)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert ONNX model file to onnxscript file")
     parser.add_argument("input", help="ONNX model file to convert")
     parser.add_argument("-o", "--output", help="Output file name")
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode", default=False)
 
     args = parser.parse_args()
-
-    # If output file name is not provided, use the input file name with .py extension
-    if args.output is None:
-        base_name = os.path.splitext(args.input)[0]  # Remove extension
-        args.output = base_name + ".py"
-
-    model = onnx.load(args.input, load_external_data=False)
-    python_code = onnxscript.proto2python(model, use_operators = not args.verbose, inline_constants = not args.verbose)
-
-    with open(args.output, "w") as f:
-        f.write(python_code)
-
-
-if __name__ == "__main__":
-    convert2script()
+    convert2script(args.input, args.output, args.verbose)
