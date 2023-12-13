98ea1ad5c6f97b9ff44efa2f1cbffd1ab8042bd9
Justin Chu <justinchuby@users.noreply.github.com>
2023-12-12 16:22:53 -0800

    Update values.py

--- onnxscript/values.py
+++ onnxscript/values.py
@@ -108,7 +108,6 @@ class Opset:
 
     def add_function_def(self, fun):
         if fun.name in self.function_defs:
-            logger = logging.getLogger("onnxscript")
             logger.warning("%s: Already defined.", fun.name)
         self.function_defs[fun.name] = fun
 
