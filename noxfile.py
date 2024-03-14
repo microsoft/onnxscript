ae9443776f4dff55e9e896ed225c0b1dd6178314
Justin Chu <justinchuby@users.noreply.github.com>
2024-03-13 18:19:46 -0700

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -27,7 +27,7 @@ COMMON_TEST_DEPENDENCIES = (
     "pyyaml",
 )
 ONNX = "onnx==1.14.1"
-ONNX_RUNTIME = "onnxruntime==1.17.1"
+ONNX_RUNTIME = "onnxruntime==1.16.1"
 PYTORCH = "torch==2.1.0"
 TORCHVISON = "torchvision==0.16"
 ONNX_RUNTIME_NIGHTLY_DEPENDENCIES = (
