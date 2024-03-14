868f95e9f2935cf95bbb8b0a4f959058bcf11fa8
Justin Chu <justinchuby@users.noreply.github.com>
2024-03-14 14:36:41 -0700

    Turn on TORCHLIB_EXPERIMENTAL_PREFER_TRACING
    This is a BC breaking change. The flag `TORCHLIB_EXPERIMENTAL_PREFER_TRACING` allows most functions with control flows to be traced by default. This will eliminate some aten functions from the graph. We will

--- onnxscript/function_libs/torch_lib/_flags.py
+++ onnxscript/function_libs/torch_lib/_flags.py
@@ -15,6 +15,7 @@ def _load_boolean_flag(
     *,
     this_will: str,
     deprecated: bool = False,
+    default: bool = False,
 ) -> bool:
     """Load a boolean flag from environment variable.
 
@@ -23,8 +24,9 @@ def _load_boolean_flag(
         this_will: A string that describes what this flag will do.
         deprecated: Whether this flag is deprecated.
     """
+    undefined = os.getenv(name) is None
     state = os.getenv(name) == "1"
-    if state:
+    if turned_on:
         if deprecated:
             logger.error(
                 "Experimental flag %s is deprecated. Please remove it from your environment.",
@@ -32,6 +34,8 @@ def _load_boolean_flag(
             )
         else:
             logger.warning("Experimental flag %s is enabled. This will %s.", name, this_will)
+    if undefined:
+        state = default
     return state
 
 
@@ -42,4 +46,5 @@ EXPERIMENTAL_INITIALIZERS_AS_INPUTS: bool = _load_boolean_flag(
 EXPERIMENTAL_PREFER_TRACING: bool = _load_boolean_flag(
     "TORCHLIB_EXPERIMENTAL_PREFER_TRACING",
     this_will="trace all traceable functions to fold if branches and collapse constant expressions",
+    default=True,
 )
