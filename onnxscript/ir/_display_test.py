a950a6387e34ab895e4b12135c3b4a40eadbc29d
Justin Chu <justinchu@microsoft.com>
2024-04-01 17:52:27 +0000

    [IR] Migrate the new IR from onnx-rewriter

---
+++ onnxscript/ir/_display_test.py
@@ -0,0 +1,20 @@
+"""Test display() methods in various classes."""
+
+import contextlib
+import unittest
+
+import numpy as np
+
+import onnxscript.ir as ir
+
+
+class DisplayTest(unittest.TestCase):
+    def test_tensor_display_does_not_raise_on_nan_values(self):
+        array_with_nan = np.array([np.inf, -np.inf, np.nan, 5, -10])
+        tensor = ir.Tensor(array_with_nan, dtype=ir.DataType.FLOAT)
+        with contextlib.redirect_stdout(None):
+            tensor.display()
+
+
+if __name__ == "__main__":
+    unittest.main()
