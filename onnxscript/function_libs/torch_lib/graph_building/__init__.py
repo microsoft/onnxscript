d43c870965a46906b8e51395af029d6e8e9208de
Justin Chu <justinchu@microsoft.com>
2024-04-09 02:13:11 +0000

    [WIP] Use the new IR for building graphs in torchlib

---
+++ onnxscript/function_libs/torch_lib/graph_building/__init__.py
@@ -0,0 +1,23 @@
+
+from __future__ import annotations
+
+__all__ = [
+    "TorchScriptTensor",
+    "TorchScriptGraph",
+    "TorchScriptTracingEvaluator",
+]
+
+from onnxscript.function_libs.torch_lib import _flags
+
+if _flags.EXPERIMENTAL_USE_IR:
+    from ._graph_building_ir import (
+        TorchScriptGraph,
+        TorchScriptTensor,
+        TorchScriptTracingEvaluator,
+    )
+else:
+    from ._graph_building_torch import (
+        TorchScriptGraph,
+        TorchScriptTensor,
+        TorchScriptTracingEvaluator,
+    )
