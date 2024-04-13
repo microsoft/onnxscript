b3e0d005cad74b9cff22f0d8b96d13ca19dd1fa0
Justin Chu <justinchu@microsoft.com>
2024-04-13 03:59:46 +0000

    lint

--- onnxscript/rewriter/generic_pattern.py
+++ onnxscript/rewriter/generic_pattern.py
@@ -9,6 +9,7 @@ import typing
 import onnx
 import onnx.helper as oh
 
+import onnxscript
 import onnxscript._legacy_ir as oir
 import onnxscript.rewriter.pattern as orp
 
@@ -1120,7 +1121,7 @@ def make_pattern_rule(
     apply_pattern: typing.Callable,
     validate_mapping: typing.Callable | None = None,
     verbose: int = 0,
-    opsets: dict[str, "onnxscript.Opset"] | None = None,  # noqa: F821
+    opsets: dict[str, onnxscript.values.Opset] | None = None,
 ) -> orp.RewriteRule:
     """
     Creates a rewriting rule.
