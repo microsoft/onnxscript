9fbc8ee62f400ea49665f913e4243c57b42aa27c
Justin Chu <justinchu@microsoft.com>
2024-04-17 15:25:34 +0000

    Use UserDict to simplify metadatastore

--- onnxscript/ir/_metadata.py
+++ onnxscript/ir/_metadata.py
@@ -6,10 +6,11 @@
 
 from __future__ import annotations
 
-from typing import Any, Iterator, Mapping
+import collections
+from typing import Any
 
 
-class MetadataStore(Mapping[str, Any]):
+class MetadataStore(collections.UserDict):
     """Class for storing metadata about the IR objects.
 
     Metadata is stored as key-value pairs. The keys are strings and the values
@@ -19,41 +20,13 @@ class MetadataStore(Mapping[str, Any]):
     when a pass wants to mark a key that needs to be recomputed.
     """
 
-    __slots__ = ["_store", "_invalid_keys"]
-
     def __init__(self) -> None:
-        self._store: dict[str, Any] = {}
+        super().__init__()
         self._invalid_keys: set[str] = set()
 
-    def __getitem__(self, key: str) -> Any:
-        # We do not check validity here because we want to allow
-        # users to access invalid keys
-        return self._store[key]
-
-    def get(self, key: str, default: Any = None) -> Any:
-        return self._store.get(key, default)
-
-    def items(self):
-        return self._store.items()
-
-    def keys(self):
-        return self._store.keys()
-
-    def values(self):
-        return self._store.values()
-
-    def __iter__(self) -> Iterator[str]:
-        return self._store.__iter__()
-
-    def __len__(self) -> int:
-        return len(self._store)
-
     def __setitem__(self, key: str, value: Any) -> None:
-        self._store[key] = value
+        self.data[key] = value
         self._invalid_keys.discard(key)
-
-    def __contains__(self, key: str) -> bool:
-        return key in self._store
 
     def invalidate(self, key: str) -> None:
         self._invalid_keys.add(key)
@@ -70,8 +43,4 @@ class MetadataStore(Mapping[str, Any]):
         return key not in self._invalid_keys
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(store={self._store}, invalid_keys={self._invalid_keys})"
-
-    def to_metadata_props(self) -> dict[str, str]:
-        # TODO(justinchuby): Handle invalid keys
-        return {k: str(v) for k, v in self.items()}
+        return f"{self.__class__.__name__}({self.data!r}, invalid_keys={self._invalid_keys!r})"
