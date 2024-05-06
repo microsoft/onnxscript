395e6b7cef046d057023c802f58929d88b33f8c2
titaiwangms <titaiwang@microsoft.com>
2024-05-06 18:58:29 +0000

    simplify loop of inserting casts to only one cast in input

--- onnxscript/rewriter/onnxruntime/bfloat16_utils/bfloat16_converter.py
+++ onnxscript/rewriter/onnxruntime/bfloat16_utils/bfloat16_converter.py
@@ -10,38 +10,47 @@ def _convert_inputs_from_bfloat16_to_float16(value: ir.Input) -> None:
     if value.dtype != ir.DataType.BFLOAT16:
         return
     value.dtype = ir.DataType.FLOAT16
-    value_users = tuple(value.uses())
-    for node, index in value_users:
-        _insert_cast_nodes_for_float16_to_bfloat16_to_inputs(node, index)
+    _insert_cast_nodes_for_float16_to_bfloat16_to_inputs(value)
 
 
 def _convert_outputs_from_bfloat16_to_float16(value: ir.Value) -> None:
     if value.dtype != ir.DataType.BFLOAT16:
         return
+    _insert_cast_nodes_for_bfloat16_to_float16_to_outputs(value)
+
+
+def _insert_cast_nodes_for_float16_to_bfloat16_to_inputs(value: ir.Input) -> None:
+    user_nodes_and_indices = tuple(value.uses())
+
+    attr = ir.AttrInt64(name="to", value=ir.DataType.BFLOAT16)
+    cast = ir.Node(
+        domain="",
+        op_type="Cast",
+        inputs=[value],
+        num_outputs=1,
+        attributes=[attr],
+    )
+    cast.outputs[0].dtype = ir.DataType.BFLOAT16
+    cast.outputs[0].shape = value.shape
+
+    for node, index in tuple(value.uses()):
+        if node == cast:
+            continue
+        node.replace_input_with(index, cast.outputs[0])
+
+    # NOTE: A safer way to insert the cast node is to prepend it to the first node
+    # of the graph
+    assert user_nodes_and_indices[0][0].graph is not None, "The node should belong to a graph"
+    user_nodes_and_indices[0][0].graph[0].prepend(cast)
+
+
+def _insert_cast_nodes_for_bfloat16_to_float16_to_outputs(value: ir.Value) -> None:
     node = value.producer()
     index = value.index()
     if node is None or index is None:
         logger.warning("Output value %s has no producer or index", value)
         return
-    _insert_cast_nodes_for_bfloat16_to_float16_to_outputs(node, index)
 
-
-def _insert_cast_nodes_for_float16_to_bfloat16_to_inputs(node: ir.Node, index: int) -> None:
-    attr = ir.AttrInt64(name="to", value=ir.DataType.BFLOAT16)
-    cast = ir.Node(
-        domain="",
-        op_type="Cast",
-        inputs=[node.inputs[index]],
-        num_outputs=1,
-        attributes=[attr],
-    )
-    cast.outputs[0].dtype = ir.DataType.BFLOAT16  # type: ignore[union-attr]
-    cast.outputs[0].shape = node.inputs[index].shape  # type: ignore[union-attr]
-    node.prepend(cast)
-    node.replace_input_with(index, cast.outputs[0])
-
-
-def _insert_cast_nodes_for_bfloat16_to_float16_to_outputs(node: ir.Node, index: int) -> None:
     attr = ir.AttrInt64(name="to", value=ir.DataType.FLOAT16)
     cast = ir.Node(
         domain="",
@@ -54,7 +63,7 @@ def _insert_cast_nodes_for_bfloat16_to_float16_to_outputs(node: ir.Node, index: 
     cast.outputs[0].shape = node.outputs[index].shape
     # To prevent naming conflicts, we need to append suffix to the output name of the cast node
     # TODO: Remove this after naming authority covers this case
-    cast.outputs[0].name = node.outputs[index].name + CREATED_CAST_BFLOAT16_NAME_SUFFIX
+    cast.outputs[0].name = node.outputs[index].name + CREATED_CAST_BFLOAT16_NAME_SUFFIX  # type: ignore[operator]
     node.append(cast)
 
     assert node.graph is not None, "Node graph should not be None"
