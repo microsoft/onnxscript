b63a005b153fd37a9f687ef7ef603f7010594605
Justin Chu <justinchuby@users.noreply.github.com>
2024-05-14 11:44:03 -0700

    Update onnxscript/ir/_name_authority.py

--- onnxscript/ir/_name_authority.py
+++ onnxscript/ir/_name_authority.py
@@ -19,7 +19,6 @@ class NameAuthority:
     the name authority will not change its name.
     It is the responsibility of the user to ensure that the names are unique
     (typically by running a name-fixing pass on the graph).
-    
     TODO(justichuby): Describe the pass when we have a reference implementation.
     """
 
