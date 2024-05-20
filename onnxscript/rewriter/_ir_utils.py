9801f6f418892fb12224519514058cfccc5c9ccf
Justin Chu <justinchu@microsoft.com>
2024-05-20 06:39:06 +0000

    Simplify propagate_const_value

--- onnxscript/rewriter/_ir_utils.py
+++ onnxscript/rewriter/_ir_utils.py
@@ -14,40 +14,33 @@ GRAPH_OUTPUT_META_KEY = "pkg.onnxscript.rewriter.generic_pattern.graph_output"
 def propagate_const_value(ir_value: ir.Value) -> ir.Value:
     """Temporary method to propagate a constant value to the IR value."""
     node = ir_value.producer()
-    if ir_value.const_value is None and node is not None and node.op_type == "Constant":
-        for attr_name in (
-            "value_float",
-            "value_int",
-            "value_string",
-            "value",
-            "value_floats",
-            "value_ints",
-            "value_strings",
-        ):
-            attr_value = node.attributes.get(attr_name)
-            if attr_value is None or not isinstance(attr_value, ir.Attr):
-                continue
+    if node is None:
+        return ir_value
+    if node.op_type != "Constant":
+        return ir_value
+    attr_name, attr_value = next(iter(node.attributes.items()))
+    if attr_value is None or not isinstance(attr_value, ir.Attr):
+        return ir_value
 
-            const_value: ir.TensorProtocol
-            if attr_name in {"value_float", "value_floats"}:
-                const_value = ir.Tensor(
-                    np.array(attr_value.value, dtype=np.float32), name=ir_value.name
-                )
-            elif attr_name in {"value_int", "value_ints"}:
-                const_value = ir.Tensor(
-                    np.array(attr_value.value, dtype=np.int64), name=ir_value.name
-                )
-            elif attr_name in {"value_string", "value_strings"}:
-                const_value = ir.StringTensor(
-                    np.array(attr_value.value, dtype=np.bytes_), name=ir_value.name
-                )
-            elif attr_name == "value":
-                const_value = typing.cast(ir.TensorProtocol, attr_value.value)
+    const_value: ir.TensorProtocol
+    if attr_name in {"value_float", "value_floats"}:
+        const_value = ir.Tensor(
+            np.array(attr_value.value, dtype=np.float32), name=ir_value.name
+        )
+    elif attr_name in {"value_int", "value_ints"}:
+        const_value = ir.Tensor(np.array(attr_value.value, dtype=np.int64), name=ir_value.name)
+    elif attr_name in {"value_string", "value_strings"}:
+        const_value = ir.StringTensor(
+            np.array(attr_value.value, dtype=np.bytes_), name=ir_value.name
+        )
+    elif attr_name == "value":
+        const_value = typing.cast(ir.TensorProtocol, attr_value.value)
+    else:
+        return ir_value
 
-            ir_value.const_value = const_value
-            ir_value.shape = const_value.shape  # type: ignore
-            ir_value.dtype = const_value.dtype
-            break
+    ir_value.const_value = const_value
+    ir_value.shape = const_value.shape  # type: ignore
+    ir_value.dtype = const_value.dtype
     return ir_value
 
 
