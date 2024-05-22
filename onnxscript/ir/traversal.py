d99af82a82f5c5dac4a59a307b01ba862cb9d4fb
Justin Chu <justinchu@microsoft.com>
2024-05-22 22:49:32 +0000

    update

--- onnxscript/ir/traversal.py
+++ onnxscript/ir/traversal.py
@@ -2,46 +2,66 @@
 
 from __future__ import annotations
 
+from typing_extensions import Self
+
 from onnxscript.ir import _core, _enums
 
 __all__ = [
-    "recursive_node_iter",
+    "RecursiveNodeIterator",
 ]
 
-from typing import Callable, Iterable, Iterator
+from typing import Callable, Iterator
 
 
-class _RecursiveNodeIterator(Iterable[_core.Node]):
-    def __init__(self,
+class RecursiveNodeIterator(Iterator[_core.Node]):
+    def __init__(
+        self,
         graph: _core.Graph,
         enter_graph_handler: Callable[[_core.Graph], None],
         exit_graph_handler: Callable[[_core.Graph], None],
         recursive: Callable[[_core.Node], bool],
         reversed: bool = False,
     ):
+        """Iterate over the nodes in the graph, recursively visiting subgraphs.
+
+        Args:
+            graph: The graph to traverse.
+            enter_graph_handler: A callback that is called when a subgraph is entered.
+            exit_graph_handler: A callback that is called when a subgraph is exited.
+            recursive: A callback that determines whether to recursively visit a node.
+            reversed: Whether to iterate in reverse order.
+        """
         self._graph = graph
         self._enter_graph_handler = enter_graph_handler
         self._exit_graph_handler = exit_graph_handler
         self._recursive = recursive
         self._subgraph_stack = []
         self._reversed = reversed
+        self._iterator = self._recursive_node_iter(graph)
 
-    def __iter__(self) -> Iterator[_core.Node]:
-        self._enter_graph_handler(self._graph)
-        iterable = reversed(self._graph) if self._reversed else self._graph
+    def __iter__(self) -> Self:
+        self._iterator = self._recursive_node_iter(self._graph)
+        return self
+
+    def __next__(self) -> _core.Node:
+        return next(self._iterator)
+
+    def _recursive_node_iter(self, graph: _core.Graph) -> Iterator[_core.Node]:
+        self._enter_graph_handler(graph)
+        iterable = reversed(graph) if self._reversed else graph
         for node in iterable:
             yield node
             if not self._recursive(node):
                 continue
             yield from self._iterate_subgraphs(node)
-        self._exit_graph_handler(self._graph)
+        self._exit_graph_handler(graph)
 
     def _iterate_subgraphs(self, node: _core.Node):
         for attr in node.attributes.values():
             if not isinstance(attr, _core.Attr):
                 continue
             if attr.type == _enums.AttributeType.GRAPH:
-                yield from _RecursiveNodeIterator(
+                yield from RecursiveNodeIterator(
                     attr.value,
                     self._enter_graph_handler,
                     self._exit_graph_handler,
@@ -50,7 +70,7 @@ class _RecursiveNodeIterator(Iterable[_core.Node]):
                 )
             elif attr.type == _enums.AttributeType.GRAPHS:
                 for graph in attr.value:
-                    yield from _RecursiveNodeIterator(
+                    yield from RecursiveNodeIterator(
                         graph,
                         self._enter_graph_handler,
                         self._exit_graph_handler,
@@ -59,26 +79,10 @@ class _RecursiveNodeIterator(Iterable[_core.Node]):
                     )
 
     def __reversed__(self) -> Iterator[_core.Node]:
-        return _RecursiveNodeIterator(
+        return RecursiveNodeIterator(
             self._graph,
             self._enter_graph_handler,
             self._exit_graph_handler,
             self._recursive,
             reversed=not self._reversed,
         )
-
-
-def recursive_node_iter(
-    graph: _core.Graph,
-    enter_graph_handler: Callable[[_core.Graph], None],
-    exit_graph_handler: Callable[[_core.Graph], None],
-    recursive: Callable[[_core.Node], bool],
-) -> Iterator[_core.Node]:
-    """Iterate over the nodes in the graph, recursively visiting subgraphs.
-
-    Args:
-        graph: The graph to traverse.
-        enter_graph_handler: A callback that is called when a subgraph is entered.
-        exit_graph_handler: A callback that is called when a subgraph is exited.
-        recursive: A callback that determines whether to recursively visit a node.
-    """
