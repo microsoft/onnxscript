7e9c293a914a9444acbda5fdf6360c98943d1c45
Justin Chu <justinchu@microsoft.com>
2024-06-01 16:19:19 +0000

    format

--- onnxscript/ir/convenience_test.py
+++ onnxscript/ir/convenience_test.py
@@ -6,7 +6,6 @@ from onnxscript.ir import convenience
 
 
 class NamespaceTest(unittest.TestCase):
-
     def test_module_members_is_public(self):
         for name in convenience.__all__:
             function = getattr(convenience, name)
