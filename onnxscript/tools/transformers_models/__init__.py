588f880ebc123dd82abe63d1da100ce2c93fabea
Xavier Dupré <xadupre@users.noreply.github.com>
2024-06-06 11:03:03 +0200

    Update onnxscript/tools/transformers_models/__init__.py
    Co-authored-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxscript/tools/transformers_models/__init__.py
+++ onnxscript/tools/transformers_models/__init__.py
@@ -24,7 +24,6 @@ def has_transformers():
 def ids_tensor(shape, vocab_size, rng=None, name=None):
     """Creates a random int32 tensor of the shape within the vocab size."""
     del name  # unused
-    import torch
 
     if rng is None:
         rng = random.Random()
