5b04b5cdfdb7e57c1223af420fc18061e5bf2df1
Xavier Dupre <xadupre@microsoft.com>
2024-06-14 12:05:07 +0200

    version
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- noxfile.py
+++ noxfile.py
@@ -17,7 +17,7 @@ COMMON_TEST_DEPENDENCIES = (
     "expecttest==0.1.6",
     "hypothesis",
     'numpy==1.24.4; python_version<"3.9"',
-    'numpy==1.26.0; python_version>="3.9"',
+    'numpy==1.26.4; python_version>="3.9"',
     "packaging",
     "parameterized",
     "pyinstrument",
@@ -32,8 +32,8 @@ COMMON_TEST_DEPENDENCIES = (
     "ml_dtypes",
 )
 ONNX = "onnx==1.16"
-ONNX_RUNTIME = "onnxruntime==1.17.3"
-ONNX_RUNTIME_TRAINING = "onnxruntime-training==1.17.0"
+ONNX_RUNTIME = "onnxruntime==1.17.1"
+ONNX_RUNTIME_TRAINING = "onnxruntime-training==1.17.1"
 PYTORCH = "torch==2.2.2"
 TORCHVISON = "torchvision==0.17.2"
 TRANSFORMERS = "transformers>=4.37.2"
@@ -86,7 +86,7 @@ def test_torch_nightly(session):
     session.install("-r", "requirements/ci/requirements-pytorch-nightly.txt")
     session.install(".", "--no-deps")
     if sys.platform == "linux":
-        session.install("numpy==1.25.1")
+        session.install("numpy==1.26.4")
         session.install("onnxruntime-training==1.17.0")
     session.run("pip", "list")
     session.run("pytest", "onnxscript", "--doctest-modules", *session.posargs)
