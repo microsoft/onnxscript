04de6d041f7fc568183342d3c2e4b907ec5c1ebf
Xavier Dupre <xadupre@microsoft.com>
2024-06-27 20:51:25 +0200

    Simple unit test to check backward function for conv
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

---
+++ onnxscript/function_libs/torch_lib/backward_test.py
@@ -0,0 +1,71 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# pylint: disable=not-callable
+
+import copy
+import sys
+import unittest
+
+import torch
+
+import onnxscript.tools.training_helper
+import onnxscript.tools.transformers_models
+import onnxscript.tools.transformers_models.llama
+from onnxscript._internal.version_utils import has_transformers, torch_older_than
+
+
+class TestBackward(unittest.TestCase):
+    @unittest.skipIf(sys.platform == "win32", reason="not supported yet on Windows")
+    @unittest.skipIf(not has_transformers(), reason="transformers is missing")
+    @unittest.skipIf(torch_older_than("2.4"), reason="fails to export")
+    def test_backward_conv(self):
+        class SimpleCNNN(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+                self.conv1 = torch.nn.Conv2d(
+                    in_channels=1, out_channels=32, kernel_size=3, stride=2, padding=1
+                )  # Output size 14x14 pour MNIST
+                self.conv2 = torch.nn.Conv2d(
+                    in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1
+                )  # Output size 7x7 pour MNIST
+
+                self.fc1 = torch.nn.Linear(64 * 7 * 7, 128)
+                self.fc2 = torch.nn.Linear(128, 10)
+
+            def forward(self, x):
+                x = torch.nn.functional.relu(self.conv1(x))
+                x = torch.nn.functional.relu(self.conv2(x))
+
+                x = x.view(-1, 64 * 7 * 7)
+
+                x = torch.nn.functional.relu(self.fc1(x))
+                x = self.fc2(x)
+
+                return x
+
+        input_tensors = (torch.randn(1, 1, 32, 32),)
+        model = SimpleCNNN()
+        expected = model(*input_tensors)
+
+        local_aot_ort = onnxscript.tools.training_helper.make_aot_ort(dynamic=False)
+
+        compiled_model = torch.compile(
+            copy.deepcopy(model),
+            backend=local_aot_ort,
+            dynamic=False,
+            fullgraph=True,
+        )
+
+        results = compiled_model(*input_tensors)
+        torch.testing.assert_allclose(expected[0], results[0], atol=1e-5, rtol=1e-5)
+
+        expected_gradients = onnxscript.tools.training_helper.train_loop(model, *input_tensors)
+        gradients = onnxscript.tools.training_helper.train_loop(compiled_model, *input_tensors)
+        torch.testing.assert_allclose(
+            expected_gradients[0], gradients[0], atol=1e-5, rtol=1e-5
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
