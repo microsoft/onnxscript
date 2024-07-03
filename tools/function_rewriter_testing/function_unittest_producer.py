e64813f494f1df1a2f128067b82823cd65dfeffd
Xavier Dupre <xadupre@microsoft.com>
2024-07-03 11:23:24 +0200

    lint
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- tools/function_rewriter_testing/function_unittest_producer.py
+++ tools/function_rewriter_testing/function_unittest_producer.py
@@ -72,14 +72,11 @@ class FunctionToKeepVisitor(visitor.ProtoVisitorCore):
         super().visit_model(model)
 
 
-FunctionMetaDict = Dict[Tuple[str, str], Tuple[List[str], List[str]]]
-
-
 class TargetFunctionMetaVisitor(visitor.ProtoVisitorCore):
     def __init__(self, function_keyword):
         self.function_keyword = function_keyword
         # Map from (domain, name) to (actual_input_names, actual_output_names)
-        self.function_meta: FunctionMetaDict = {}
+        self.function_meta: dict[tuple[str, str], tuple[list[str], list[str]]] = {}
         self._functions = {}
         super().__init__()
 
