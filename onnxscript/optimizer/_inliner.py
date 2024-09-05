883a80a7f22788b93bde1c5a6e37a7f10628bc08
Ganesan Ramalingam <grama@microsoft.com>
2024-09-05 16:27:14 -0700

    Remove unused import

--- onnxscript/optimizer/_inliner.py
+++ onnxscript/optimizer/_inliner.py
@@ -5,7 +5,6 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from dataclasses import dataclass, field
 from typing import Iterable, Sequence, Tuple
 
 import onnxscript.ir as ir
