b69dd83ef390b052469b453798947358acfb0345
Yichen <yichenli97@g.ucla.edu>
2024-09-06 00:33:58 -0400

    remove unused

--- onnxscript/ir/passes/_remove_unused.py
+++ onnxscript/ir/passes/_remove_unused.py
@@ -5,54 +5,78 @@
 from __future__ import annotations
 
 from collections import deque
-from typing import Union
 
 import onnxscript.ir as ir
-from onnxscript.ir import Graph, Node, Value
+from onnxscript.ir import Attr, Graph, Node, Value, _enums
 
 
 class RemoveUnused:
-    def __init__(self, graph_like: Union[Graph, ir.GraphView]):
+    def __init__(self, graph_like: Graph):
         self._graph = graph_like
 
     def purge(self) -> None:
-        """Remove unused nodes in this graph and all its subgraphs that do not contribute to any graph_outputs."""
-        # 1. Initialize
-        #   Gather all nodes from the graph and its subgraphs using a recursive iterator.
-        #   Identify all subgraphs by checking the graph of each node.
-        #   Initialize sets to keep track of visited values and nodes.
+        """Remove unused nodes in this graph (and all subgraphs) that do not contribute to main graph outputs."""
+        # 1. Initialize:
+        #   Gather all nodes from the graph and its subgraphs.
+        #   Initialize sets to keep track of visited graphs, values, and nodes.
         # 2. BFS traversal:
-        #   Create a queue initialized with all output values from the subgraphs.
+        #   Create a queue initialized with all output values of the main graph.
         #   While there are values in the queue:
         #     - Dequeue a value and retrieve its producer node.
-        #     - Skip processing if the producer node is already visited or doesn't exist.
-        #     - Mark the producer node as visited.
-        #     - Enqueue all input values of this producer node for further exploration, if they haven't been visited.
+        #     - Mark the producer node as visited, if it hasn't been visited.
+        #     - Enqueue all output values of the attribute subgraphs of the producer node,
+        #       if they haven't been visited.
+        #     - Enqueue all input values of the producer node, if they haven't been visited.
         # 3. Remove:
-        #   Remove any node from its graph if it has not been marked as visited during the BFS traversal.
+        #   Remove all nodes that have not been marked as visited during the BFS traversal.
 
         # Initialize
         all_nodes: list[Node] = list(ir.traversal.RecursiveGraphIterator(self._graph))
-        subgraphs: set[Graph] = {node.graph for node in all_nodes if node.graph}
+        visited_graphs: set[Graph] = set()
         visited_values: set[Value] = set()
         visited_nodes: set[Node] = set()
 
         # BFS Traversal
-        value_queue: deque[Value] = deque(
-            output for graph in subgraphs for output in graph.outputs if output
-        )
-        while value_queue:
-            current_value = value_queue.popleft()
+        queue: deque[Value] = deque()
+
+        def add_graph_output_values_to_queue(graph: Graph | None) -> None:
+            """Helper function to add all output values of a graph to the queue."""
+            if not graph or graph in visited_graphs:
+                return
+            visited_graphs.add(graph)
+            for output in graph.outputs:
+                if not output:
+                    continue
+                queue.append(output)
+                visited_values.add(output)
+
+        add_graph_output_values_to_queue(self._graph)
+
+        while queue:
+            # Dequeue a value and retrieve its producer_node
+            # Add producer_node to visited_nodes
+            current_value = queue.popleft()
             producer_node = current_value.producer()
             if not producer_node or producer_node in visited_nodes:
                 continue
             visited_nodes.add(producer_node)
+            # Add producer_node's subgraphs to visited_graphs
+            # Add subgraphs' output values to queue
+            for attr in producer_node.attributes.values():
+                if not isinstance(attr, Attr):
+                    continue
+                if attr.type == _enums.AttributeType.GRAPH:
+                    add_graph_output_values_to_queue(attr.value)
+                elif attr.type == _enums.AttributeType.GRAPHS:
+                    for subgraph in attr.value:
+                        add_graph_output_values_to_queue(subgraph)
+            # Add producer_node's input values to queue
             for input_value in producer_node.inputs:
                 if input_value and input_value not in visited_values:
                     visited_values.add(input_value)
-                    value_queue.append(input_value)
+                    queue.append(input_value)
 
         # Remove
         for node in all_nodes:
-            if node not in visited_nodes:
-                node.graph.remove(node)  # type: ignore[union-attr]
+            if node not in visited_nodes: # type: ignore[union-attr]`
+                node.graph.remove(node)
--- onnxscript/ir/passes/_remove_unused_test.py
+++ onnxscript/ir/passes/_remove_unused_test.py
@@ -50,7 +50,7 @@ class RemoveUnusedTest(unittest.TestCase):
         remove_unused.purge()
         self.assertEqual(tuple(graph), ())
 
-    def test_purge_subgraph(self):
+    def test_purge_subgraph_partial(self):
         v0 = ir.Value(name="va")
         v1 = ir.Value(name="vb")
         v2 = ir.Value(name="vc")
@@ -81,8 +81,7 @@ class RemoveUnusedTest(unittest.TestCase):
             inputs=(node6.outputs[0],),
             num_outputs=1,
             attributes=[
-                ir.AttrGraph("then_branch", then_graph),
-                ir.AttrGraph("else_branch", else_graph),
+                ir.AttrGraphs("subgraphs", [then_graph, else_graph]),
             ],
         )
         main_graph = ir.Graph(
@@ -97,6 +96,37 @@ class RemoveUnusedTest(unittest.TestCase):
         self.assertEqual(tuple(main_graph), (node0, node1, node2, node3, node6, node7))
         self.assertEqual(tuple(then_graph), (node4,))
         self.assertEqual(tuple(else_graph), ())
+
+    def test_purge_subgraph_all(self):
+        v0 = ir.Value(name="v0")
+        node0 = ir.Node("", "c", inputs=(v0,), num_outputs=1)
+        node1 = ir.Node("", "sub", inputs=(node0.outputs[0],), num_outputs=1)
+        node2 = ir.Node("", ">", inputs=(v0,), num_outputs=1)
+        then_graph = ir.Graph(
+            inputs=(node0.outputs[0],),
+            outputs=(node1.outputs[0],),
+            nodes=(node1,),
+            name="then_graph",
+        )
+        node4 = ir.Node(
+            "",
+            "if",
+            inputs=(node2.outputs[0],),
+            num_outputs=1,
+            attributes=[
+                ir.AttrGraph("then_graph", then_graph),
+            ],
+        )
+        main_graph = ir.Graph(
+            inputs=(v0,),
+            outputs=(),
+            nodes=(node0, node2, node4),
+            name="main_graph",
+        )
+        remove_unused = RemoveUnused(main_graph)
+        remove_unused.purge()
+        self.assertEqual(tuple(main_graph), ())
+        self.assertEqual(tuple(then_graph), ())
 
 
 if __name__ == "__main__":
