8c4dff5505d75df34a3e5163e416128fa59c7814
Ganesan Ramalingam <grama@microsoft.com>
2024-10-23 17:26:22 -0700

    MultiHeadAttention fusion

---
+++ onnxscript/rewriter/multi_head_attention.py
@@ -0,0 +1,55 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+from __future__ import annotations
+
+import numpy as np
+import onnxscript.ir as ir
+from onnxscript.rewriter import pattern
+
+"""
+MultiHeadAttention:
+
+for Q, K, V:
+   MatMul
+   Reshape to B, S, 32, 64
+   Transpose to B, 32, S, 64
+
+Here, 32 is the number of heads and 64 is the head size
+
+Embed Q and K
+
+One of the embeddings (namely ) is also output of layer
+and last two axes are transposed for SDPA
+
+"""
+
+def project_transpose_head(op, input, weight):
+   projected = op.MatMul(input, weight)
+   # Reshape from (B, S, D) to (B, S, H, D/H)
+   reshaped = op.Reshape(projected, _allow_other_inputs=True, _allow_other_attributes=True)
+   # Transpose from (B, S, H, D/H) to (B, H, S, D/H)
+   transposed = op.Transpose(reshaped, perm=[0, 2, 1, 3])
+   return transposed
+
+def multi_head_attention_pattern (op, input, query_weight, key_weight, value_weight):
+      query = project_transpose_head(op, input, query_weight)
+      query_rope = op.Embed(query, _domain="local")
+      key = project_transpose_head(op, input, key_weight)
+      key_rope = op.Embed(key, _domain="local")
+      # Transpose last two axes of key_rope to compute dot-product via matmul.
+      key_reshaped = op.Reshape(key_rope, _allow_other_inputs=True)
+      key_reshaped_transposed = op.Transpose(key_reshaped)
+      key_transposed = op.Reshape(key_reshaped_transposed, _allow_other_inputs=True)
+      value = project_transpose_head(op, input, value_weight)
+      attention = op.SDPA(query_rope, key_transposed, value, _allow_other_inputs=True, _domain="local")
+      # Transpose back to (B, S, H, D/H)
+      attention_transposed = op.Transpose(attention, perm=[0, 2, 1, 3])
+      # Reshape back to (B, S, D)
+      attention_reshaped = op.Reshape(attention_transposed, _allow_other_inputs=True)
+      return attention_reshaped, value, key_rope
+
+def multi_head_attention(op, input, query_weight, key_weight, value_weight):
+    # TODO: other checks and concatenation of weights
+    return op.MultiHeadAttention(input, query_weight, key_weight, value_weight, _domain="local", _outputs=3)
+
+rule = pattern.RewriteRule(multi_head_attention_pattern, multi_head_attention)
