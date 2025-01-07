41aa17752fce10c626c6e0c7484ffff040edd36b
Ganesan Ramalingam <grama@microsoft.com>
2025-01-07 13:44:46 -0800

    Fix merge conflict

--- onnxscript/rewriter/onnxruntime/xformers/cos_sin_cache_test.py
+++ onnxscript/rewriter/onnxruntime/xformers/cos_sin_cache_test.py
@@ -6,21 +6,13 @@ import unittest
 
 import onnxscript.optimizer
 from onnxscript.rewriter.onnxruntime.xformers import fuse_cos_sin_cache, fuse_rotary_embedding
-<<<<<<< HEAD
 from onnxscript.rewriter.onnxruntime.xformers._smollm_1 import TestData
-=======
-from onnxscript.rewriter.onnxruntime.xformers._smollm_1layer import _SmollmTestData
->>>>>>> main
 from onnxscript.rewriter.onnxruntime.xformers._test_utils import assert_allclose, ort_run
 
 
 class TestCosSinCacheTransform(unittest.TestCase):
     def test_smollm(self):
-<<<<<<< HEAD
         smollm_test = TestData()
-=======
-        smollm_test = _SmollmTestData()
->>>>>>> main
         model = smollm_test.get_onnx_model()
         onnxscript.optimizer.optimize(model)
         inputs = smollm_test.get_ort_inputs()
