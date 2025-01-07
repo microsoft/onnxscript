e0f29e2222ace6a39a4badbc9a36f9c1431d7df6
Ganesan Ramalingam <grama@microsoft.com>
2025-01-07 13:43:17 -0800

    Fix merge conflict

--- onnxscript/rewriter/onnxruntime/xformers/cos_sin_cache.py
+++ onnxscript/rewriter/onnxruntime/xformers/cos_sin_cache.py
@@ -41,14 +41,11 @@ class CosSinCacheFusion(pattern.RewriteRuleClassBase):
         # pass to remove unused nodes.
         super().__init__(name, remove_nodes=False)
         self._max_pos_id = max_pos_id
-<<<<<<< HEAD
         # map from inv_freq to (cos, sin) values for transformed graph
         self._inv_freq_cos_sin_cache: dict[ir.Value, tuple[ir.Value, ir.Value]] = {}
 
     def cleanup(self):
         self._inv_freq_cos_sin_cache.clear()
-=======
->>>>>>> main
 
     def pattern(self, op, x, inv_freq, position_ids, interleaved, num_heads):
         position_ids_expanded = op.Unsqueeze(position_ids, 1)
@@ -69,11 +66,7 @@ class CosSinCacheFusion(pattern.RewriteRuleClassBase):
             _domain="ai.onnxruntime.fusion",
         )
 
-<<<<<<< HEAD
-    def check(self, context, inv_freq, position_ids, **_):
-=======
     def check(self, context, inv_freq, position_ids, **_) -> bool:
->>>>>>> main
         if not _ir_utils.has_rank(position_ids, 2):
             return False
         if not _ir_utils.has_rank(inv_freq, 3):
@@ -84,7 +77,6 @@ class CosSinCacheFusion(pattern.RewriteRuleClassBase):
         return inv_freq_shape[0] == 1 and inv_freq_shape[2] == 1
 
     def rewrite(self, op, x, inv_freq, position_ids, interleaved, num_heads, **_):
-<<<<<<< HEAD
         if inv_freq in self._inv_freq_cos_sin_cache:
             cos_2d, sin_2d = self._inv_freq_cos_sin_cache[inv_freq]
         else:
@@ -96,15 +88,6 @@ class CosSinCacheFusion(pattern.RewriteRuleClassBase):
             cos_2d = op.Constant(value=ir.tensor(cos_value))
             sin_2d = op.Constant(value=ir.tensor(sin_value))
             self._inv_freq_cos_sin_cache[inv_freq] = (cos_2d, sin_2d)
-=======
-        inv_freq_values = inv_freq.const_value.numpy().reshape(1, -1)
-        pos_id_range = np.arange(self._max_pos_id, dtype=np.float32).reshape(-1, 1)
-        angles = np.matmul(pos_id_range, inv_freq_values)
-        cos_value = np.cos(angles)
-        sin_value = np.sin(angles)
-        cos_2d = op.Constant(value=ir.tensor(cos_value))
-        sin_2d = op.Constant(value=ir.tensor(sin_value))
->>>>>>> main
         return op.RotaryEmbedding(
             x,
             position_ids,
