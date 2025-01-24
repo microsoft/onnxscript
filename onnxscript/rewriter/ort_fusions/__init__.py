ad2332fc643ca5efaf41ec713f621192d724ed5b
Ganesan Ramalingam <grama@microsoft.com>
2025-01-24 14:06:35 -0800

    Remove onnxruntime init

--- onnxscript/rewriter/ort_fusions/__init__.py
+++ onnxscript/rewriter/ort_fusions/__init__.py
@@ -4,6 +4,22 @@
 
 __all__ = [
     "optimize_for_ort",
+    "transformers",
 ]
 
+from onnxscript.rewriter.ort_fusions import (
+    fused_matmul_rule_sets,
+    # group_normalization_merge_silu,
+    instance_to_group_normalization,
+    softmax,
+    transformers,
+)
 from onnxscript.rewriter.ort_fusions._core import optimize_for_ort
+
+ORT_PATTERN_REWRITE_RULES = [
+    *softmax.rules.rules,
+    *instance_to_group_normalization.rules.rules,
+    # NOTE: group normalization merge silu should be applied after instance to group normalization
+    # *group_normalization_merge_silu.rules.rules,
+    *fused_matmul_rule_sets.fused_matmul_rule_sets(),
+]
