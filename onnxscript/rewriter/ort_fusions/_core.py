807fe9b4f802c30516e4e3a7e986ce08295ca4a8
Ganesan Ramalingam <grama@microsoft.com>
2025-02-12 14:07:31 -0800

    Minor cleanup

--- onnxscript/rewriter/ort_fusions/_core.py
+++ onnxscript/rewriter/ort_fusions/_core.py
@@ -4,12 +4,27 @@ from __future__ import annotations
 
 import onnxscript.ir as ir
 from onnxscript.optimizer import optimize, remove_unused_nodes
+from onnxscript.rewriter import rewrite
+from onnxscript.rewriter.ort_fusions import (
+    fused_matmul_rule_sets,
+    # group_normalization_merge_silu,
+    instance_to_group_normalization,
+    softmax,
+)
 from onnxscript.rewriter.ort_fusions.cos_sin_cache import fuse_cos_sin_cache
 from onnxscript.rewriter.ort_fusions.mha import fuse_mha
 from onnxscript.rewriter.ort_fusions.rms_normalization import fuse_rms_normalization
 from onnxscript.rewriter.ort_fusions.rotary_embedding import fuse_rotary_embedding
 from onnxscript.rewriter.ort_fusions.sdpa import fuse_sdpa
 from onnxscript.rewriter.ort_fusions.skip_normalization import fuse_normalization
+
+ORT_PATTERN_REWRITE_RULES = [
+    *softmax.rules.rules,
+    *instance_to_group_normalization.rules.rules,
+    # NOTE: group normalization merge silu should be applied after instance to group normalization
+    # *group_normalization_merge_silu.rules.rules,
+    *fused_matmul_rule_sets.fused_matmul_rule_sets(),
+]
 
 
 def fuse_xformers(model: ir.Model) -> None:
@@ -24,5 +39,5 @@ def fuse_xformers(model: ir.Model) -> None:
 
 
 def optimize_for_ort(model: ir.Model) -> None:
-    # TODO(rama): Include the other optimizations
+    rewrite(model, ORT_PATTERN_REWRITE_RULES)
     fuse_xformers(model)
