dfee02eeb4fd9783e039e53d981f3e23a40ab30e
G. Ramalingam <grama@microsoft.com>
2025-02-12 17:09:56 -0800

    Minor cleanup (#2056)
    Minor cleanup of rewriter.ort_fusions.

--- onnxscript/rewriter/ort_fusions/__init__.py
+++ onnxscript/rewriter/ort_fusions/__init__.py
@@ -7,18 +7,5 @@ __all__ = [
     "ORT_PATTERN_REWRITE_RULES",
 ]
 
-from onnxscript.rewriter.ort_fusions import (
-    fused_matmul_rule_sets,
-    # group_normalization_merge_silu,
-    instance_to_group_normalization,
-    softmax,
-)
-from onnxscript.rewriter.ort_fusions._core import optimize_for_ort
 
-ORT_PATTERN_REWRITE_RULES = [
-    *softmax.rules.rules,
-    *instance_to_group_normalization.rules.rules,
-    # NOTE: group normalization merge silu should be applied after instance to group normalization
-    # *group_normalization_merge_silu.rules.rules,
-    *fused_matmul_rule_sets.fused_matmul_rule_sets(),
-]
+from onnxscript.rewriter.ort_fusions._core import ORT_PATTERN_REWRITE_RULES, optimize_for_ort
