0fbadb0d469f2eae7d913ae67f68d7a6550473b3
Ganesan Ramalingam <grama@microsoft.com>
2025-03-20 16:55:15 -0700

    Renaming

--- onnxscript/rewriter/ort_fusions/sdpa_test.py
+++ onnxscript/rewriter/ort_fusions/sdpa_test.py
@@ -17,18 +17,18 @@ from onnxscript.onnx_opset import opset18 as op
 from onnxscript.onnx_types import FLOAT
 from onnxscript.rewriter.ort_fusions.sdpa import fuse_sdpa
 
-batchsize = 2
-num_heads = 4
-seq_len = 8
-headsize = 128
-scale_factor = math.sqrt(headsize)
-sqrt_scale_factor = math.sqrt(scale_factor)
+B = 2  # batch size
+N = 4  # number of heads
+S = 8  # sequence length
+H = 128  # head size
+SCALE_FACTOR = math.sqrt(H)
+SQRT_SCALE_FACTOR = math.sqrt(SCALE_FACTOR)
 
 
 @script()
 def _masked_pre_div_sdpa_script(query, key, value, mask):
     key_transposed = op.Transpose(key, perm=[0, 1, 3, 2])
-    divisor = op.Constant(value_float=sqrt_scale_factor)
+    divisor = op.Constant(value_float=SQRT_SCALE_FACTOR)
     scaled_query = op.Div(query, divisor)
     scaled_key = op.Div(key_transposed, divisor)
     attn_score = op.MatMul(scaled_query, scaled_key)
@@ -41,8 +41,8 @@ def _masked_pre_div_sdpa_script(query, key, value, mask):
 class _MaskedPreDivSDPATestCase:
     def get_onnx_model(self):
         if not hasattr(self, "_onnx_model"):
-            qkv_type = FLOAT[batchsize, num_heads, seq_len, headsize]
-            mask_type = FLOAT[batchsize, num_heads, seq_len, seq_len]
+            qkv_type = FLOAT[B, N, S, H]
+            mask_type = FLOAT[B, N, S, S]
             model_proto = _masked_pre_div_sdpa_script.to_model_proto(
                 input_types=[qkv_type, qkv_type, qkv_type, mask_type], output_types=[qkv_type]
             )
@@ -53,18 +53,10 @@ class _MaskedPreDivSDPATestCase:
     def get_ort_inputs(self):
         if not hasattr(self, "_ort_inputs"):
             inputs = {
-                "query": numpy.random.rand(batchsize, num_heads, seq_len, headsize).astype(
-                    numpy.float32
-                ),
-                "key": numpy.random.rand(batchsize, num_heads, seq_len, headsize).astype(
-                    numpy.float32
-                ),
-                "value": numpy.random.rand(batchsize, num_heads, seq_len, headsize).astype(
-                    numpy.float32
-                ),
-                "mask": numpy.random.rand(batchsize, num_heads, seq_len, seq_len).astype(
-                    numpy.float32
-                ),
+                "query": numpy.random.rand(B, N, S, H).astype(numpy.float32),
+                "key": numpy.random.rand(B, N, S, H).astype(numpy.float32),
+                "value": numpy.random.rand(B, N, S, H).astype(numpy.float32),
+                "mask": numpy.random.rand(B, N, S, S).astype(numpy.float32),
             }
             self._ort_inputs = inputs
         return self._ort_inputs
