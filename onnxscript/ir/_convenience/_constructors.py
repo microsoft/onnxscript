eff55daec7007f8c969b5a9e5ea399e765aa38b7
Justin Chu <justinchuby@users.noreply.github.com>
2025-03-24 17:54:02 -0700

    Apply suggestions from code review

--- onnxscript/ir/_convenience/_constructors.py
+++ onnxscript/ir/_convenience/_constructors.py
@@ -97,10 +97,10 @@ def tensor(
         numpy_dtype = dtype.numpy()
     elif isinstance(value, float):
         # If the value is a single float, we use np.float32 as the default dtype
-        numpy_dtype = np.float32
+        numpy_dtype = np.dtype(np.float32)
     elif isinstance(value, Sequence) and all(isinstance(elem, float) for elem in value):
         # If the value is a sequence of floats, we use np.float32 as the default dtype
-        numpy_dtype = np.float32
+        numpy_dtype = np.dtype(np.float32)
     else:
         numpy_dtype = None
     array = np.array(value, dtype=numpy_dtype)
