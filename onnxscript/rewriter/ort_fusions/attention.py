909bea859930aa39d6310d69d984936cf5e3556d
shubhambhokare1 <shubhambhokare@gmail.com>
2025-03-28 22:33:59 +0000

    initial fusion rules

---
+++ onnxscript/rewriter/ort_fusions/attention.py
@@ -0,0 +1,193 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+from __future__ import annotations
+
+from typing import Sequence, Union
+
+import onnxscript.ir as ir
+from onnxscript.rewriter import pattern
+
+
+Dim = Union[int, ir.SymbolicDim]
+
+
+# TODO: Maybe add this check to utilities
+def _check_shape(bindings: dict[str, Dim], val: ir.Value, shape: Sequence[str]) -> bool:
+    if val.shape is None:
+        return False
+    if val.shape.rank() != len(shape):
+        return False
+    for actual, expected in zip(val.shape, shape):
+        if expected not in bindings:
+            bindings[expected] = actual  # type: ignore[assignment]
+        elif actual != bindings[expected]:
+            return False
+    return True
+
+
+class Attention(pattern.RewriteRuleClassBase):
+    def __init__(self, name, *, has_input_bias: bool):
+        super().__init__(name, remove_nodes=False)
+        # TODO: We can just pass bias to MultiHeadAttention
+        # and let it handle the bias addition, once that pattern is added to MHA
+        self._has_input_bias = has_input_bias
+
+    def pattern(
+        self,
+        op,
+        input,
+        qkv_weight,
+        qkv_bias,
+        # mask_index,
+        past,
+        attention_bias,
+        num_heads,
+        scale,
+        **_,
+    ):
+
+        projected = op.MatMul(input, qkv_weight)
+        # Add bias if present
+        if self._has_input_bias:
+            projected = op.Add(projected, qkv_bias)
+
+        # Slice packed Matmul QKV into Q, K, and V
+        # Q, K, and V are of shape (B, S, D)
+        query_BSD = op.Slice(
+            projected,
+            _allow_other_inputs=True,
+            _allow_other_attributes=True,
+            _outputs=["query_mm_sliced"],
+        )
+        key_BSD = op.Slice(
+            projected,
+            _allow_other_inputs=True,
+            _allow_other_attributes=True,
+            _outputs=["key_mm_sliced"],
+        )
+        value_BSD = op.Slice(
+            projected,
+            _allow_other_inputs=True,
+            _allow_other_attributes=True,
+            _outputs=["value_mm_sliced"],
+        )
+        
+        # Split past into past_key and past_value
+        # past_key and past_value are of shape (B, H, S, D/H)
+        past_key, past_value = op.Split(past, axis=0, split=[1, 1])
+
+        # TODO: Pass other attributes
+        attention, present_key, present_value = op.MultiHeadAttention(
+            query_BSD,
+            key_BSD,
+            value_BSD,
+            None,  # bias
+            None,  # key_padding_mask
+            attention_bias,
+            past_key,
+            past_value,
+            num_heads=num_heads,
+            scale=scale,           
+            _domain="com.microsoft",
+            _outputs=3,
+        )
+
+        # Concat present_key and present_value to form present
+        present = op.Concat(present_key, present_value, axis=0)
+        return attention, present
+
+    def check(
+        self,
+        op,
+        input,
+        qkv_weight,
+        qkv_bias,
+        query_mm_sliced,
+        key_mm_sliced,
+        value_mm_sliced,
+        **_,
+    ):
+        bindings: dict[str, Dim] = {}
+
+        def no_match(val: ir.Value, dims: Sequence[str]) -> bool:
+            return not _check_shape(bindings, val, dims)
+
+        if no_match(input, ["B", "S", "D"]):
+            return False
+        if no_match(qkv_weight, ["D", "Dh"]):
+            return False
+        if no_match(qkv_bias, ["Dh"]):
+            return False
+        if no_match(query_mm_sliced, ["B", "S", "Dh_q"]):
+            return False
+        if no_match(key_mm_sliced, ["B", "S", "Dh_k"]):
+            return False
+        if no_match(value_mm_sliced, ["B", "S", "Dh_v"]):
+            return False
+        
+        # Ensure Dh = Dh_q + Dh_k + Dh_v
+        Dh = bindings.get("Dh")
+        Dh_q = bindings.get("Dh_q")
+        Dh_k = bindings.get("Dh_k")
+        Dh_v = bindings.get("Dh_v")
+
+        if Dh is None or Dh_q is None or Dh_k is None or Dh_v is None:
+            return False  # Missing bindings, cannot verify
+
+        if Dh != Dh_q + Dh_k + Dh_v:
+            return False  # Hidden size mismatch
+        # TODO: Add mask check once mask is added to the pattern
+
+        return True
+
+    def rewrite(
+        self,
+        op,
+        input,
+        qkv_weight,
+        qkv_bias,
+        # mask_index,
+        past,
+        attention_bias,
+        num_heads,
+        scale,
+        **_,
+    ):
+        return op.Attention(
+            input,
+            qkv_weight,
+            qkv_bias,
+            # mask_index
+            past,
+            attention_bias,
+            # past_sequence_length
+            num_heads=num_heads,
+            # qkv_hidden_sizes=qkv_hidden_sizes,
+            scale=scale,
+            _domain="com.microsoft",
+            _outputs=2,
+        )
+
+
+attention_with_input_bias_rule = Attention.rule(
+    "attention_input_bias", has_input_bias=True
+)
+attention_with_no_input_bias_rule = Attention.rule(
+    "attention_no_input_bias", has_input_bias=False
+)
+
+attention_rules = pattern.RewriteRuleSet(
+    [
+        attention_with_input_bias_rule,
+        attention_with_no_input_bias_rule,
+    ]
+)
+
+
+def fuse_attention(model: ir.Model, *, debug: bool = False) -> int:
+    count = attention_rules.apply_to_model(model)
+    if debug and count == 0:
+        tracer = pattern.MatchingTracer()
+        attention_rules.apply_to_model(model, tracer=tracer)
+        tracer.report()
+    return count
