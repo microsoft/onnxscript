58c054c36f5eae6594e0a6bb6d238330a718e8b5
Justin Chu <justinchuby@users.noreply.github.com>
2025-04-08 17:28:12 -0700

    repr

--- onnxscript/rewriter/pattern.py
+++ onnxscript/rewriter/pattern.py
@@ -1673,6 +1673,9 @@ class RewriteRuleSet:
         # NOT remove nodes (immediately when it is applied)
         self.remove_unused_nodes = any(not rule.remove_nodes for rule in rules)
 
+    def __repr__(self) -> str:
+        return f"RewriteRuleSet({self.rules})"
+
     def _apply_to_graph_or_function(
         self,
         model: ir.Model,
