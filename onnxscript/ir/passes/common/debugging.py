88ccecb43ef18151c98678815cff529828fae84b
Justin Chu <justinchuby@users.noreply.github.com>
2025-04-14 11:59:26 -0700

    checker pass

--- onnxscript/ir/passes/common/debugging.py
+++ onnxscript/ir/passes/common/debugging.py
@@ -3,12 +3,15 @@
 from __future__ import annotations
 
 __all__ = [
-    "LiftConstantsToInitializersPass",
+    "CheckerPass",
 ]
 
 import logging
 
+import onnx
+
 from onnxscript import ir
+from onnxscript.ir.passes.common import _c_api_utils
 
 logger = logging.getLogger(__name__)
 
@@ -24,7 +27,35 @@ class CheckerPass(ir.passes.PassBase):
     def changes_input(self) -> bool:
         return False
 
-    def __init__(self, lift_all_constants: bool = False, size_limit: int = 16):
+    def __init__(
+        self,
+        full_check: bool = False,
+        skip_opset_compatibility_check: bool = False,
+        check_custom_domain: bool = False,
+    ):
         super().__init__()
-        self.lift_all_constants = lift_all_constants
-        self.size_limit = size_limit
+        self.full_check = full_check
+        self.skip_opset_compatibility_check = skip_opset_compatibility_check
+        self.check_custom_domain = check_custom_domain
+
+    def call(self, model: ir.Model) -> ir.Model:
+        """Run the onnx checker on the model."""
+
+        def _partial_check_model(proto: onnx.ModelProto) -> onnx.ModelProto:
+            """Partial function to check the model."""
+            onnx.checker.check_model(
+                proto,
+                full_check=self.full_check,
+                skip_opset_compatibility_check=self.skip_opset_compatibility_check,
+                check_custom_domain=self.check_custom_domain,
+            )
+            return proto
+
+        _c_api_utils.call_onnx_api(
+            func=_partial_check_model,
+            model=model,
+            # Since we do not modify the model. merge_func is not used but provided for completeness
+            merge_func=lambda m, proto: (m, False),
+        )
+        # The model is not modified
+        return model
