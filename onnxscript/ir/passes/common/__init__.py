8d0dcebdd67a596dd49f3023cbd57b554c63d054
Justin Chu <justinchuby@users.noreply.github.com>
2025-05-21 15:07:50 -0700

    Import from ir

--- onnxscript/ir/passes/common/__init__.py
+++ onnxscript/ir/passes/common/__init__.py
@@ -16,21 +16,17 @@ __all__ = [
     "TopologicalSortPass",
 ]
 
-from onnxscript.ir.passes.common.clear_metadata_and_docstring import (
-    ClearMetadataAndDocStringPass,
-)
-from onnxscript.ir.passes.common.constant_manipulation import (
+from onnx_ir.passes.common import (
     AddInitializersToInputsPass,
+    CheckerPass,
+    ClearMetadataAndDocStringPass,
+    InlinePass,
     LiftConstantsToInitializersPass,
     LiftSubgraphInitializersToMainGraphPass,
     RemoveInitializersFromInputsPass,
-)
-from onnxscript.ir.passes.common.inliner import InlinePass
-from onnxscript.ir.passes.common.onnx_checker import CheckerPass
-from onnxscript.ir.passes.common.shape_inference import ShapeInferencePass
-from onnxscript.ir.passes.common.topological_sort import TopologicalSortPass
-from onnxscript.ir.passes.common.unused_removal import (
     RemoveUnusedFunctionsPass,
     RemoveUnusedNodesPass,
     RemoveUnusedOpsetsPass,
+    ShapeInferencePass,
+    TopologicalSortPass,
 )
