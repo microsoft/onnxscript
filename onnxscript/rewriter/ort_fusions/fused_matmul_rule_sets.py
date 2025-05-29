4611fdcedf19e3d77cad3440208648988eaa1f6e
Bhagirath Mehta <bhamehta@microsoft.com>
2025-05-29 16:16:57 +0000

    Condense rules using type function and classVars

--- onnxscript/rewriter/ort_fusions/fused_matmul_rule_sets.py
+++ onnxscript/rewriter/ort_fusions/fused_matmul_rule_sets.py
@@ -15,7 +15,7 @@ def get_node(value: ir.Value, name: str) -> ir.Node:
     return node
 
 
-def get_kwargs(node: ir.Node) -> dict[str, float | int | str]:
+def get_kwargs(node: ir.Node) -> dict[str, ir.Attr]:
     """Get the kwargs from the node."""
     kwargs = {key: val.value for key, val in node.attributes.items()}
     return kwargs
@@ -61,7 +61,7 @@ class FusedMatMulDiv2(orp.RewriteRuleClassBase):
         c = float(value[0] if value.shape == (1,) else value)
         fused_node = get_node(fused, "FusedMatMul")
         kwargs = get_kwargs(fused_node)
-        kwargs["alpha"] = fused_node.attributes["alpha"].as_float() / c
+        kwargs["alpha"] = fused_node.attributes["alpha"].as_float() / c  # type: ignore[assignment]
         return op.FusedMatMul(x, y, **kwargs, _domain="com.microsoft")
 
 
@@ -97,7 +97,7 @@ class _TransposeMatMulBase(orp.RewriteRuleClassBase):
             fused_node = get_node(fused, "FusedMatMul")
             kwargs = get_kwargs(fused_node)
         name = "transA" if self._pos == 1 else "transB"
-        kwargs[name] = 1 - kwargs.get(name, 0)
+        kwargs[name] = 1 - kwargs.get(name, 0)  # type: ignore[assignment]
         return op.FusedMatMul(x, y, **kwargs, _domain="com.microsoft")
 
 
@@ -148,6 +148,46 @@ class _TransposeFusedMatMulBaseWithBatch(orp.RewriteRuleClassBase):
     _flip_transpose_batch: ClassVar = False
     _flip_transpose: ClassVar = False
 
+    def check(
+        self, context, x, y, transposed: ir.Value, fused: ir.Value, **_
+    ) -> orp.MatchResult:
+        check_result = orp.MatchResult()
+        fused_node = get_node(fused, "FusedMatMul")
+        if self._pos == 1:
+            transBatchProperty = "transBatchA"
+        else:
+            transBatchProperty = "transBatchB"
+        transBatch = fused_node.attributes.get(transBatchProperty, 0).as_int()  # type: ignore[union-attr]
+        transposed_node = get_node(transposed, "Transpose")
+        perm = transposed_node.attributes["perm"].as_ints()
+        list_perm = list(range(len(perm)))
+
+        if self._flip_transpose_batch and self._flip_transpose:
+            # Check when transposeBatch is 0 or 1 and transpose can be flipped i.e., when the first index is moved to the end
+            # for transposeBatch = 0 or last index is moved to the front for transposeBatch = 1
+            expected_perm0 = [*list_perm[1:], list_perm[0]]
+            expected_perm1 = [list_perm[-1], *list_perm[0:-1]]
+            if (expected_perm0 == perm and transBatch == 0) or (
+                expected_perm1 == perm and transBatch == 1
+            ):
+                return check_result
+        elif self._flip_transpose_batch:
+            # Check when transposeBatch can be flipped i.e., when the first index is moved to the second-to-last position
+            # for transposeBatch = 0 or second-to-last index is moved to the front for transposeBatch = 1
+            expected_perm0 = [*list_perm[1:-1], list_perm[0], list_perm[-1]]
+            expected_perm1 = [list_perm[-2], *list_perm[0:-2], list_perm[-1]]
+            if (expected_perm0 == perm and transBatch == 0) or (
+                expected_perm1 == perm and transBatch == 1
+            ):
+                return check_result
+        elif self._flip_transpose:
+            # Check when transposeBatch = 1 and transpose can be flipped i.e., when the first and last indices are flipped.
+            expected_perm = [list_perm[-1], *list_perm[1:-1], list_perm[0]]
+            if expected_perm == perm and transBatch == 1:
+                return check_result
+
+        return check_result.fail("Permutation values for Transpose are not correct.")
+
     def rewrite(self, op, x, y, fused: ir.Value, **_):
         kwargs = {}
         fused_node = get_node(fused, "FusedMatMul")
@@ -155,10 +195,10 @@ class _TransposeFusedMatMulBaseWithBatch(orp.RewriteRuleClassBase):
         name = "A" if self._pos == 1 else "B"
         if self._flip_transpose_batch:
             transBatchName = f"transBatch{name}"
-            kwargs[transBatchName] = 1 - kwargs[transBatchName]
+            kwargs[transBatchName] = 1 - kwargs[transBatchName]  # type: ignore[assignment]
         if self._flip_transpose:
             transName = f"trans{name}"
-            kwargs[transName] = 1 - kwargs[transName]
+            kwargs[transName] = 1 - kwargs[transName]  # type: ignore[assignment]
         return op.FusedMatMul(x, y, **kwargs, _domain="com.microsoft")
 
     def pattern(self, op, x, y):
@@ -178,128 +218,36 @@ class _TransposeFusedMatMulBaseWithBatch(orp.RewriteRuleClassBase):
             )
 
 
-class TransposeFusedMatMulWithFlippedBatch1(_TransposeFusedMatMulBaseWithBatch):
-    """Replaces ``Transpose + FusedMatMul`` with FusedMatMul.
-    This rule is for when only transBatchA can be flipped i.e.,
-    when the transpose indices are [1:-1, 0, -1] for transBatchA = 0 and
-    [-2, 0:-2, -1] for transBatchA = 1.
-    """
-
-    _flip_transpose_batch: ClassVar = True
-
-    def check(
-        self, context, x, y, transposed: ir.Value, fused: ir.Value, **_
-    ) -> orp.MatchResult:
-        check_result = orp.MatchResult()
-        transposed_node = get_node(transposed, "Transpose")
-        perm = transposed_node.attributes["perm"].as_ints()
-        fused_node = get_node(fused, "FusedMatMul")
-        # Check that last two dimensions are swapped
-        list_perm = list(range(len(perm)))
-        expected_perm0 = [*list_perm[1:-1], list_perm[0], list_perm[-1]]
-        expected_perm1 = [list_perm[-2], *list_perm[0:-2], list_perm[-1]]
-        if self._pos == 1:
-            transBatchProperty = "transBatchA"
-        else:
-            transBatchProperty = "transBatchB"
-        transBatch = fused_node.attributes.get(transBatchProperty, 0).as_int()  # type: ignore[union-attr]
-        if (expected_perm0 == perm and transBatch == 0) or (
-            expected_perm1 == perm and transBatch == 1
-        ):
-            return check_result
-        return check_result.fail("Permutation values for Transpose are not correct.")
-
-
-class TransposeFusedMatMulWithFlippedBatch2(_TransposeFusedMatMulBaseWithBatch):
-    """Replaces ``Transpose + FusedMatMul`` with FusedMatMul.
-    This rule is for when only transBatchB can be flipped i.e.,
-    when the transpose indices are [1:-1, 0, -1] for transBatchB = 0 and
-    [-2, 0:-2, -1] for transBatchB = 1.
-    """
-
-    _pos: ClassVar = 2
-
-
-class TransposeFusedMatMulWithFlippedBatchAndTranspose1(_TransposeFusedMatMulBaseWithBatch):
-    """Replaces ``Transpose + FusedMatMul`` with FusedMatMul.
-    This rule is for when transBatchA and transA can be flipped i.e.,
-    when the transpose indices are [1:-1, -1, 0] for transBatchA = 0 and
-    [-2, 0:-2, -1] for transBatchA = 1.
-    """
-
-    _flip_transpose_batch: ClassVar = True
-    _flip_transpose: ClassVar = True
-
-    def check(
-        self, context, x, y, transposed: ir.Value, fused: ir.Value, **_
-    ) -> orp.MatchResult:
-        check_result = orp.MatchResult()
-        transposed_node = get_node(transposed, "Transpose")
-        perm = transposed_node.attributes["perm"].as_ints()
-        fused_node = get_node(fused, "FusedMatMul")
-        # Check that last two dimensions are swapped
-        list_perm = list(range(len(perm)))
-        expected_perm0 = [*list_perm[1:], list_perm[0]]
-        expected_perm1 = [list_perm[-1], *list_perm[0:-1]]
-        if self._pos == 1:
-            transBatchProperty = "transBatchA"
-        else:
-            transBatchProperty = "transBatchB"
-        transBatch = fused_node.attributes.get(transBatchProperty, 0).as_int()  # type: ignore[union-attr]
-        if (expected_perm0 == perm and transBatch == 0) or (
-            expected_perm1 == perm and transBatch == 1
-        ):
-            return check_result
-        return check_result.fail("Permutation values for Transpose are not correct.")
-
-
-class TransposeFusedMatMulWithFlippedBatchAndTranspose2(
-    TransposeFusedMatMulWithFlippedBatchAndTranspose1
-):
-    """Replaces ``Transpose + FusedMatMul`` with FusedMatMul.
-    This rule is for when transBatchB and transB can be flipped i.e.,
-    when the transpose indices are [1:-1, -1, 0] for transBatchB = 0 and
-    [-2, 0:-2, -1] for transBatchB = 1.
-    """
-
-    _pos: ClassVar = 2
-
-
-class TransposeFusedMatMulWithBatchAndTranspose1(_TransposeFusedMatMulBaseWithBatch):
-    """Replaces ``Transpose + FusedMatMul`` with FusedMatMul.
-    This rule is for when transBatchA = 1 and transA can be flipped i.e.,
-    when the transpose indices are [-1, 1:-1, 0].
-    """
-
-    _flip_transpose: ClassVar = True
-
-    def check(
-        self, context, x, y, transposed: ir.Value, fused: ir.Value, **_
-    ) -> orp.MatchResult:
-        check_result = orp.MatchResult()
-        transposed_node = get_node(transposed, "Transpose")
-        perm = transposed_node.attributes["perm"].as_ints()
-        fused_node = get_node(fused, "FusedMatMul")
-        # Check that last two dimensions are swapped
-        list_perm = list(range(len(perm)))
-        expected_perm = [list_perm[-1], *list_perm[1:-1], list_perm[0]]
-        if self._pos == 1:
-            transBatchProperty = "transBatchA"
-        else:
-            transBatchProperty = "transBatchB"
-        transBatch = fused_node.attributes.get(transBatchProperty, 0).as_int()  # type: ignore[union-attr]
-        if expected_perm == perm and transBatch == 1:
-            return check_result
-        return check_result.fail("Permutation values for Transpose are not correct.")
-
-
-class TransposeFusedMatMulWithBatchAndTranspose2(TransposeFusedMatMulWithBatchAndTranspose1):
-    """Replaces ``Transpose + FusedMatMul`` with FusedMatMul.
-    This rule is for when transBatchB = 1 and transB can be flipped i.e.,
-    when the transpose indices are [-1, 1:-1, 0].
-    """
-
-    _pos: ClassVar = 2
+TransposeFusedMatMulWithFlippedBatchAndTranspose1 = type(
+    "TransposeFusedMatMulWithFlippedBatchAndTranspose1",
+    (_TransposeFusedMatMulBaseWithBatch,),
+    {"_flip_transpose": True, "_flip_transpose_batch": True},
+)
+TransposeFusedMatMulWithFlippedBatchAndTranspose2 = type(
+    "TransposeFusedMatMulWithFlippedBatchAndTranspose2",
+    (_TransposeFusedMatMulBaseWithBatch,),
+    {"_pos": 2, "_flip_transpose": True, "_flip_transpose_batch": True},
+)
+TransposeFusedMatMulWithFlippedBatch1 = type(
+    "TransposeFusedMatMulWithFlippedBatch1",
+    (_TransposeFusedMatMulBaseWithBatch,),
+    {"_flip_transpose_batch": True},
+)
+TransposeFusedMatMulWithFlippedBatch2 = type(
+    "TransposeFusedMatMulWithFlippedBatch2",
+    (_TransposeFusedMatMulBaseWithBatch,),
+    {"_pos": 2, "_flip_transpose_batch": True},
+)
+TransposeFusedMatMulWithBatchAndTranspose1 = type(
+    "TransposeFusedMatMulWithBatchAndTranspose1",
+    (_TransposeFusedMatMulBaseWithBatch,),
+    {"_flip_transpose": True},
+)
+TransposeFusedMatMulWithBatchAndTranspose2 = type(
+    "TransposeFusedMatMulWithBatchAndTranspose2",
+    (_TransposeFusedMatMulBaseWithBatch,),
+    {"_pos": 2, "_flip_transpose": True},
+)
 
 
 class MatMulTranspose(orp.RewriteRuleClassBase):
@@ -324,7 +272,7 @@ class MatMulTranspose(orp.RewriteRuleClassBase):
             fused_node = get_node(fused, "FusedMatMul")
             kwargs = get_kwargs(fused_node)
         for name in ["transA", "transB"]:
-            kwargs[name] = 1 - kwargs.get(name, 0)
+            kwargs[name] = 1 - kwargs.get(name, 0)  # type: ignore[assignment]
         return op.FusedMatMul(y, x, **kwargs, _domain="com.microsoft")
 
 
@@ -356,11 +304,11 @@ def fused_matmul_rule_sets() -> orp.RewriteRuleSet:
             TransposeFusedMatMul1.rule(),
             TransposeMatMul2.rule(),
             TransposeFusedMatMul2.rule(),
-            TransposeFusedMatMulWithFlippedBatch1.rule(),
-            TransposeFusedMatMulWithFlippedBatch2.rule(),
-            TransposeFusedMatMulWithFlippedBatchAndTranspose1.rule(),
-            TransposeFusedMatMulWithFlippedBatchAndTranspose2.rule(),
-            TransposeFusedMatMulWithBatchAndTranspose1.rule(),
-            TransposeFusedMatMulWithBatchAndTranspose2.rule(),
+            TransposeFusedMatMulWithFlippedBatch1.rule(),  # type: ignore[attr-defined]
+            TransposeFusedMatMulWithFlippedBatch2.rule(),  # type: ignore[attr-defined]
+            TransposeFusedMatMulWithFlippedBatchAndTranspose1.rule(),  # type: ignore[attr-defined]
+            TransposeFusedMatMulWithFlippedBatchAndTranspose2.rule(),  # type: ignore[attr-defined]
+            TransposeFusedMatMulWithBatchAndTranspose1.rule(),  # type: ignore[attr-defined]
+            TransposeFusedMatMulWithBatchAndTranspose2.rule(),  # type: ignore[attr-defined]
         ]
     )
