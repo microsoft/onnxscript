ac22d84b3418a30762988943dbfc7c9012749bdc
Johansmm <jmejia_26@hotmail.com>
2025-05-31 02:23:15 +0200

    [Rewriter]: introduce normalize_pad_format_conv (#2301)
    Convert 'auto_pad' attribute into a list of explicit pads.

--- onnxscript/rewriter/fuse_pad_into_conv_test.py
+++ onnxscript/rewriter/fuse_pad_into_conv_test.py
@@ -6,13 +6,14 @@ import unittest
 import numpy as np
 import onnx_ir as ir
 import parameterized
-from onnx_ir.passes.common import onnx_checker
+from onnx_ir.passes.common import onnx_checker, shape_inference
 
 from onnxscript.rewriter import pattern as orp
 from onnxscript.rewriter import testing
 from onnxscript.rewriter.fuse_pad_into_conv import (
     fuse_pad_into_conv,
     fuse_pad_into_conv_rule_set,
+    normalize_pad_format_conv,
 )
 
 
@@ -83,22 +84,24 @@ class FusePadConvBaseTest(unittest.TestCase):
             ir_version=9,
         )
         onnx_checker.CheckerPass(True)(ir_model)
+        ir_model = shape_inference.infer_shapes(ir_model)
         return ir_model
 
 
 class FusePadConvTest(FusePadConvBaseTest):
     @parameterized.parameterized.expand(
         [
-            (pad_pads, const_value, axes, conv_pads)
-            for pad_pads, axes, conv_pads in [
-                ([0, 0, 2, 2, 0, 0, 2, 2], None, None),
-                ([0, 2, 2, 0, 2, 2], ir.tensor([1, -2, -1], name="axes"), [2, 0, 2, 0]),
-                ([1, 1, 1, 1], ir.tensor([-2, 3], name="axes"), [0, 1, 0, 1]),
+            (pad_pads, const_value, axes, conv_pads, conv_auto_pad)
+            for pad_pads, axes, conv_pads, conv_auto_pad in [
+                ([0, 0, 2, 2, 0, 0, 2, 2], None, None, None),
+                ([0, 2, 2, 0, 2, 2], ir.tensor([1, -2, -1], name="axes"), [2, 0, 2, 0], None),
+                ([1, 1, 1, 1], ir.tensor([-2, 3], name="axes"), [0, 1, 0, 1], None),
+                ([1, 3, 1, 3], ir.tensor([3, 2], name="axes"), None, "VALID"),
             ]
             for const_value in [None, 0.0]
         ]
     )
-    def test_fuse_pad_into_conv(self, pad_pads, const_value, axes, conv_pads):
+    def test_fuse_pad_into_conv(self, pad_pads, const_value, axes, conv_pads, conv_auto_pad):
         pad_inputs = [ir.tensor(pad_pads, name="pads")]
         if const_value is not None or axes is not None:
             pad_inputs.append(const_value)
@@ -109,7 +112,7 @@ class FusePadConvTest(FusePadConvBaseTest):
             input_shape=ir.Shape(("N", 32, 14, 16)),
             weight_shape=(10, 32, 3, 3),
             pad_inputs=pad_inputs,
-            conv_attributes={"pads": conv_pads},
+            conv_attributes={"pads": conv_pads, "auto_pad": conv_auto_pad},
         )
         updated_model = _clone_model(base_model)
 
@@ -117,7 +120,7 @@ class FusePadConvTest(FusePadConvBaseTest):
         count = fuse_pad_into_conv_rule_set().apply_to_model(updated_model)
 
         # Check that Pad was fused
-        self.assertEqual(count, 1)
+        self.assertEqual(count, 1 if conv_auto_pad is None else 2)
         self.assertEqual(updated_model.graph.num_nodes(), 1)
         onnx_checker.CheckerPass(True)(updated_model)
 
@@ -223,16 +226,19 @@ class FusePadConvIntegerTest(FusePadConvBaseTest):
 
     @parameterized.parameterized.expand(
         [
-            (pad_pads, const_value, axes, conv_pads)
-            for pad_pads, axes, conv_pads in [
-                ([0, 0, 3, 2, 0, 0, 1, 4], None, [1, 1, 1, 1]),
-                ([2, 2, 0, 2, 2, 0], ir.tensor([-2, -1, 1], name="axes"), None),
-                ([1, 2, 2, 1], ir.tensor([-1, 2], name="axes"), [0, 1, 0, 1]),
+            (pad_pads, const_value, axes, conv_pads, conv_auto_pad)
+            for pad_pads, axes, conv_pads, conv_auto_pad in [
+                ([0, 0, 3, 2, 0, 0, 1, 4], None, [1, 1, 1, 1], None),
+                ([2, 2, 0, 2, 2, 0], ir.tensor([-2, -1, 1], name="axes"), None, None),
+                ([1, 2, 2, 1], ir.tensor([-1, 2], name="axes"), [0, 1, 0, 1], None),
+                ([3, 3], ir.tensor([2], name="axes"), None, "SAME_UPPER"),
             ]
             for const_value in [None, ir.tensor(np.array([0], "uint8"), name="const_value")]
         ]
     )
-    def test_fuse_pad_into_conv_integer(self, pad_pads, const_value, axes, conv_pads):
+    def test_fuse_pad_into_conv_integer(
+        self, pad_pads, const_value, axes, conv_pads, conv_auto_pad
+    ):
         pad_inputs = [ir.tensor(pad_pads, name="pads")]
         if const_value is not None or axes is not None:
             pad_inputs.append(const_value)
@@ -243,7 +249,7 @@ class FusePadConvIntegerTest(FusePadConvBaseTest):
             input_shape=ir.Shape(("N", 24, 19, 23)),
             weight_shape=(8, 24, 3, 3),
             pad_inputs=pad_inputs,
-            conv_attributes={"pads": conv_pads},
+            conv_attributes={"pads": conv_pads, "auto_pad": conv_auto_pad},
         )
         updated_model = _clone_model(base_model)
 
@@ -251,13 +257,75 @@ class FusePadConvIntegerTest(FusePadConvBaseTest):
         count = fuse_pad_into_conv_rule_set().apply_to_model(updated_model)
 
         # Check that Pad was fused
-        self.assertEqual(count, 1)
+        self.assertEqual(count, 1 if conv_auto_pad is None else 2)
         self.assertEqual(updated_model.graph.num_nodes(), 1)
         onnx_checker.CheckerPass(True)(updated_model)
 
         # Check inference
         inputs = self.rng.integers(0, 255, (1, 24, 19, 23), dtype="uint8")
         testing.assert_numerically_equal(base_model, updated_model, (inputs,), atol=0, rtol=0)
+
+
+class NormalizePadFormatTest(FusePadConvBaseTest):
+    @parameterized.parameterized.expand(
+        [
+            (strides, kernel_shape, auto_pad)
+            for strides, kernel_shape in [((2, 3), (1, 4)), ((2, 1), (5, 2))]
+            for auto_pad in ["SAME_UPPER", "SAME_LOWER", "VALID"]
+        ]
+    )
+    def test_normalize_pad_format(self, strides, kernel_shape, auto_pad):
+        pad_inputs = [
+            ir.tensor([1, 1, 1, 1], name="pads"),
+            None,
+            ir.tensor([2, 3], name="axes"),
+        ]
+        base_model = self.build_model(
+            op_type="Conv",
+            input_shape=ir.Shape(("N", 32, 22, 27)),
+            weight_shape=(32, 32, *kernel_shape),
+            pad_inputs=pad_inputs,
+            conv_attributes={
+                "strides": strides,
+                "auto_pad": auto_pad,
+                "kernel_shape": kernel_shape,
+            },
+        )
+        updated_model = _clone_model(base_model)
+
+        # Apply rule
+        count = fuse_pad_into_conv_rule_set().apply_to_model(updated_model)
+
+        # Check that Pad was fused
+        self.assertEqual(count, 2)
+        self.assertEqual(updated_model.graph.num_nodes(), 1)
+        onnx_checker.CheckerPass(True)(updated_model)
+
+        # Check inference
+        inputs = self.rng.random((1, 32, 22, 27), dtype="float32")
+        testing.assert_numerically_equal(base_model, updated_model, (inputs,), atol=0, rtol=0)
+
+    def test_unsupported_normalize_pad_format(self):
+        base_model = self.build_model(
+            op_type="Conv",
+            input_shape=ir.Shape(("N", 32, 14)),
+            weight_shape=(32, 11, 4),
+            pad_inputs=[ir.tensor([0, 0, 0, 0, 0, 0], name="pads")],
+            conv_attributes={"auto_pad": "VALID"},
+        )
+        # Drop convolutional input shape
+        base_model.graph[0].outputs[0].shape = None
+        onnx_checker.CheckerPass(True)(base_model)
+
+        # Apply rule and check it was not applied
+        tracer = orp.MatchingTracer()
+        count = normalize_pad_format_conv.apply_to_model(base_model, tracer=tracer)
+        self.assertEqual(count, 0)
+
+        # Check that the error message is the expected one
+        tracer_match = tracer.best_matches_map[normalize_pad_format_conv][0]
+        self.assertEqual(tracer_match.status.value, orp.MatchStatus.CONDITION_FAILED)
+        self.assertRegex(tracer_match.match_result.reason, "Input shapes are not defined")
 
 
 if __name__ == "__main__":
