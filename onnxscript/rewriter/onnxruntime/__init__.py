1d92fd89a258138861fc8dd77c9d75904a381799
Markus Bilz <github@markusbilz.com>
2025-06-03 16:51:42 +0200

    refactor: remove unused function rules param

--- onnxscript/rewriter/onnxruntime/__init__.py
+++ onnxscript/rewriter/onnxruntime/__init__.py
@@ -25,14 +25,12 @@ ORT_FUNCTION_REWRITE_RULES: list[Any] = []
 def rewrite(
     model_proto: onnx.ModelProto,
     /,
-    function_rules=None,
     pattern_rules: Sequence[pattern.RewriteRule] | None = None,
 ) -> onnx.ModelProto:
     """Rewrite the model using the given rules.
 
     Args:
         model_proto: The model to rewrite.
-        function_rules: The function rewrite rules to apply. Deprecated.
         pattern_rules: The pattern rewrite rules to apply. If None, the default rules
             for onnxruntime are used.
 
