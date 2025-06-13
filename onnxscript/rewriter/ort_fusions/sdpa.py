949bc240f0bf788419179908b56581ce835dc7b5
G. Ramalingam <grama@microsoft.com>
2025-06-13 15:56:47 -0700

    Fusion extensions to improve GQA fusion (#2374)
    Various extensions to improve GQA fusion.
    
    * Move key-transpose into SDPA fusion and clean it up
    * Extend cos-sin-cache fusion to handle a new pattern
    * Reorder GQA and MHA rules
    * Introduce MaskedGQA, since many uses in practice generated GQA with a
    mask
    * MaskedGQA is subsequently simplified to ORT's GQA if the mask can be
    verified to be causal.
    
    ---------
    
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>

--- onnxscript/rewriter/ort_fusions/sdpa.py
+++ onnxscript/rewriter/ort_fusions/sdpa.py
@@ -20,13 +20,34 @@ class SDPA(pattern.RewriteRuleClassBase):
         self,
         op,
         query,
-        key_transposed,
+        key,
         value,
         mask,
         query_scale,
         key_scale,
         qk_scale,
     ):
+        # The last two axes of key must be transposed before computing the dot product with query.
+        # Three patterns are observed in practice:
+
+        # Pattern 1: Transpose 4D key directly: BHSd => BHdS
+        key_transposed_1 = op.Transpose(key, perm=[0, 1, 3, 2])
+
+        # Pattern 2: Transpose key after converting to 3D and then convert back to 4D: BHSd => 3D => BHdS
+        key_3d = op.Reshape(key, pattern.ANY_VALUE)
+        key_3d_transposed = op.Transpose(key_3d, perm=[0, 2, 1])
+        key_transposed_2 = op.Reshape(key_3d_transposed, pattern.ANY_VALUE)
+
+        # Pattern 3: This transpose is sometimes composed with an earlier transpose to convert
+        # the key from BSHd format to BHSd format.
+        key_transposed_3 = op.Transpose(key, perm=[0, 2, 3, 1])
+
+        key_transposed = pattern.OrValue(
+            [key_transposed_1, key_transposed_2, key_transposed_3],
+            tag_var="key_format",
+            tag_values=["BHSd", "BHSd", "BSHd"],
+        )
+
         # Some implementations scale the query and key before computing the dot product
         query = pattern.OrValue(
             [
@@ -74,9 +95,10 @@ class SDPA(pattern.RewriteRuleClassBase):
         self,
         context,
         query: ir.Value | None,
-        key_transposed: ir.Value | None,
+        key: ir.Value | None,
         value: ir.Value | None,
         mask: ir.Value | None,
+        key_format: str,
         **match_bindings,
     ):
         check_result = pattern.MatchResult()
@@ -90,7 +112,11 @@ class SDPA(pattern.RewriteRuleClassBase):
         # Query and Key should have same head-size (Dh) while value can have different head-size (Dv).
         # Key and Value should have same sequence length (Skv), while Query can have different sequence length (S).
         _fusion_utils.check_shape(bindings, query, ["B", "H", "S", "Dh"])
-        _fusion_utils.check_shape(bindings, key_transposed, ["B", "H", "Dh", "Skv"])
+        if key_format == "BHSd":
+            _fusion_utils.check_shape(bindings, key, ["B", "H", "Skv", "Dh"])
+        else:
+            assert key_format == "BSHd", f"Unexpected key format: {key_format}"
+            _fusion_utils.check_shape(bindings, key, ["B", "Skv", "H", "Dh"])
         _fusion_utils.check_shape(bindings, value, ["B", "H", "Skv", "Dv"])
 
         def get_scale_value(tag_name: str, scale_name: str) -> float:
@@ -132,20 +158,29 @@ class SDPA(pattern.RewriteRuleClassBase):
         self,
         op,
         query: ir.Value | None,
-        key_transposed: ir.Value | None,
+        key: ir.Value | None,
         value: ir.Value | None,
         mask: ir.Value | None,
+        key_format: str,
         **_,
     ):
-        sdpa_args = [query, key_transposed, value]
+        sdpa_args = [query, key, value]
         if mask is not None:
             sdpa_args.append(mask)
         # If the scale is None, SDPA will use the default scaling factor, which is 1/sqrt(head_size).
-        return op.SDPA(*sdpa_args, scale=self._scale, _domain="ai.onnxruntime.fusion")
+        return op.SDPA(
+            *sdpa_args,
+            scale=self._scale,
+            key_format=key_format,
+            _domain="ai.onnxruntime._fusion",
+        )
 
 
 # Dynamically create the rules
-sdpa_rules = pattern.RewriteRuleSet([SDPA.rule()])
-
+sdpa_rules = pattern.RewriteRuleSet(
+    [
+        SDPA.rule(),
+    ]
+)
 
 fuse_sdpa = _fusion_utils.apply_fusion_rules(sdpa_rules)
