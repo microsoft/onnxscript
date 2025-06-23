97119bd3c90f5b872de2d81241cc9f49eaba85d3
Ganesan Ramalingam <grama@microsoft.com>
2025-06-23 11:49:16 -0700

    Add support for onnx fusions
    Signed-off-by: Ganesan Ramalingam <grama@microsoft.com>

---
+++ onnxscript/rewriter/onnx_fusions/_core.py
@@ -0,0 +1,37 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+from __future__ import annotations
+
+import onnx_ir as ir
+from onnxscript.rewriter.onnx_fusions import rms_normalization
+
+
+def _get_onnx_opset_version(model: ir.Model) -> int | None:
+    """Get the ONNX opset version imported by the model."""
+    model_version1 = model.opset_imports.get("")
+    model_version2 = model.opset_imports.get("ai.onnx")
+    if model_version1 is not None and model_version2 is not None:
+        if model_version1 != model_version2:
+            raise ValueError(
+                f"Model imports multiple onnx opsets: {model_version1} and {model_version2}."
+            )
+    return model_version1 or model_version2
+
+
+def _opset_23_fuse(model: ir.Model, *, debug: bool = False) -> dict[str, int]:
+    """
+    Apply fusions targetting ONNX opset 23.
+    """
+    counts: dict[str, int] = {}
+    counts["RMSNormalization"] = rms_normalization.fuse(
+        model, debug=debug
+    )
+
+
+def fuse(model: ir.Model, *, debug: bool = False) -> dict[str, int]:
+    """
+    Apply fusions targetting ONNX ops.
+    """
+    model_opset_version = _get_onnx_opset_version(model)
+    if model_opset_version == 23:
+        return _opset_23_fuse(model, debug=debug)
