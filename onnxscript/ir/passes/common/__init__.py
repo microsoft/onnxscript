44b494d9c5223ebd146107691a2b534abdb645e2
Justin Chu <justinchuby@users.noreply.github.com>
2025-06-25 14:33:47 -0700

    Update ir.pass.common alias to import all from onnx_ir

--- onnxscript/ir/passes/common/__init__.py
+++ onnxscript/ir/passes/common/__init__.py
@@ -1,34 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-__all__ = [
-    "AddInitializersToInputsPass",
-    "CheckerPass",
-    "ClearMetadataAndDocStringPass",
-    "CommonSubexpressionEliminationPass",
-    "InlinePass",
-    "LiftConstantsToInitializersPass",
-    "LiftSubgraphInitializersToMainGraphPass",
-    "RemoveInitializersFromInputsPass",
-    "RemoveUnusedFunctionsPass",
-    "RemoveUnusedNodesPass",
-    "RemoveUnusedOpsetsPass",
-    "ShapeInferencePass",
-    "TopologicalSortPass",
-]
-
-from onnx_ir.passes.common import (
-    AddInitializersToInputsPass,
-    CheckerPass,
-    ClearMetadataAndDocStringPass,
-    CommonSubexpressionEliminationPass,
-    InlinePass,
-    LiftConstantsToInitializersPass,
-    LiftSubgraphInitializersToMainGraphPass,
-    RemoveInitializersFromInputsPass,
-    RemoveUnusedFunctionsPass,
-    RemoveUnusedNodesPass,
-    RemoveUnusedOpsetsPass,
-    ShapeInferencePass,
-    TopologicalSortPass,
-)
+from onnx_ir.passes.common import *
