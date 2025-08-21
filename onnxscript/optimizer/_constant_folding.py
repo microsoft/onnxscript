6a5848256fe59162f5da6f68587d65f963ab5e15
Justin Chu <justinchuby@users.noreply.github.com>
2025-08-21 12:58:00 -0700

    Fixes for when attr type can be ambiguous for empty lists
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxscript/optimizer/_constant_folding.py
+++ onnxscript/optimizer/_constant_folding.py
@@ -388,7 +388,7 @@ def gather(node: ir.Node, op, state: OptimizerState) -> ReturnValue:
     if output is not None:
         state.set_sym_value(output, ir.Shape(gathered))
     if all(isinstance(d, int) for d in gathered):
-        return op.Constant(value_ints=gathered)
+        return op.Constant(value_ints=ir.AttrInt64s("value_ints", gathered))
     return None
 
 
@@ -466,7 +466,7 @@ def shape(node: ir.Node, op, state: OptimizerState) -> ReturnValue:
     if output is not None:
         state.set_sym_value(output, ir.Shape(shape_slice))
     if all(isinstance(d, int) for d in shape_slice):
-        return op.Constant(value_ints=list(shape_slice))
+        return op.Constant(value_ints=ir.AttrInt64s("value_ints", list(shape_slice)))
     return None
 
 
