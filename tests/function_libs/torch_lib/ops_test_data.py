b53670a3bd01324cf7a3d3f64f797e165f215179
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-12 08:55:18 -0700

    Create tests
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- tests/function_libs/torch_lib/ops_test_data.py
+++ tests/function_libs/torch_lib/ops_test_data.py
@@ -2124,6 +2124,7 @@ TESTED_TORCHLIB_OPS: tuple[TorchLibOpInfo, ...] = (
             "Our implementation is based on that for CUDA"
         ),
     ),
+    TorchLibOpInfo("ops.prims.broadcast_in_dim.default", prims_ops.prims_broadcast_in_dim),
     TorchLibOpInfo(
         "ops.prims.var.default", prims_ops.prims_var, tolerance={torch.float16: (1e-3, 5e-2)}
     ),
