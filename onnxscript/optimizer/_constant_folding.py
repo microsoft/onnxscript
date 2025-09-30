722765500257cdcc89a59eec35a5c2f17f79e522
Daniel Zhang <wodesuck@gmail.com>
2025-10-01 04:04:20 +0800

    Merge input and output shape when removing identity (#2588)
    Similar with #2578, for this case:
    
    ```python
    import torch
    import torch.nn as nn
    
    
    class Model(nn.Module):
        def forward(self, x):
            return x.new_zeros(x.shape)
    
    
    def main():
        model = Model()
        args = torch.rand(4, 4),
        batch = torch.export.Dim("batch")
        dynamic_shapes = {"x": {0: batch}}
        torch.onnx.export(
            model,
            args,
            "model_test.onnx",
            dynamic_shapes=dynamic_shapes,
            dynamo=True,
        )
    
    
    if __name__ == "__main__":
        main()
    ```
    
    ---------
    
    Co-authored-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxscript/optimizer/_constant_folding.py
+++ onnxscript/optimizer/_constant_folding.py
@@ -608,6 +608,9 @@ def identity(node: ir.Node, op, state: OptimizerState) -> ReturnValue:
     input = node.inputs[0]
     output = node.outputs[0]
     if input is not None and output is not None:
+        input.shape = _merge_shapes(input.shape, output.shape)
+        if input.type is None:
+            input.type = output.type
         state.set_sym_value(output, input)
     return None
 
