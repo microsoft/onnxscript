c1bfdfc5baf2dabf09191ad2ab7603ea21cfe614
G. Ramalingam <grama@microsoft.com>
2025-11-12 10:33:54 -0800

    Utility and example for custom op expansion (#2701)
    A utility and an example showing how onnxscript functions can be used to
    define function expansions and be used with the inliner to replace calls
    to the custom function with an expanded subgraph. This is useful to
    perform certain classes of graph surgery easily.

---
+++ onnxscript/utils/replace.py
@@ -0,0 +1,35 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+"""A utility function to replace custom operations in a model with their expansions"""
+
+from typing import Sequence
+
+import onnx
+import onnx_ir as ir
+import onnx_ir.passes.common as common_passes
+
+
+def replace_functions(
+    model: onnx.ModelProto, functions: Sequence[onnx.FunctionProto]
+) -> onnx.ModelProto:
+    """A utility function to replace custom operations in a model with their expansions:
+    Args:
+        model: An ONNX ModelProto possibly containing calls to custom operations.
+        functions: A sequence of FunctionProto defining the expansions for the custom operations.
+
+    Returns:
+        An updated ModelProto with custom operations replaced by their expansions.
+    """
+    irmodel = ir.from_proto(model)
+    irfunctions = [ir.from_proto(func) for func in functions]
+    model_functions = irmodel.functions
+    if len(model_functions) != 0:
+        # Since we use inlining, check that there are no model-local functions.
+        raise ValueError("Input model cannot have model-local functions.")
+    for func in irfunctions:
+        model_functions[func.identifier()] = func
+
+    # TODO (rama): Ideally, we should provide users more control over renaming strategy for inlined values.
+    common_passes.InlinePass()(irmodel)
+    common_passes.RemoveUnusedOpsetsPass()(irmodel)
+    return ir.to_proto(irmodel)
