718b87681b7ab2a78fb7cf0f348e1e5066229a2a
Justin Chu <justinchuby@users.noreply.github.com>
2026-01-07 16:59:15 -0800

    wip
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxscript/sourceinfo.py
+++ onnxscript/sourceinfo.py
@@ -1,7 +1,5 @@
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-# -------------------------------------------------------------------------
 
 """Source code information used for diagnostic messages."""
 
