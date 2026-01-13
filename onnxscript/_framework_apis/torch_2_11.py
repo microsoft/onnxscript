94b73231d4f574abfc9bff597c367b825e80c952
Justin Chu <justinchuby@users.noreply.github.com>
2026-01-13 11:17:51 -0800

    Create torch apis 2.11
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

---
+++ onnxscript/_framework_apis/torch_2_11.py
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+"""Stable APIs for PyTorch 2.9."""
+
+from __future__ import annotations
+
+__all__ = [
+    "check_model",
+    "convert_version",
+    "get_torchlib_ops",
+    "optimize",
+    "save_model_with_external_data",
+]
+
+
+from onnxscript._framework_apis.torch_2_9 import (
+    check_model,
+    convert_version,
+    get_torchlib_ops,
+    optimize,
+    save_model_with_external_data,
+)
